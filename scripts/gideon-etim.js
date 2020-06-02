20b087525ef0bbd6bec8da246986c4a99d7283cb
Tha Gidi <you@example.com>
2020-06-02 22:14:02 +0100

    output string corrected

--- scripts/gideon-etim.js
+++ scripts/gideon-etim.js
@@ -2,5 +2,5 @@ let fullName = "Gideon Etim";
 let hngId = "HNG-00144";
 let language = "Javascript";
 
-let message = `Hello World, this is ${fullName}, with HNGi7 ID ${hngId} using ${language} for the stage 2 task`;
+let message = `Hello World, this is ${fullName}, with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
 console.log(message);
