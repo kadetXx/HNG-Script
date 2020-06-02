b7c65acd6de2ffdddcfa28fa537a2b297a2026f1
Hussaini Muhammad Auwal <53785220+blackalbino01@users.noreply.github.com>
2020-06-02 18:36:31 +0100

    Commited

---
+++ scripts/Hussaini-Muhammad Auwal.py
@@ -0,0 +1,6 @@
+full_name="Hussaini Muhammad Auwal"
+ID="HNG-01895"
+language="Python"
+def task( ):
+	return "Hello World,this is {0} with HNGi7 ID {1} using {2} for stage 2 task".format(full_name,ID,language)
+print(task())
