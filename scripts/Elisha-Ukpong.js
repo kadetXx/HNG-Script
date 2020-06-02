71eb5883a45efe1c77dc288e429c45468a009265
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-02 23:43:10 +0100

    fixed

--- scripts/Elisha-Ukpong.js
+++ scripts/Elisha-Ukpong.js
@@ -1,1 +1,4 @@
-console.log("Hello World, this is Elisha Ukpong with HNGi7 ID HNG-03659 using JavaScript for stage 2 task.");
+
+(
+    console.log("Hello World, this is Elisha Ukpong with HNGi7 ID HNG-03659 using JavaScript for stage 2 task.")
+)();
