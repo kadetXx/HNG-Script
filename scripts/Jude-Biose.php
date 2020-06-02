f3a9ffa96e247da2827a9a4cde16be23cba40341
judeb21 <Judebiose20@gmail.com>
2020-06-02 23:39:59 +0100

    Latest commit

--- scripts/Jude-Biose.php
+++ scripts/Jude-Biose.php
@@ -4,13 +4,13 @@ header("Content-Type: application/json");
 
     $fName  = "Jude";
     $lName = "Biose";
-    $HNG_ID = "HNG-01207";
+    $HNGi7_ID = "HNG-01207";
     $language = "PHP";
 
     $location_vars = "Hello World, this is "
                       .$fName. " " .$lName.
                       " with HNGi7 ID "
-                      .$HNG_ID. " using " .$language. 
+                      .$HNGi7_ID. " using " .$language. 
                       " for stage 2 task.";
 
     $result = json_encode($location_vars);;
