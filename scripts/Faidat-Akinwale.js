2db847e8a523ea1ba890196b1037949acdba8639
Akinwale Mutiat <60321940+teeteelawlah@users.noreply.github.com>
2020-06-02 21:40:03 +0100

    Delete Faidat-Akinwale.js

--- scripts/Faidat-Akinwale.js
+++
@@ -1,5 +0,0 @@
-task = { fullName : "Faidat Akinwale", hngID: "HNG-04059", language : "JavaScript(JS)" };
-
-var details = `Hello world, this is ${task.fullName} with HNGi7 ID ${task.hngID} using ${task.language} for Stage 2 task`;
-
-console.log(details);
