6a44f4520fc78a9926164978cb44118fc54f5f5e
Anefu Favour <favouranefu@gmail.com>
2020-06-02 18:27:52 +0100

    Fix

--- scripts/firstname-lastname.py
+++ scripts/firstname-lastname.py
@@ -1,4 +1,4 @@
 def hello():
-    return "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task"
+    print("Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task")
 
 hello()
