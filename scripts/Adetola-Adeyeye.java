c7ba07a6195ddf860e46ee2ab189960b9b5b53e6
Tola-byte <66223408+Tola-byte@users.noreply.github.com>
2020-06-02 23:08:36 +0100

    Update Adetola-Adeyeye.java

--- scripts/Adetola-Adeyeye.java
+++ scripts/Adetola-Adeyeye.java
@@ -1,1 +1,9 @@
-System.out.println("Hello World, this is Adeyeye Adetola, HNGi7 ID,00876,using Java for stage 2 task");
+class Adetola {
+    public static void main(String[] args) {
+        System.out.println("Hello World, my name is Adetola Adeyeye, my HNGi7 ID is , HNG-00876, and i would be using java for my stage 2 task");
+    }
+}
+
+
+
+
