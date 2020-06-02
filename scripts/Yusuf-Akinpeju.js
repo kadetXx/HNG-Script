872c13fef3d7ab5986767610f531e4be3cad37f5
Thekodezilla <thekodezilla@gmail.com>
2020-06-02 16:01:49 -0700

    Akinpeju Yusuf re-submission

--- scripts/Yusuf-Akinpeju.js
+++ scripts/Yusuf-Akinpeju.js
@@ -8,6 +8,7 @@ function alltext() {
 
   let texts = `Hello World, this is ${user.last_name} ${user.first_name} with HNGi7 ID ${user.ID} using ${user.language} for stage 2 task`;
   let text2 = JSON.stringify(texts);
-  console.log(text2);
+  let noString = eval(text2);
+  console.log(noString);
 }
 alltext();
