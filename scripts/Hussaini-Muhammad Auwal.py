c256090c6823473ea0a385d7833f8aed61fc0731
Hussaini Muhammad Auwal <53785220+blackalbino01@users.noreply.github.com>
2020-06-02 18:39:13 +0100

    Delete Hussaini-Muhammad Auwal.py

--- scripts/Hussaini-Muhammad Auwal.py
+++
@@ -1,6 +0,0 @@
-full_name="Hussaini Muhammad Auwal"
-ID="HNG-01895"
-language="Python"
-def task( ):
-	return "Hello World,this is {0} with HNGi7 ID {1} using {2} for stage 2 task".format(full_name,ID,language)
-print(task())
