c25c38d9dad30aec1f96bf6e787c4f48cd36b888
simidawalang <58118557+simidawalang@users.noreply.github.com>
2020-06-03 00:43:13 +0100

    Update simi-dawalang.py
    Made some changes

--- scripts/simi-dawalang.py
+++ scripts/simi-dawalang.py
@@ -1,9 +1,9 @@
 full_name = "Simi Morayo Da-Walang"
-HNG_ID = "HNG-04578" 
+hng_ID = "HNG-04578" 
 
 """since zero is the first digit, it is best to save it as string.
 Otherwise, it will be omitted by the interpreter"""
 
 lang = "Python"
 
-print("Hello World, this is",full_name,"with HNGi7 ID",HNG_ID,"using",lang,"for stage 2 task.")
+print("Hello World, this is",full_name,"with HNGi7 ID",hng_ID,"using",lang,"for stage 2 task.")
