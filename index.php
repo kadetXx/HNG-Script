14b72702ac203ee6c81f8fa8a5284389b5649952
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-02 20:26:33 +0100

    finished the task

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$json = $_SERVER["QUERY_STRING"];
+$json = $_SERVER["QUERY_STRING"] ?? '';
 
 $files = scandir("scripts/");
 
@@ -11,10 +11,9 @@ $output = [];
 
 foreach($files as $file){
 
-    $extension = explode('.', $file)[1];
+    $extension = explode('.', $file);
 
-
-    switch($extension){
+    switch($extension[1]){
         case 'php':
             $startScript = "php";
             break;
@@ -29,18 +28,50 @@ foreach($files as $file){
     $f = exec($startScript . " scripts/".$file);
 
 
-    $output[] = ["content" => $f, "status" => testFileContent($f)];
+    $output[] = [$f,testFileContent($f), $extension[0]];
 
 }
 
 function testFileContent($string){
     if(preg_match('/^Hello World, this is (([a-zA-Z]+?\s)+)with HNGi7 ID (HNG-[0-9]{5}) using ([a-zA-Z]+?) for stage 2 task$/',trim($string))){
-        return true;
+        return 'Pass';
     }
 
-    return false;
+    return 'Fail';
 }
 
+ob_end_flush();
 
-var_dump($output);
+    if(isset($json) && $json == 'json'){
+
+       echo json_encode($output);
+
+    }else{
+            ?>
+        <html>
+        <body>
+        <h1>Format</h1>
+        <ul>
+
+            <?php
+
+            foreach ($output as $out){
+                echo <<<EOL
+                <li>
+                Text: $out[2] - Status: $out[1]
+                </li>
+EOL;
+            }
+            ?>
+           
+
+        </ul>
+
+        </body>
+
+        </html>
+<?php
+    }
+ob_start();
+?>
 
