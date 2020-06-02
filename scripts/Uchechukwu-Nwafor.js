9fa6769e5202b0ebdc83d08dba3cc5a7c9c680d0
caspero <52088600+caspero-62@users.noreply.github.com>
2020-06-02 18:25:31 +0100

    Update Uchechukwu-Nwafor.js

--- scripts/Uchechukwu-Nwafor.js
+++ scripts/Uchechukwu-Nwafor.js
@@ -1,1 +1,1 @@
-document.write('Hello World, this is Nwafor Uchechukwu with HNGi7 ID HNG-03041 using Javscript for stage 2 task.')
+console.log('Hello World, this is Nwafor Uchechukwu with HNGi7 ID HNG-03041 using Javscript for stage 2 task.')
