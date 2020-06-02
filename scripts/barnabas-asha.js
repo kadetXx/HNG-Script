9424b7757ae874a175a2da4537f1779f57f37580
Barnabas Asha <barnabee58@gmail.com>
2020-06-02 22:43:01 +0100

    update

--- scripts/barnabas-asha.js
+++ scripts/barnabas-asha.js
@@ -7,4 +7,5 @@ const language = 'Javascript';
 const hngProfile = function () {
     return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`
 }
+
 console.log(hngProfile());
