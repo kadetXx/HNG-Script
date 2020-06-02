04d231d6ae94cf72079d762bbf9e759ed8aa252c
Adebare Amos <42787603+Adeinfo@users.noreply.github.com>
2020-06-02 21:35:03 +0100

    Update Adebare-Amos.js

--- scripts/Adebare-Amos.js
+++ scripts/Adebare-Amos.js
@@ -1,2 +1,2 @@
 const HNG = { name: 'Adebare Amos', id: 'HNG-02063', lang:'JavaScript'};
-console.log (`Hello World, this is ${HNG.name} with HNGi7 ${HNG.id} using ${HNG.lang} for stage 2 task");
+console.log (`Hello World, this is ${HNG.name} with HNGi7 ${HNG.id} using ${HNG.lang} for stage 2 task`);
