efb679674bbebf1bea6719e15c02838b80514a84
cugbo <64258165+cugbo@users.noreply.github.com>
2020-06-02 23:37:39 +0100

    Update chima-ugbo.py

--- scripts/chima-ugbo.py
+++ scripts/chima-ugbo.py
@@ -1,1 +1,4 @@
-print("Hello World, this is Chima Ugbo with HNGi7 ID HNG-01119 using Python for stage 2 task")
+name = "Chima Ugbo"
+ID = "HNG-01119"
+language = "Python"
+print("Hello World, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task")
