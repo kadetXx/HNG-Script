a5fb3ac0c460f37ffae373028756443fa65351df
simidawalang <58118557+simidawalang@users.noreply.github.com>
2020-06-03 00:58:16 +0100

    Update simi-dawalang.py

--- scripts/simi-dawalang.py
+++ scripts/simi-dawalang.py
@@ -2,9 +2,9 @@ full_name = "Simi Morayo Da-Walang"
 hng_ID = "HNG-04578" 
 
 """since zero is the first digit, it is best to save it as string.
-Otherwise, it will be omitted by the interpreter, also the it will
-make sure there is no space between 0 and the hyphen."""
+Otherwise, it will be omitted by the interpreter, also it will
+ensure there is no space between 0 and the hyphen."""
 
-lang = "Python"
+language = "Python"
 
-print("Hello World, this is",full_name,"with HNGi7 ID",hng_ID,"using",lang,"for stage 2 task.")
+print("Hello World, this is",full_name,"with HNGi7 ID",hng_ID,"using",language,"for stage 2 task.")
