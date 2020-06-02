c5774b19a29ce2deed2d5bcd7239f53ae85ae099
msoladoye <soladoyeolaOS@gmail.com>
2020-06-02 21:22:09 +0100

    json format

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ $files = scandir("scripts/");
 unset($files[0]);
 unset($files[1]);
 $output = [];
+$outputJSON = [];
 
-foreach($files as $file){
+foreach ($files as $file) {
 
     $extension = explode('.', $file);
 
-    switch($extension[1]){
+    switch ($extension[1]) {
         case 'php':
             $startScript = "php";
             break;
@@ -25,15 +26,20 @@ foreach($files as $file){
             break;
     }
 
-    $f = exec($startScript . " scripts/".$file);
+    $f = exec($startScript . " scripts/" . $file);
 
-
-    $output[] = [$f,testFileContent($f), $extension[0]];
-
+    $data[$extension[0]]->content = $f;
+    $data[$extension[0]]->status = testFileContent($f);
+    $data[$extension[0]]->name = $extension[0];
+    // $intern[$extension[0]] = $data;
+    array_push($outputJSON,$data);
+    // $output;
+    $output[] = [$f, testFileContent($f), $extension[0]];
 }
 
-function testFileContent($string){
-    if(preg_match('/^Hello\sWorld[,|.|!]*\sthis\sis\s[a-zA-Z]{2,}\s[a-zA-Z]{2,}(\s[a-zA-Z]{2,})?\swith\sHNGi7\sID\s(HNG-\d{3,})\susing\s[a-zA-Z]{3,}\sfor\sstage\s2\stask.?$/i',trim($string))){
+function testFileContent($string)
+{
+    if (preg_match('/^Hello\sWorld[,|.|!]*\sthis\sis\s[a-zA-Z]{2,}\s[a-zA-Z]{2,}(\s[a-zA-Z]{2,})?\swith\sHNGi7\sID\s(HNG-\d{3,})\susing\s[a-zA-Z]{3,}\sfor\sstage\s2\stask.?$/i', trim($string))) {
         return 'Pass';
     }
 
@@ -42,20 +48,20 @@ function testFileContent($string){
 
 ob_end_flush();
 
-    if(isset($json) && $json == 'json'){
+if (isset($json) && $json == 'json') {
 
-       echo json_encode($output);
+    echo json_encode($outputJSON);
+} else {
+?>
+    <html>
 
-    }else{
-            ?>
-        <html>
-        <body>
+    <body>
         <h1>Format</h1>
         <ul>
 
             <?php
 
-            foreach ($output as $out){
+            foreach ($output as $out) {
                 $color = $out[1] == 'Pass' ? 'green' : 'red';
                 echo <<<EOL
                 <li>
@@ -64,15 +70,14 @@ ob_end_flush();
 EOL;
             }
             ?>
-           
+
 
         </ul>
 
-        </body>
+    </body>
 
-        </html>
+    </html>
 <?php
-    }
+}
 ob_start();
 ?>
-
