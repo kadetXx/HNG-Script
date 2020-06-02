fa63fc9415d84b80cbf43541b569383384738304
daniel <danielchibuzoigwe@gmail.com>
2020-06-02 21:54:44 +0100

    renamed igwe-daniel.py to daniel-igwe.py and fixed script error

--- scripts/Igwe-Daniel.py
+++
@@ -1,9 +0,0 @@
-name="Igwe Daniel Chibuzo"
-HNG_id="HNG-02682"
-LANG="Python"
-
-def start():
-    print(f"Hello World, this is {name} with HNGi7 ID {HNG_id} using {LANG} for stage 2 task")
-
-if __name__ == '__main__':
-    start()
