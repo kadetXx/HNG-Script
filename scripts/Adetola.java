89d6ca3c05b7b9e08deb22743bd11b351f5bb770
Tola-byte <66223408+Tola-byte@users.noreply.github.com>
2020-06-03 00:20:30 +0100

    Update and rename Adetola-Adeyeye.java to Adetola.java

--- scripts/Adetola.java
+++ scripts/Adetola.java
@@ -1,4 +1,4 @@
-class Adetola {
+public class Adetola {
     public static void main(String[] args) {
         System.out.println("Hello World, my name is Adetola Adeyeye with HNGi7 ID [HNG-00876] using va for age 2 task");
     }
