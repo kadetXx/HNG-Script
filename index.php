4bb936e1c188c390d04b7f907d23d139c24157b9
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-02 20:00:52 +0100

    working test

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
 
-$files = scandir("scripts/");
+$json = $_SERVER["QUERY_STRING"];
 
+$files = scandir("scripts/");
 
 unset($files[0]);
 unset($files[1]);
@@ -21,19 +22,25 @@ foreach($files as $file){
             $startScript = "node";
             break;
         case 'py':
+            $startScript = "python";
             break;
     }
 
     $f = exec($startScript . " scripts/".$file);
 
 
-    $output[] = ['content' => $f, 'status' => testFileContent()];
+    $output[] = ["content" => $f, "status" => testFileContent($f)];
 
 }
 
-function testFileContent(){
-    return 'pass';
+function testFileContent($string){
+    if(preg_match('/^Hello World, this is (([a-zA-Z]+?\s)+)with HNGi7 ID (HNG-[0-9]{5}) using ([a-zA-Z]+?) for stage 2 task$/',trim($string))){
+        return true;
+    }
+
+    return false;
 }
 
 
 var_dump($output);
+
---
+++ scripts/Mohammed-Kabir-Hussaini.php
@@ -0,0 +1,2 @@
+<?php
+echo("Hello World, this is Mohammed Kabir Hussaini with HNGi7 ID HNG-00759 using php for stage 2 task");
