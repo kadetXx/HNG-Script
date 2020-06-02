ec099bccea24286a07703e6d979e55e24e6b2371
daniel <danielchibuzoigwe@gmail.com>
2020-06-02 22:34:01 +0100

    refactored my script

--- scripts/Daniel-Igwe.py
+++ scripts/Daniel-Igwe.py
@@ -1,11 +1,5 @@
 name = "Daniel Igwe"
 HNG_id = "HNG-02682"
 
-
-def start():
-    print(
-        f"Hello World, this is {name} with HNGi7 ID {HNG_id} using python for stage 2 task")
-
-
-if __name__ == '__main__':
-    start()
+print(
+    "Hello World, this is {} with HNGi7 ID {} using Python for stage 2 task".format(name, HNG_id))
