ddc4acd1f3df551e832a6e389fd1a7ec6650258f
Samuel377 <samuelsholademi37@gmail.com>
2020-06-02 22:34:53 +0100

    updated

--- scripts/samuel-Sholademi.php
+++ scripts/samuel-Sholademi.php
@@ -1,8 +1,6 @@
 
 <?php
-    echo "Hello World, this is Sholademi Samuel with HNGi7 ID HNG-02065 using PHP for stage 2 task";
+    echo ("Hello World, this is Sholademi Samuel with HNGi7 ID HNG-02065 using php for stage 2 task");
 ?>
 
-My name is Sholademi Samuel;
-My HNG idenitification number is HNG-02065;
-Written in : PHP programming language;
+
