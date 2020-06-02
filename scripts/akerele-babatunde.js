b6981e6681f8a119d493394ef3e96e1a9d264165
Machine <babsake@gmail.com>
2020-06-02 23:16:40 +0100

    Add introduction(fix2)

--- scripts/akerele-babatunde.js
+++ scripts/akerele-babatunde.js
@@ -1,5 +1,5 @@
 const introduction = () => {
-  return 'Hello World, this is Akerele Babatunde with HNGi7 ID HNG-04839 using Javscript for stage 2 task';
+  console.log('Hello World, this is Akerele Babatunde with HNGi7 ID HNG-04839 using Javscript for stage 2 task');
 };
 
-console.log(introduction());
+introduction();
