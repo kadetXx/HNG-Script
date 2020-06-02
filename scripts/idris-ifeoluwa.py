a40e3285662eb0c3b3388244eecb1eb64caf8833
crooz <bobbycrooz@users.noreply.github.com>
2020-06-02 22:38:25 +0100

    idisIfe added a file

---
+++ scripts/idris-ifeoluwa.py
@@ -0,0 +1,8 @@
+#task assignment to output a particular sentence indidcating name id and language used
+
+def message(fullname,Id,language):
+     print("Hello world!,this is {} with HNGi7 ID:{} using {}.".format(fullname,Id,language))
+
+
+
+message('Idris Ifeoluwa','HGN-01480','Python')
