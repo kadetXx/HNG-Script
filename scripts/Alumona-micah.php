dd0820fd0810f2c7172c0e5e26a8561df5627984
mike-ske <66280654+mike-ske@users.noreply.github.com>
2020-06-02 23:26:03 +0100

    Update Alumona-micah.php

--- scripts/Alumona-micah.php
+++ scripts/Alumona-micah.php
@@ -5,6 +5,6 @@ $_id = "HNC-04840";
 $_lang = "php";
 
 echo "Hello world, this is ".$_full_name." with 
-HNGi7 ID ".$_id." using ".$_lang." for stage 2 task.";
+HNGi7 ID ".$_id." using ".$_lang." for stage 2 task";
 
 ?>
