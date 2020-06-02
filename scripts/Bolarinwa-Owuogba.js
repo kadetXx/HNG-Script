e130eebc85623de0a232ebfe3978a2958b34a9e1
bhorlarh <bhorlarinwah@gmail.com>
2020-06-02 21:30:43 +0100

    added my own script

---
+++ scripts/Bolarinwa-Owuogba.js
@@ -0,0 +1,7 @@
+// declare user variables
+const fullName = "Bolarinwa Owuogba";
+const myHNGID = "HNG-04092";
+const scriptingLanguage = "JavaScript";
+
+// send output to console
+console.log("Hello World, this is", fullName, "with HNGi7 ID", myHNGID, "using", scriptingLanguage, "for stage 2 task");
