ab0e382be4e44d89e780058bcb32e1a0ff4b4848
simidawalang <58118557+simidawalang@users.noreply.github.com>
2020-06-03 00:42:06 +0100

    Update simi-dawalang.py
    Made some changes, allocated variables to name, ID and language

--- scripts/simi-dawalang.py
+++ scripts/simi-dawalang.py
@@ -1,1 +1,9 @@
-print('Hello World, this is Simi Morayo Da-Walang with HNGi7 ID HNG-04578 using Python for stage 2 task.')
+full_name = "Simi Morayo Da-Walang"
+HNG_ID = "HNG-04578" 
+
+"""since zero is the first digit, it is best to save it as string.
+Otherwise, it will be omitted by the interpreter"""
+
+lang = "Python"
+
+print("Hello World, this is",full_name,"with HNGi7 ID",HNG_ID,"using",lang,"for stage 2 task.")
