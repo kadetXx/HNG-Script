baf7880eb216fd969f3db28052a3e1e3e7920d91
Kamikaze49 <44038301+Kamikaze49@users.noreply.github.com>
2020-06-02 17:58:57 +0000

    Add files via upload

---
+++ scripts/edwin-kayang.js
@@ -0,0 +1,5 @@
+var name = "Edwin Kayang";
+var ID = "HNG-00019";
+var language = "Javascript";
+
+document.write(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task`);
