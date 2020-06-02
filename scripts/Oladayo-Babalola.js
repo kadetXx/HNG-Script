ee2a309634d9d8b874a23f1967ca060d616dd61c
Cooncudee <66222345+cooncudee@users.noreply.github.com>
2020-06-02 20:12:57 +0100

    Update Oladayo-Babalola.js
    Refined

--- scripts/Oladayo-Babalola.js
+++ scripts/Oladayo-Babalola.js
@@ -1,1 +1,7 @@
-console.log("Hello World, this is Oladayo Babalola with HNGi7 ID HNG-00626 using JavaScript for stage 2 task")
+const username = {
+  name: "Oladayo Babalola",
+  id: "HNG-00626",
+  language: "JavaScript"
+};
+const literal = `Hello World, this is ${username.name} with HNGi7 ID ${username.id} using ${username.language} for stage 2 task.`;
+console.log(literal);
