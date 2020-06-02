5dcda84179f73d83063e53ec1b3091b2eb157cf5
JemimaNaomiBen <jemmycutiee@gmail.com>
2020-06-02 20:54:54 +0100

    Task2 commit

--- scripts/jemimanaomi-ben.php
+++ scripts/jemimanaomi-ben.php
@@ -1,5 +1,5 @@
 <?php
 $string1 = "Hello World, this is Jemima-Naomi Godwin Ben with HNGi7 ID";
 $string2 = "HNG-02526 using php for stage 2 task.";
-echo $string1 . " " . string2;
+echo $string1 . " " . $string2;
 ?>
