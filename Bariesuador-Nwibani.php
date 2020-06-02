48220ffd22123117fbe722d886d7784d77db0679
Nwibani Harmony <esuador@gmail.com>
2020-06-02 17:25:49 +0100

    Add files via upload

---
+++ Bariesuador-Nwibani.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "Hello World, this is Bariesuador Harmony Nwibani with HNGi7 ID HNG-00046 using php for stage 2 task";
+?>
+</body>
+</html>
