7d4cfe5bd41131c08236fd34c66998f2145b07d5
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-02 17:48:16 +0100

    Change HNG ID

--- scripts/faithfulness-alamu.go
+++ scripts/faithfulness-alamu.go
@@ -6,7 +6,7 @@ import "fmt"
 const (
 	outformat = "Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task\n"
 	name      = "Faithfulness Alamu"
-	id        = "thealamu"
+	id        = "HNG-02407"
 	language  = "golang"
 )
 
