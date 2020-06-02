5dff993f0f8baf270204ddee570b987284b927b6
MarlineKhavele <marlinekhavele6@gmail.com>
2020-06-02 22:27:54 +0300

    first commit

---
+++ scripts/marline-khavele.py
@@ -0,0 +1,10 @@
+# stage 2 task
+
+name = "Marline Khavele"
+id = "HNG-04957"
+language = "python"
+
+print(
+    f"Hello world, This is {name } with HNGi7 {id} using {language} for stage 2 task."
+)
+
