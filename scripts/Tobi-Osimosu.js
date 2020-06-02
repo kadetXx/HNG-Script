84c4e03de52009ece816b9113f7e3d26f2bdb899
Tobi Osimosu <osimosutobi@gmail.com>
2020-06-02 22:12:18 +0100

    updated Tobi-Osimosu

--- scripts/Tobi-Osimosu.js
+++ scripts/Tobi-Osimosu.js
@@ -3,8 +3,8 @@
 
 output = (lastName, firstName, HNGID, language) => {
   console.log(
-    `Hello World, this is ${lastName} ${firstName} with HNGi7 ID HNG-${HNGID} using ${language} for stage 2 task`
+    `Hello World, this is ${lastName} ${firstName} ${middleName} with HNGi7 ID HNG-${HNGID} using ${language} for stage 2 task`
   );
 };
 
-output("Osimosu", "Oluwatobiloba", "01133", "JavaScript");
+output("Osimosu", "Oluwatobiloba", "James", "01133", "JavaScript");
