6ba08aceeea7e7877903f35779522736f05ef7ae
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-02 18:13:17 +0100

    working on the script - rough

--- index.php
+++ index.php
@@ -1,2 +1,39 @@
 <?php
 
+
+$files = scandir("scripts/");
+
+
+unset($files[0]);
+unset($files[1]);
+$output = [];
+
+foreach($files as $file){
+
+    $extension = explode('.', $file)[1];
+
+
+    switch($extension){
+        case 'php':
+            $startScript = "php";
+            break;
+        case 'js':
+            $startScript = "node";
+            break;
+        case 'py':
+            break;
+    }
+
+    $f = exec($startScript . " scripts/".$file);
+
+
+    $output[] = ['content' => $f, 'status' => testFileContent()];
+
+}
+
+function testFileContent(){
+    return 'pass';
+}
+
+
+var_dump($output);
