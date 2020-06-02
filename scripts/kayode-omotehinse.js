c922991be7224b23a392491d63335b7b8aca7c58
oluwalakez <kayodeomotehinse@gmail.com>
2020-06-02 23:40:33 +0100

    Update kayode-omotehinse.js

--- scripts/kayode-omotehinse.js
+++ scripts/kayode-omotehinse.js
@@ -3,4 +3,4 @@
   let id =  'HNG-04498'
   let language = 'Javascript'
 
-  console.log('Hello world, this is ' + name + ' ' + 'with HNG ID ' + id + ' ' + 'using ' + language + ' ' + 'for stage 2 task.')
+  console.log('Hello world, this is ' + name + ' ' + 'with HNGi7 ID ' + id + ' ' + 'using ' + language + ' ' + 'for stage 2 task.')
