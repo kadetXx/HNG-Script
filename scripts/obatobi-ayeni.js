43d8de92ac310e8eb96a3e819b53afbc74773edd
weedydash <57416736+weedydash@users.noreply.github.com>
2020-06-02 18:34:06 +0100

    Update obatobi-ayeni.js

--- scripts/obatobi-ayeni.js
+++ scripts/obatobi-ayeni.js
@@ -1,1 +1,1 @@
-Console.log("Hello World, this is Obatobi Ayeni with HNGi7 HNG-04196 using JavaScript for stage 2 task")
+Console.log("Hello World, this is Obatobi Ayeni with HNGi7 ID HNG-04196 using JavaScript for stage 2 task")
