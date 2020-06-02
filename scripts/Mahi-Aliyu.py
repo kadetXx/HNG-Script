6d3460a54be4599f0269979d67d06c9ffd8fcb3a
A-l-Mahi <mahigital@gmail.com>
2020-06-02 22:05:22 +0100

    Mahi-Aliyu.py Uploaded

---
+++ scripts/Mahi-Aliyu.py
@@ -0,0 +1,9 @@
+'''The goal of this script should  returns the text "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task". Otherwise it fails'''
+#storing info to dict data type
+info = {'Full_name':"Mahi Aminu Aliyu", 'HNGi7_ID':"HNG-01114",'Language':"Python"}
+
+#my info from the dict value
+print(f"\tHello World, this is {info['Full_name']} with HGNi7 ID{info['HNGi7_ID']} using {info['Language']}")
+
+
+
