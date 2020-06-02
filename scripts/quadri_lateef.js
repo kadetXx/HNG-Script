df51eae89b212d4da0a028a7275f1f05c694911f
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2020-06-02 17:17:04 +0100

    Update quadri_lateef.js

--- scripts/quadri_lateef.js
+++ scripts/quadri_lateef.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Lateef Quadri Olayinka with HNGi7 ID HNG-02622 using JavaScript for stage 2 task")
+document.write("Hello World, this is Lateef Quadri Olayinka with HNGi7 ID HNG-02622 using JavaScript for stage 2 task")
