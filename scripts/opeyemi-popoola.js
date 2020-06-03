d2e71557b475507524ed8049059573037e18cea5
Opeyemi Popoola <popstarbaed@gmail.com>
2020-06-03 12:50:11 +0100

    added myscript opeyemi-popoola.js to the scripts folder for test

---
+++ scripts/opeyemi-popoola.js
@@ -0,0 +1,8 @@
+let result, printResult  // variable declaration
+
+// function declaration
+printResult = (prop) => `Hello World, this is ${prop.firstname} ${prop.lastname} with HNGi7 ID ${prop.ID} using Javascript for stage 2 task`
+
+result = printResult({ ID: "HNG-06043", firstname: "opeyemi", lastname: "popoola", dev_lang: "Javascript" })
+
+console.log(result)
