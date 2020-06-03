94bedb54f73cbc9ab236a57253d762c4470c2c7d
Styne001 <macstine@gmail.com>
2020-06-03 07:02:45 +0100

    Edited 2nd time celestine-okonkwo.py

--- scripts/celestine-okonkwo.py
+++ scripts/celestine-okonkwo.py
@@ -3,4 +3,4 @@ name = "Okonkwo Celestine"
 id = "HNG-00342"
 lang = "Python"
 task = "stage 2 task"
-print("Hello World, this is " + name + " with HNGi7 ID-" + id + " using " + lang + " for " + task)
+print("Hello World, this is " + name + " with HNGi7 ID" + id + " using " + lang + " for " + task)
