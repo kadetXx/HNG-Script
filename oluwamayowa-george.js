61c1ba4eff45d697e1b8c2fbb320905b9085aaa5
mayowana <themayowageorge@gmail.com>
2020-06-03 17:32:11 +0100

    Create oluwamayowa-george.js
    Created script file as instructed.

---
+++ oluwamayowa-george.js
@@ -0,0 +1,9 @@
+const fullName = "Oluwamayowa George";
+const ID = "HNG-00461";
+const language = "Javascript";
+
+function intro() {
+    return `Hello world, this is ${fullName }, with  HNGi7 ID: ${ID} using ${language } for Stage 2 task`
+}
+
+console.log(intro())
