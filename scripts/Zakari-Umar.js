b40954de5f470d99c1a7429b9da2bc6784255165
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-06-03 18:46:15 +0100

    Update Zakari-Umar.js
    Correction to capital letter

--- scripts/Zakari-Umar.js
+++ scripts/Zakari-Umar.js
@@ -1,1 +1,1 @@
-console.log("hello world, this is Umar Muhammad Zakari with HNGi7 ID HNG-01897 and email umarfarouqft@gmail.com using python for stage 2 task")
+print("Hello world, this is Umar Muhammad Zakari with HNGi7 ID HNG-01897 and email umarfarouqft@gmail.com using python for stage 2 task")
