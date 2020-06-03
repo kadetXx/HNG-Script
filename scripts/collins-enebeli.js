f6b3bbd5461c6321cad16eac111e72cdb3420602
Kadet <techkadet@gmail.com>
2020-06-03 10:17:46 +0100

    script submission for kadet

--- scripts/collins-enebeli.js
+++ scripts/collins-enebeli.js
@@ -2,7 +2,8 @@ function hngTask() {
   const intern = {
     fullName: 'Collins Enebeli',
     id: 'HNG-05252',
-    language: 'Javascript'
+    language: 'Javascript',
+    email: 'collynizy@gmail.com'
   };
 
   const output = `Hello World, this is ${intern.fullName} with HNGi7 ID ${intern.id} using ${intern.language} for stage 2 task`;
