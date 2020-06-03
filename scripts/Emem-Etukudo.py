c2e375adb45d13c9aa161ef5723a9ac0ef8b6bda
Emem <saspiee@gmail.com>
2020-06-03 18:21:05 +0100

    outputing my info

---
+++ scripts/Emem-Etukudo.py
@@ -0,0 +1,8 @@
+def print_myinfo():
+    print('Hello world, this is Emem Lawrence Etukudo',
+    'with HNG ID HNG-02217 and',
+    'email saspiee@gmail.com using python for',
+    'stage 2 task.')
+
+
+print_myinfo()
