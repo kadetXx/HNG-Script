8838694ba98d7fcb2961630a794bc1c04a6ae023
Dayvvo <45407706+Dayvvo@users.noreply.github.com>
2020-06-03 17:36:00 +0100

    Update Opeyemi-Adeyemi.js

--- scripts/Opeyemi-Adeyemi.js
+++ scripts/Opeyemi-Adeyemi.js
@@ -2,6 +2,6 @@
 
 
 (function (name="Adeyemi Opeyemi David",id="HNG-03135",language="Javascript", email="dyvvoo@gmail.com") {
-    document.write('Hello World, this is ' +name+ ' with HNGi7 ID ' +id+ ' and email ' +email+ ' using language ' +language+ ' for stage 2 task ')
+    console.log('Hello World, this is ' +name+ ' with HNGi7 ID ' +id+ ' and email ' +email+ ' using language ' +language+ ' for stage 2 task ')
 
  })()
