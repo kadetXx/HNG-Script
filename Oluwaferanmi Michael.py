d4101ee4a4f3a0a5780e3e15aa4a2cc8cc0b540b
Oluwaferanmi-Michael <54253877+Oluwaferanmi-Michael@users.noreply.github.com>
2020-06-03 22:07:39 +0100

    Delete Oluwaferanmi Michael.py

--- Oluwaferanmi Michael.py
+++
@@ -1,7 +0,0 @@
-#HNGi7 Task 2
-
-name = "Oluwaferanmi Michael"
-ID = "HNG-02909"
-dev_language = "Python"
-
-print("Hello World, this is {name} with HNGi7 ID {ID} using {dev_language} for stage 2 task")
