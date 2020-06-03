d47634d925e1282ea6887be5bbc35094b4dd7049
Bernard Arhia <55264725+everich4040@users.noreply.github.com>
2020-06-03 04:59:22 -0700

    email-added
    I updated the script and added my email

--- scripts/Bernard-Arhia.js
+++ scripts/Bernard-Arhia.js
@@ -4,7 +4,7 @@ displayInfo = () => {
 	const hndId = "HNG-00347";
 	const languageChoice = "Javascript";
 
-	output = `Hello world, this is ${fullName} with HNGi7 ID ${hndId} using ${languageChoice} for stage 2 task`;
+	output = `Hello world, this is ${fullName} with HNGi7 ID ${hndId} and email bernardarhia@gmail.com using ${languageChoice} for stage 2 task`;
 	return output;
 };
 
