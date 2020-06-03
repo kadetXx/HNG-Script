05881bc874ebfce49fe16f90d25056c3a9ad35cd
BolaofLagos <abolarinwakazeem@gmail.com>
2020-06-03 14:34:18 +0100

    Add bolarinwa-kazeem.js

--- scripts/bolarinwa-kazeem.js
+++ scripts/bolarinwa-kazeem.js
@@ -1,12 +1,15 @@
 let hngIntern = {
     fullName : 'Bolarinwa Kazeem',
     id : 'HNG-00305',
+    email: 'bola@reliancehmo.com',
     language : 'JavaScript',
     outputString : function(){
-        console.log('Hello World, this is ' + this.fullName + ' with HNGi7 ID ' + this.id + ' using ' + this.language + ' for stage 2 task'); 
+        console.log('Hello World, this is ' + this.fullName + ' with HNGi7 ID ' + this.id + ' and email ' + this.email + ' using ' + this.language + ' for stage 2 task'); 
     }
 }
 
 
 
 hngIntern.outputString();
+
+//Hello World, this is Elisha Simeon Ukpong Udoh with HNGi7 ID HNG-01827 and email simeon.udoh45@gmail.com using python for stage 2 task
