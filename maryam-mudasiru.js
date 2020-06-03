a8d5df505f4cf6f1ac3a7b7400551db6209ca4f1
Anita9771 <66222985+Anita9771@users.noreply.github.com>
2020-06-03 07:32:30 +0100

    Anita-Idemudia.js
    #Team Sentry

--- maryam-mudasiru.js
+++ maryam-mudasiru.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is [Idemudia Anita Eghonghon] with HNGi7 ID [HNG-00847] using [Javascript] for stage 2 task".)
+console.log("Hello World, this is [Idemudia Anita] with HNGi7 ID [HNG-00847] using [Javascript] for stage 2 task".)
