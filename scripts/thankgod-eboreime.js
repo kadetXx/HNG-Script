efdb50b1920a90d408096b4fe533702b06d5bc41
Tk-brainCodes <54640421+Tk-brainCodes@users.noreply.github.com>
2020-06-03 12:12:16 +0100

    refactored my code from Eboreime ThankGod

--- scripts/thankgod-eboreime.js
+++ scripts/thankgod-eboreime.js
@@ -1,9 +1,9 @@
-myMessage = () => {
+const myMessage = () => {
     let details = {
         name: 'Eboreime ThankGod',
         hngi7Id: '02109',
         language: 'javascript' 
     }
-    return `Hello world this is ${details.name} with HNGI7 ID ${details.hngi7Id} using ${language} for stage 2 task`
+    return `Hello World, this is ${details.name} with HNGI7 ID ${details.hngi7Id} using ${details.language} for stage 2 task`
 }
 console.log(myMessage());
