3b4b11a42a4389467a5551e322fdef92a74b6228
UnsignedHype <66218402+UnsignedHype@users.noreply.github.com>
2020-06-03 10:06:31 +0100

    my task1 team submission

---
+++ scripts/olamide-davids.js
@@ -0,0 +1,7 @@
+//Code to print statement required
+
+    function UnsignedHype (){
+        console.log("Hello World, this is Olamide Davids with HNGi7 ID HNG-01138 using Javascript for stage 2 task")
+    }
+
+UnsignedHype();
