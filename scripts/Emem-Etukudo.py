8c5c96f02f05b287c0884a0ff133477058514725
Emem <saspiee@gmail.com>
2020-06-03 18:39:45 +0100

    removed fullstop as informed

--- scripts/Emem-Etukudo.py
+++ scripts/Emem-Etukudo.py
@@ -2,7 +2,7 @@ def print_myinfo():
     print('Hello world, this is Emem Lawrence Etukudo',
     'with HNG ID HNG-02217 and',
     'email saspiee@gmail.com using python for',
-    'stage 2 task.')
+    'stage 2 task')
 
 
 print_myinfo()
