f0a7a52b5a75a9fd800d5f07ec9c1ac0bec2018c
Emmanuel Erasmus <2016wealthtips@gmail.com>
2020-06-03 15:22:05 +0100

    Created by Erasmus

---
+++ scripts/Emmanuel-Erasmus-Sulai.js
@@ -0,0 +1,5 @@
+var fullname = "Emmanuel Erasmus Sulai";
+var HNGi7_ID = "HNG-01104";
+var LanguageUsed = "Javascript";
+var myMessage = "Hello world, this is " + fullname + " with HNGi7 ID " + HNGi7_ID + " using " + LanguageUsed + " for stage 2 task.";
+console.log(myMessage)
