edfcdbe23c4cedc7d2740f30683a9fe684b0427d
raraku <airenov500@gmail.com>
2020-06-03 12:16:02 +0100

    added email

--- scripts/Israel-Airenovboise.py
+++ scripts/Israel-Airenovboise.py
@@ -3,8 +3,8 @@
 # HNG-05846
 
 print(
-    "Hello World, this is {} with HNGi7 ID {} using python for stage 2 task".format(
-        "Airenovboise Israel", "HNG-05846"
+    "Hello World, this is {} with HNGi7 ID {} and email {} using python for stage 2 task".format(
+        "Airenovboise Israel", "HNG-05846", "airenov500@gmail.com"
     ),
     flush=True,
 )
