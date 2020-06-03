caf2ebf9f5f42cd53e8da2313328e6f138138dfb
Ephraim Omenai <eomenaiofficial@gmail.com>
2020-06-03 20:32:38 +0100

    Added my email

--- scripts/ephraim-omenai.js
+++ scripts/ephraim-omenai.js
@@ -1,3 +1,8 @@
-const MyData = `Hello World this is Ephraim Omenai with HNGi7 ID HNG-02742 using JavaScript for stage 2 task`;
+let MyName = "Ephraim Omenai";
+let MyId = "HNG-02742";
+let MyEmail = "eomenaiofficial@gmail.com";
+let myLang = "JavaScript";
+
+const MyData = `Hello World, this is ${MyName} with HNGi7 ID ${MyId} and email ${MyEmail} using ${myLang} for stage 2 task`;
 
 console.log(MyData);
