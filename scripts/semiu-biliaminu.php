4edc096055020c27bab41543a7037b096c1c71c6
trustidkid <yemi.bili07@gmail.com>
2020-06-03 11:59:47 +0100

    added email

--- scripts/semiu-biliaminu.php
+++ scripts/semiu-biliaminu.php
@@ -1,1 +1,16 @@
-<?php echo "Hello World, this is semiu biliaminu with HNGi7 ID HNG-04209 using php for stage 2 task"; ?>
+<?php 
+
+    $fullname = "Semiu Biliaminu";
+    $jsonObject = [
+        file => "semiu-biliaminu.php",
+        output => "Hello World, this is semiu biliaminu with HNGi7 ID HNG-04209 using php for stage 2 task",
+        name => "Semiu Biliaminu",
+        id => "HNG-04209",
+        email => "codedash07@gmail.com",
+        language => "PHP",
+        status => "Pass"
+    ];
+
+    echo $jsonObject['output'];// "Hello World, this is semiu biliaminu with HNGi7 ID HNG-04209 using php for stage 2 task"; 
+
+?>
