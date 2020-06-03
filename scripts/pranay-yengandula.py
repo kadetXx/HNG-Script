6b124f5033cd06785fb85d1cbf9a1afbd4ce62f8
Pranay Yenagandula <Pranay41@github.com>
2020-06-03 15:30:58 +0530

    changes

--- scripts/pranay-yengandula.py
+++
@@ -1,7 +0,0 @@
-#Pranay-Yenagandula 
-
-class display_string:
-    def __init__(self):
-        print("Hello World, this is Pranay Yenagandula with HNGi7 ID HNG-01542 using Python for stage 2 task")
-
-d=display_string()
