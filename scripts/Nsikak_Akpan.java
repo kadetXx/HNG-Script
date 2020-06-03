ec8ba5b5da5fdd43bbcf6087fd773c0ed13e2aef
Tonyalex <tonyalex@gmail.com>
2020-06-03 21:57:14 +0100

    Nsikak_Akpan.java

--- scripts/Nsikak_Akpan.java
+++ scripts/Nsikak_Akpan.java
@@ -1,9 +1,7 @@
-package com.example.java;
-
 public class Nsikak_Akpan {
 
         public static void main(String[] args) {
-        System.out.println("Hello World, my name is Nsikak Akpan, with HNGi7 ID  HNG-01250 and email tonyalex6258@gmail.com and i would be using java for my stage 2 task");
+        System.out.println("Hello World, this is Nsikak_Akpan with HNGi7 ID HNG-01250 and email tonyalex6258@gmail.com using java for my stage 2 task");
             }
 
 
