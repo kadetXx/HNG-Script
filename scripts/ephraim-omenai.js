93b0066282f9eadd9c8bc006d74c191757756046
Ephraim Omenai <eomenaiofficial@gmail.com>
2020-06-03 21:13:33 +0100

    Added email and made correction

--- scripts/ephraim-omenai.js
+++ scripts/ephraim-omenai.js
@@ -1,8 +1,8 @@
 let MyName = "Ephraim Omenai";
 let MyId = "HNG-02742";
 let MyEmail = "eomenaiofficial@gmail.com";
-let myLang = "JavaScript";
+let MyLang = "JavaScript";
 
-const MyData = `Hello World, this is ${MyName} with HNGi7 ID ${MyId} and email ${MyEmail} using ${myLang} for stage 2 task`;
+const MyData = `Hello World, this is ${MyName} with HNGi7 ID ${MyId} and email ${MyEmail} using ${MyLang} for stage 2 task`;
 
 console.log(MyData);
