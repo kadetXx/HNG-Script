5125644bb3e2d18686ad82a70e58283776c1d185
Daniel Adeneye <57042077+Dahnie@users.noreply.github.com>
2020-06-03 13:40:16 +0100

    modify syntax

--- scripts/Daniel-Adeneye.js
+++ scripts/Daniel-Adeneye.js
@@ -3,7 +3,7 @@ const STATUS = {
     "lastName": "Adeneye",
     "language": "Javascript",
     "id": "HNG-02304",
-    "email": "adeneyedaniel007@gmail.com"
+    "email" : "adeneyedaniel007@gmail.com"
 };
 
 const RESULT = () => `Hello World, this is ${STATUS.firstName} ${STATUS.lastName} with HNGi7 ID ${STATUS.id} and email ${STATUS.email} using ${STATUS.language} for stage 2 task`;
