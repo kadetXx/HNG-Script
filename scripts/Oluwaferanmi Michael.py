102c287414b6d8d254d8029f9ec373f2efa27055
Oluwaferanmi-Michael <54253877+Oluwaferanmi-Michael@users.noreply.github.com>
2020-06-03 23:54:18 +0100

    Update Oluwaferanmi Michael.py

--- scripts/Oluwaferanmi Michael.py
+++ scripts/Oluwaferanmi Michael.py
@@ -3,5 +3,6 @@
 name = "Oluwaferanmi Michael"
 ID = "HNG-02909"
 dev_language = "Python"
+email = "aechealgr8@gmail.com"
 
 print("Hello World, this is {name} with HNGi7 ID {ID} using {dev_language} for stage 2 task")
