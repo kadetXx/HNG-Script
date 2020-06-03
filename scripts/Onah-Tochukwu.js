8aaffd093e1be03e569de3dc82ff4aafdbe60ba2
tchytech <65729360+tchytech@users.noreply.github.com>
2020-06-03 21:24:04 +0100

    Update Onah-Tochukwu.js
    typo error

--- scripts/Onah-Tochukwu.js
+++ scripts/Onah-Tochukwu.js
@@ -2,5 +2,5 @@
 let fullname = "Onah Tochukwu";
 let id =  "HNG-02371";
 let language = "JavaScript";
-let email = "pearlpey1@gmail.com"
+let email = "pearlpey1@gmail.com";
 console.log("Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email + " using " + language + " stage 2 task ");
