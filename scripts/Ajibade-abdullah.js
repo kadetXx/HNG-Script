701efdb1ed44fc9c3f1514afb9245ec40e8bcce5
ajibadeabd <ajibadeabd@gmail.com>
2020-06-03 12:32:27 +0100

    added my profile

--- scripts/Ajibade-abdullah.js
+++ scripts/Ajibade-abdullah.js
@@ -2,9 +2,10 @@ let  outPut = ()=> {
     let profile ={
       fullName: "Ajibade abdullah",
       hngId: "HNG-03951",
-      Language: "NodeJs"
+      Language: "NodeJs",
+      email:'ajibadeabd@gmail.com'
     }
-  return `Hello World, this is ${profile.fullName} with HNGi7 ID ${profile.hngId} using ${profile.Language} for stage 2 task`;
+  return `Hello World, this is ${profile.fullName} with HNGi7 ID ${profile.hngId} email ${profile.email} using ${profile.Language} for stage 2 task`;
   }
   console.log(outPut());
   
