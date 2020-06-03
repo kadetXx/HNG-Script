3d424ca581fac9c0824092edeff2bc271b9ceb79
MAC <mac@MACs-MacBook-Pro.local>
2020-06-03 18:37:30 +0100

    commit

--- scripts/tola-shobowale.php
+++ scripts/tola-shobowale.php
@@ -5,6 +5,6 @@ $id = "HNG-00272";
 $language = "PHP";
 $email = 'shobowaletola@gmail.com';
 
-print("Hello world,this is {$fullname} with HNGi7 ID {$id} using {$language} for stage 2 task {$email}") ;
+print("Hello world, this is {$fullname} with HNGi7 ID {$id} using {$language} for stage 2 task {$email}") ;
 
 ?>
