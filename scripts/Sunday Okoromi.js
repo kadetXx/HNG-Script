bd35dea766d292f96495110f8100229438e574ab
KingVics <54664965+KingVics@users.noreply.github.com>
2020-06-03 10:08:19 +0100

    Update Sunday Okoromi.js

--- scripts/Sunday Okoromi.js
+++ scripts/Sunday Okoromi.js
@@ -1,6 +1,6 @@
 let profile = {firstName: "Sunday", lastName:"Okoromi", hngId: "HNG-03987", language: "Javascript",
   fullName: function() {
-    return  "Hello World, " + "this is " + this.firstName + " " + this.lastName + " with " + this.hngId + " using " + this.language + " for stage 2 task";
+    return  "Hello World, " + "this is " + this.firstName + " " + this.lastName + " with HNGi7 ID " + this.hngId + " using " + this.language + " for stage 2 task";
 }
 
 }; 
