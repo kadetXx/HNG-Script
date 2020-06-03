0dbf8eaaeb8272d733180058f2530ddcf1e5bb26
starday01 <33865418+starday01@users.noreply.github.com>
2020-06-03 07:51:46 +0100

    Update sunday-orimoyegun.js

--- scripts/sunday-orimoyegun.js
+++ scripts/sunday-orimoyegun.js
@@ -1,1 +1,1 @@
-﻿console.log('Hello World, this is Orimoyegun Sunday with HNGi7 ID HNG - 01400 using Javascript for stage 2 task');
+﻿console.log('Hello World, this is Orimoyegun Sunday with HNGi7 ID HNG-01400 using Javascript for stage 2 task');
