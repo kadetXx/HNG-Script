9ba1e770d64a313d9b6d820fdce25b6407abd206
Boluwatife2904 <sanvicola2000@gmail.com>
2020-06-03 17:04:54 +0100

    added email to the output

--- scripts/sanusi-victor.js
+++ scripts/sanusi-victor.js
@@ -1,6 +1,7 @@
 var myFullName = "Sanusi Victor";
 var myHNGID = "HNG-03071";
 var languageUsed = "JavaScript";
-var expectedMessage = `Hello World, this is ${myFullName} with HNGi7 ID ${myHNGID} using ${languageUsed} for stage 2 task`;
+var myEmailAddress = "sanvicola2000@gmail.com"
+var expectedMessage = `Hello World, this is ${myFullName} with HNGi7 ID ${myHNGID} and email ${myEmailAddress} using ${languageUsed} for stage 2 task`;
 
 console.log(expectedMessage)
