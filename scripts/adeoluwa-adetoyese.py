17fde77cd048e914aef0ff35338548b8297f037c
AdeOluwa <adetoyeseadeoluwa@gmail.com>
2020-06-03 15:57:25 +0100

    added email

--- scripts/adeoluwa-adetoyese.py
+++ scripts/adeoluwa-adetoyese.py
@@ -1,1 +1,5 @@
-print("Hello World, this is Adeoluwa Adetoyese with HNGi7 ID HNG-04248 using Python for stage 2 task")
+# Name = Adeoluwa Adetoyese
+# ID = HNG-04248
+# email = adetoyeseadeoluwa@gmail.com
+
+print("Hello World, this is Adeoluwa Adetoyese with HNGi7 ID HNG-04248 and email adetoyeseadeoluwa@gmail.com using Python for stage 2 task")
