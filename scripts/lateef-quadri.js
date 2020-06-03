bf081b601540b0f22a48f2757d173682102927fe
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2020-06-03 12:52:07 +0100

    Email added

--- scripts/lateef-quadri.js
+++ scripts/lateef-quadri.js
@@ -1,1 +1,1 @@
-document.write("Hello World, this is Lateef Quadri Olayinka with HNGi7 ID HNG-02622 using JavaScript for stage 2 task")
+console.log("Hello World, this is Lateef Quadri Olayinka with HNGi7 ID HNG-02622 and email lateef9816@gmail.com using JavaScript for stage 2 task")
