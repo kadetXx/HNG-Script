d4555bd6f50cac8f8b8512d154805afceb58d795
Jules-Renaud <julesrenaud10@gmail.com>
2020-06-03 13:29:48 +0100

    add email to the output string

--- scripts/jules-tjahe.py
+++ scripts/jules-tjahe.py
@@ -1,6 +1,7 @@
-def sayHello(myName,myId,myLanguage):
-    print("Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
-    .format(name = myName, id = myId, language = myLanguage))
+def sayHello(myName,myId,myLanguage, myEmail):
+    print("Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task"
+    .format(name = myName, id = myId, language = myLanguage, email = myEmail))
     return
 
-sayHello("Tjahe Essomba Jules Renaud","HNG-01452","python")
+sayHello("Tjahe Essomba Jules Renaud","HNG-01452","python","julesrenaud10@gmail.com")
+#Hello World, this is Elisha Simeon Ukpong Udoh with HNGi7 ID HNG-01827 and email simeon.udoh45@gmail.com using python for stage 2 task
