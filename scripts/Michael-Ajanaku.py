b8a63bcc31e2f38f50db0afb7bcaf3e850f15bdf
Michael Ajanaku <remiljw@gmail.com>
2020-06-03 19:08:36 +0100

    func

--- scripts/Michael-Ajanaku.py
+++ scripts/Michael-Ajanaku.py
@@ -1,3 +1,1 @@
-def stage2_task():
-    print("Hello world, this is Michael Ajanaku with HNGi7 ID HNG-02854 and email remiljw@gmail.com  using Python for stage task 2.")
-stage2_task()
+print("Hello world, this is Michael Ajanaku with HNGi7 ID HNG-02854 and email remiljw@gmail.com  using Python for stage task 2.")
