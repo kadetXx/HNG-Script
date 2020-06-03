b75ba53017bd8106ea2830393cb9c90584647d62
onwuagba <onwuagbakenenna@gmail.com>
2020-06-03 11:41:39 +0100

    Kenenna Onwuagba script

--- scripts/Kenenna-Onwuagba.php
+++ scripts/Kenenna-Onwuagba.php
@@ -1,13 +1,9 @@
 <?php
 
-$name = "Kenenna Onwuagba";
-$id = "HNG-02948";
-$lang = "PHP";
+$name = 'Kenenna Onwuagba';
+$id = 'HNG-02948';
+$lang = 'PHP';
 
-function myHello($name, $id, $lang) {
-	$content = "Hello World, this is ".$name." with HNGi7 ID" . $id . "  using " . $lang ." for stage 2 task";
-	return $content;
-}
+	echo ( "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$lang." for stage 2 task" );
 
-echo myHello($name, $id, $lang);
 ?>
