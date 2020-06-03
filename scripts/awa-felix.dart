527baef4c4850630e804993f05d498040302498d
Phelickz <felixhope30@gmail.com>
2020-06-02 23:50:42 -0700

    awa felix hng task

---
+++ scripts/awa-felix.dart
@@ -0,0 +1,17 @@
+
+// Main Function - Dart Language
+void main() {
+  hngTask();
+}
+
+void hngTask(){
+  
+  //  Declearing Variable to be use for Future purpose
+  final String Name = 'Awa Felix';  // Fullname
+  final String hngID = 'HNG-00187';               // HNGi7 ID
+  final String lang = 'Dart Language';        // Programming Language 
+
+  //  Print the script
+  print(
+      'Hello World, this is $Name with HNGi7 ID $hngID using $lang for stage 2 task');
+}
