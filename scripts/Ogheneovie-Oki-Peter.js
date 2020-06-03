bd3bfcddcc6e0ed2173f047c86b99fb457039792
Oki-Peter <okipeterovie@gmail.com>
2020-06-03 11:57:52 +0100

    I added my email address
    I added my email address into my file titled Ogheneovie-Oki-Peter.js

--- scripts/Ogheneovie-Oki-Peter.js
+++ scripts/Ogheneovie-Oki-Peter.js
@@ -1,14 +1,15 @@
 var name = 'Ogheneovie Oki-Peter';
+var email = 'okipeterovie@gmail.com'
 var ID = 'HNG-03714';
 var lang = 'JavaScript';
 
-function showProps(myName, myID, myLang) {
+function showProps(myName, myID, myEmail, myLang) {
     var result = ``;
 
-        result += (`Hello World, this is ${myName} with HNGi7 ID: ${myID} using ${myLang} for stage 2 task`);
+        result += (`Hello World, this is ${myName} HNGi7 ID: ${myID} with email address ${myEmail} using ${myLang} for stage 2 task`);
      
     return result;
   }
 
  
-  console.log (showProps(name, ID, lang));
+  console.log (showProps(name, ID, lang, email));
