bbd6610b05d9f10f49472d796077cc1b6ac435e4
Johnkayode <57530761+Johnkayode@users.noreply.github.com>
2020-06-03 12:16:40 +0100

    Added my Python script to the scripts folder

---
+++ scripts/john_shodipo.py
@@ -0,0 +1,12 @@
+'''
+This program prints out my name, HNG id, email and language used in the task
+'''
+
+def myScript():
+	name = 'John Shodipo'
+	email= 'newtonjohn043@gmail.com'
+	id = 'HNG-00428'
+	language = 'Python'
+	return f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task'
+	
+print(myScript())
