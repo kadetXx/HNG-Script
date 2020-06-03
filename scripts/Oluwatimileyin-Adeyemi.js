2ca127fe0f427d4c29e1f387bbc5924bd3a0628a
D00msdaE <46998987+D00msdaE@users.noreply.github.com>
2020-06-03 14:55:15 +0100

    Add files via upload

---
+++ scripts/Oluwatimileyin-Adeyemi.js
@@ -0,0 +1,4 @@
+console.log('Hello World, this is Adeyemi Oluwatimileyin with HNGi7 ID HNG-02238 and email oluwatimilehin14@gmail.com using JavaScript for stage 2 task')
+//console.log() displays the text in quotes as an output
+//This is my way of making my code special.
+
