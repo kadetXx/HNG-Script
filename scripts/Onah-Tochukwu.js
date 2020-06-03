88c704ed4e094d4cdab6c7b57cf43e432aaf9ae9
tchytech <65729360+tchytech@users.noreply.github.com>
2020-06-03 18:50:43 +0300

    Create Onah-Tochukwu.js

---
+++ scripts/Onah-Tochukwu.js
@@ -0,0 +1,7 @@
+let name = 'Onah Tochukwu'
+let ID = 'HNG-02371';
+let language = 'JavaScript';
+ let email = 'pearlpey1@gmail.com'
+let result = `Hello World, this is ${name} with HNG-ID ${ID} and email${email} using ${language} for stage two task`;
+ console.log(result);
+
