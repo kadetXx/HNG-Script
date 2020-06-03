7695059fb03d01702c5ba298bd70612a4c88a7d3
Tobi Osimosu <osimosutobi@gmail.com>
2020-06-03 11:39:34 +0100

    updated Bolaji-Ola.js

--- scripts/Bolaji-ola.js
+++ scripts/Bolaji-ola.js
@@ -2,7 +2,7 @@ const fullName = "Bolaji Ola";
 const language = "JavaScript";
 const ID = "HNG-1703";
 
-function intro(){
-  return `Hello World, this is ${fullName}, with HNGi7 ID: ${ID} using ${language} for stage 2 task`
- }
-console.log(intro())
+function intro() {
+  return `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+}
+console.log(intro());
