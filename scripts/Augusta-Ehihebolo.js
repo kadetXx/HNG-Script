075673d05b7248262add90df98e7901f3d48a4d8
netae <ehiheboloaugustar@gmail.com>
2020-06-03 14:20:41 +0100

    AugustaEhihebolo commit

--- scripts/Augusta-Ehihebolo.js
+++ scripts/Augusta-Ehihebolo.js
@@ -1,5 +1,5 @@
 function myDetails(fullName, id, language, email){
-   return (`Hello World, this is ${fullName} with HNGi7 ID-${id} and email ${email} using ${language} for stage 2 task`)
+   return (`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
 }
 
-console.log (myDetails("Augusta Ehihebolo", "02000", "JavaScript", "ehiheboloaugustar@gmail.com"))
+console.log (myDetails("Augusta Ehihebolo", "HNG-02000", "JavaScript", "ehiheboloaugustar@gmail.com"))
