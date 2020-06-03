137c489d591b9086fda30d968776b3f0c4203146
Exact <62805072+Exact337@users.noreply.github.com>
2020-06-03 11:25:35 +0100

    Add files via upload

---
+++ scripts/exact.js
@@ -0,0 +1,2 @@
+const HNG = { name: 'Adebare Amos', id: 'HNG-02063', lang:'JavaScript'};
+console.log (`Hello World, this is ${HNG.name} with HNGi7 ID ${HNG.id} using ${HNG.lang} for stage 2 task`);
