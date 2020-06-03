362de9638ccd85de857c683092717875a291c78f
Daviking95 <daviking95@gmail.com>
2020-06-03 22:24:00 +0100

    trying to push

--- scripts/OpeyemiPeter.js
+++ scripts/OpeyemiPeter.js
@@ -1,9 +1,9 @@
-const fullName = "Opeyemi PETER";
+const fullName = "Opeyemi Peter"
 const ID = "HNG-01612";
 const language = "Javascript";
 
 function intro() {
-    return `Hello world, this is ${fullName } with  HNGi7 ID ${ID} using ${language } for Stage 2 task`
+    return `Hello world, this is ${fullName }, with  HNGi7 ID: ${ID} using ${language } for Stage 2 task`
 }
 
 console.log(intro())
