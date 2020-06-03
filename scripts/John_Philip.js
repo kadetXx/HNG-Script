72b3136bb482493357501f214e3240760f061b2f
John Philip <60175531+developerphilo@users.noreply.github.com>
2020-06-03 19:23:43 +0300

    Update and rename John_Philip.js to John-Philip.js

--- scripts/John_Philip.js
+++
@@ -1,7 +0,0 @@
-//this is my commit to the stage 1 task
-const name = "John Philip";
-const hngid = "HNG-01923";
-const lang = "Javascript";
-const email = "developerphilo@gmail.com"
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${hngid} using ${lang} and ${email} for stage 2 task`);
