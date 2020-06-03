21d3ec9cd295b58049c7c29fd3fe3a1765f1991f
Kingsley31 <kingsleyemeka31@gmail.com>
2020-06-03 12:56:00 +0100

    Added my php script for stage 2 task

---
+++ scripts/Ufoegbulam-Chukwuemeka-Kingsley.php
@@ -0,0 +1,9 @@
+<?php
+
+  $fullname="Ufoegbulam Chukwuemeka Kingsley";
+  $hng_board_id="HNG-05670";
+  $language_used="PHP";
+  
+  echo "Hello World, this is ".$fullname." with HNGi7 ID ".$hng_board_id." using ".$language_used." for stage 2 task.";
+
+?>
