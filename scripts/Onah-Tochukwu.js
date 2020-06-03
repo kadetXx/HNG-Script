9c98ccf30a1d0eb2f5f084737faa64265ea1806b
tchytech <65729360+tchytech@users.noreply.github.com>
2020-06-03 20:33:41 +0100

    Update Onah-Tochukwu.js
    Added email

--- scripts/Onah-Tochukwu.js
+++ scripts/Onah-Tochukwu.js
@@ -1,7 +1,6 @@
-let name = 'Onah Tochukwu'
-let ID = 'HNG-02371';
-let language = 'JavaScript';
- let email = 'pearlpey1@gmail.com'
-let result = `Hello World, this is ${name} with HNG-ID ${ID} and email${email} using ${language} for stage two task`;
- console.log(result);
 
+let fullname = "Onah Tochukwu";
+let id =  "HNG-02371";
+let language = "JavaScript";
+let email = "pearlpey1@gmail.com"
+console.log("Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email + " using " + language + " stage 2 task ");
