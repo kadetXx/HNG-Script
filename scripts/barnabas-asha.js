c5c97ea1ab76c8822848cac5e954bdacbe212f52
Barnabas Asha <barnabee58@gmail.com>
2020-06-03 15:26:35 +0100

    update

--- scripts/barnabas-asha.js
+++ scripts/barnabas-asha.js
@@ -1,13 +1,13 @@
 //This function returns the string
-const hngProfile = {
+let hngProfile = {
     fullName: 'Barnabas Asha',
     hngId: 'HNG-01877',
     email: 'barnabee58@gmail.com',
     language: 'Javascript',
-    loadProfile: function() {
-        return `Hello World, this is ${this.fullName} with HNGi7 ID ${this.hngId} using ${this.language} and email ${this.email} for stage 2 task`
+    loadProfile : function() {
+        console.log(`Hello World, this is ${this.fullName} with HNGi7 ID ${this.hngId} using ${this.language} and email ${this.email} for stage 2 task`)
     }
 }
 
 
-console.log(hngProfile.loadProfile());
+hngProfile.loadProfile();
