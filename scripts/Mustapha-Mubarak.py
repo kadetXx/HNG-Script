5403fbf6afa9008b456c87911f318e3ad0278900
pythagoras-dev <58697223+pythagoras-dev@users.noreply.github.com>
2020-06-03 22:15:13 +0100

    Update Mustapha-Mubarak.py

--- scripts/Mustapha-Mubarak.py
+++ scripts/Mustapha-Mubarak.py
@@ -5,4 +5,3 @@ my_info = {"name":"Mubarak Mustapha",
 
 
 print("Hello World, this is "+my_info["name"]+" with HNGi7 ID "+my_info["id"]+" and email "+my_info["email"]+" using "+my_info["lanquage"]+" for stage 2 task")
-#print("Hello World, this is Mustapha Mubarak with HNGi7 ID HNG-03644  and email pythagoras.dev15@gmail.com using python for stage 2 task ")
