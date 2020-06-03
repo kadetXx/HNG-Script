13cb7af3a34e313d2e9bd615eb0e816e3db4d52a
Karen Efereyan <thedevkaren@gmail.com>
2020-06-03 16:36:16 +0100

    Update script to reflect email address

--- scripts/Karen-Efereyan.js
+++ scripts/Karen-Efereyan.js
@@ -1,12 +1,13 @@
-function MoveToStage2(name, id, language) {
+function MoveToStage2(name, id, language, email) {
   (this.name = name), (this.id = id), (this.language = language);
   console.log(
-    `Hello World! This is ${name} with HNG ID ${id} using ${language} for stage2 task`
+    `Hello World, This is ${name} with HNG ID ${id} using ${language} and email ${email} for stage2 task`
   );
 }
 
 const karen = MoveToStage2(
   "Efereyan Karen Simisola",
   "HNG-01050",
-  "Javascript"
+  "Javascript",
+  "kimsyefe@gmail.com"
 );
