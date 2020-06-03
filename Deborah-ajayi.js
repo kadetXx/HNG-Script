eb98fc4a13380da143bbfd147229486b0e65267b
speak2debby <54291723+speak2debby@users.noreply.github.com>
2020-06-03 13:04:17 +0100

    Create Deborah-ajayi.js
    A simple js file on HNG neede info

---
+++ Deborah-ajayi.js
@@ -0,0 +1,5 @@
+var name= 'Deborah Ajayi'
+username= 'speak2debby'
+id= 'HNG-03850'
+email='speak2debby@gmail.com'
+console.log(`Hello World, this is ${name}, Email-Address: ${email} with slack username: ${username} and HND-ID: ${id} for Stage 2 task`)
