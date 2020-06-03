40ecfa38deae2c1c8d4d80617f5b430c8e906214
raraku <airenov500@gmail.com>
2020-06-03 11:37:49 +0100

    Airenovboise Israel, added script

---
+++ scripts/Israel-Airenovboise.py
@@ -0,0 +1,6 @@
+print(
+    "Hello World, this is {} with HNGi7 ID {} and email {} using python for stage 2 task".format(
+        "Airenovboise Israel", "HNG-05846", "airenov500@gmail.com"
+    ),
+    flush=True,
+)
