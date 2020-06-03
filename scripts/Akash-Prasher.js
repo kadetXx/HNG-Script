c9e71705fd165ec1326b0529e09182779a229a96
akashprasher <prince.akash.prasher@gmail.com>
2020-06-03 15:21:50 +0530

    change the format of output: Hello World, this is [full name] with HNGi7 ID [ID] using [language]

--- scripts/Akash-Prasher.js
+++ scripts/Akash-Prasher.js
@@ -2,7 +2,7 @@ let myDetails = () => {
     let myFirstName = "Akash";
     let myLastName = "Prasher";
     let myHNGID = "HNG-06260";
-    console.log(`Hello World, this is ${myFirstName} ${myLastName} with HNGi7 ID ${myHNGID} using JavaScript`);
+    console.log(`Hello World, this is ${myFirstName} ${myLastName} with HNGi7 ID ${myHNGID} using JavaScript for stage 2 task`);
 };
 
 myDetails();
