171e02f6c97a323c97166c6eba76e6e3f45830a6
Chinaza <ogwo.chinaza@yahoo.com>
2020-06-03 19:41:09 +0100

    added email to output

--- scripts/Ogwo-Chinaza.js
+++ scripts/Ogwo-Chinaza.js
@@ -1,8 +1,3 @@
-// const outputStr = 'Hello World, this is Ogwo Chinaza with HNGi7 ID HNG-02067 using JavaScript for stage 2 task';
-
-// console.log(outputStr);
-
-
 const output = (fullname, id, email, language) => {
   console.log(
     `Hello World, this is ${fullname} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
