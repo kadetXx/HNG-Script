e944ab951d3585fe89006a1d181c7846e55809f9
Peace-adebola <60345650+Peace-adebola@users.noreply.github.com>
2020-06-03 14:14:32 +0100

    Update Peace-adeoye.js

--- scripts/Peace-adeoye.js
+++ scripts/Peace-adeoye.js
@@ -1,1 +1,9 @@
-console.log("Hello World, this is Peace Adeoye with HNGi7 ID HNG-06335 using JavaScript for stage 2 task");
+const myName= {name: 'Peace Adeoye',
+               id: 'HNG-06335',
+               language: 'Javascript',
+  email: 'adebolapeace0@gmail.com'
+};
+
+const myOutput = `Hello World, this is ${myName.name} with HNGi7 ID ${myName.id} and email ${myName.email} using ${myName.language} for stage 2 task`;
+
+console.log(myOutput);
