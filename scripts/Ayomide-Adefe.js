ef9eb5c6a8322b7e4b67751844b9432f36ca1935
Ayomide96 <ayomideadefe@gmail.com>
2020-06-03 23:05:37 +0100

    improved my script by adding email

--- scripts/Ayomide-Adefe.js
+++ scripts/Ayomide-Adefe.js
@@ -1,1 +1,10 @@
-console.log(`"Hello World, This is Ayomide Adefe with HNGi7 ID HNG-02441 using Javascript for stage 2 task" `);
+// variables declaration
+const fullName = "Ayomide Adefe";
+const ID       = "HNG-02441";
+const programmingLanguage = "Javascript";
+const emailAddress ="ayomideadefe@gmail.com";
+
+
+// output using string interpolation
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} and email ${emailAddress} using ${programmingLanguage} for stage 2 task`);
+
