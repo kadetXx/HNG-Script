59641e3af118adab6a3df4ee3edcdf47f7b6bece
Oluwaseyi <seyi.juliana@gmail.com>
2020-06-03 12:31:09 +0100

    typo change

--- scripts/oluwaseyi-awotunde.js
+++ scripts/oluwaseyi-awotunde.js
@@ -5,7 +5,7 @@ class MyInfo {
     this.language = language;
   }
 
-}
+};
 
 const info = new MyInfo("Oluwaseyi Awotunde", "HNG-00262", "JavaScript");
-console.log(`Hello World, this is ${info.name} with HNGi7 ID ${info.id} using ${info.language} for Stage 2 track`);
+console.log(`Hello World, this is ${info.name} with HNGi7 ID ${info.id} using ${info.language} for Stage 2 task`);
