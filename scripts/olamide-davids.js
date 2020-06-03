c63bf7963d0a71e4d40f709bf7b4d3abfb30418a
UnsignedHype <66218402+UnsignedHype@users.noreply.github.com>
2020-06-03 11:03:43 +0100

    Added email & minor changes

--- scripts/olamide-davids.js
+++ scripts/olamide-davids.js
@@ -1,7 +1,11 @@
-//Code to print statement required
+ //Code to print statement required
+var name = "Olamide Davids"; //my fullname
+var id = "HNG-01138";        // my id 
+var language = "Javascript";  //my chosen language
+var email = "olamidedavid189@gmail.com"; //my email
 
     function UnsignedHype (){
-        console.log("Hello World, this is Olamide Davids with HNGi7 ID HNG-01138 using Javascript for stage 2 task")
+        console.log("Hello World, this is" + " " +name + " "+ "with HNGi7 ID"+ " "+ id +" " + "using"+ " "+ language + " "+ "for stage 2 task")
     }
 
 UnsignedHype();
