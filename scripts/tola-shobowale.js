a69de8c84dca4b5e01efaae86d10eb7fb25d92d9
MAC <mac@MACs-MacBook-Pro.local>
2020-06-03 21:53:54 +0100

    my script

---
+++ scripts/tola-shobowale.js
@@ -0,0 +1,10 @@
+const myName = "Tola Shobowale";
+const id = "HNG 00272";
+const lang = "javascript";
+const email = "shobowaletola@gmail.com";
+
+function myDetails() {
+  return `Hello World, this is ${myName} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`;
+}
+
+console.log(myDetails());
