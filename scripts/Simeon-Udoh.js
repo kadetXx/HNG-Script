aa1ef78cd85c9a945710ff3acd6dfc67dc6b8202
simeon4real <simeon.udoh45@gmail.com>
2020-06-03 19:11:05 +0100

    :zap: updated my script with  my email

--- scripts/Simeon-Udoh.js
+++ scripts/Simeon-Udoh.js
@@ -1,4 +1,5 @@
 const name = 'Simeon Udoh',
         ID = '01827',
-        language = 'Javascript';
-console.log(`Hello World, This is ${name} with HNGi7 ID HNG-${ID} using ${language} for stage 2 task.`);
+        language = 'Javascript',
+        email = 'simeon.udoh45@gmail.com'
+console.log(`Hello World, This is ${name} with HNGi7 ID HNG-${ID} and email simeon.udoh45@gmail.com using ${language} for stage 2 task.`);
