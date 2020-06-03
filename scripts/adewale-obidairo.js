c8d6dd0e0305aafb60d7af5f2ee086df8767bdd7
adewale-samson <49336875+adewale-samson@users.noreply.github.com>
2020-06-03 14:50:04 +0100

    Update adewale-obidairo.js

--- scripts/adewale-obidairo.js
+++ scripts/adewale-obidairo.js
@@ -1,9 +1,10 @@
 let name = "Adewale Samson Obidairo";
 let id = "HNG-04791";
 let language = "javascript";
+let email = "mascot4sure@gmail.com";
 
 let outputFile = function() {
-	return console.log("Hello World, this is " + name + " with HNGi7 ID " + id + 
-		" using " + language + " for stage 2 task")
+	return console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " +
+		email + " using " + language + " for stage 2 task")
 };
 outputFile(); 
