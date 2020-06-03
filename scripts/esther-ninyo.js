b4b0dcb8aa002cd27e81437172acca39f3c442f6
ninyhorlah <ninyhorlah6@gmail.com>
2020-06-03 02:32:47 +0100

    updated

--- scripts/esther-ninyo.js
+++ scripts/esther-ninyo.js
@@ -1,2 +1,2 @@
 
-console.log('Hello world, this is Esther Ninyo with HNGi7 ID-04176 using Javascript for stage 2 task.')
+console.log('Hello world, this is Esther Ninyo with HNGi7 ID-04176 using Javascript for stage 2 task.');
