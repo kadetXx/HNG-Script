67035d1369939ab919bba3a31c0c9810bbbeb6a4
Nnaji Victor <nnajivictor0@gmail.com>
2020-06-03 16:01:55 +0100

    Update nnaji-victor.js
    added email to the script

--- scripts/nnaji-victor.js
+++ scripts/nnaji-victor.js
@@ -2,6 +2,7 @@
 const data = { 
   name: 'Victor Nnaji', 
   ID: 'HNG-04553', 
-  language:'javaScript'
+  language:'javaScript',
+  email: "nnajivictor0@gmail.com"
   };
-console.log (`Hello World, this is ${data.name} with HNGi7 ID ${data.ID} using ${data.language} for stage 2 task`);
+console.log (`Hello World, this is ${data.name} with HNGi7 ID ${data.ID} and email: ${data.email} using ${data.language} for stage 2 task`);
