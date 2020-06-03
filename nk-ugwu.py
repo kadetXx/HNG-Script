e4c07637796473c3efa2fbc444e2e564aba4be94
Ego <47955681+egocious@users.noreply.github.com>
2020-06-03 23:50:42 +0100

    deleted

--- nk-ugwu.py
+++
@@ -1,3 +0,0 @@
-print('Hello World, this is Nk Ugwu with HNGi7 ID HNG-02568 using python for stage 2 task')
-
-print('Hello World, this is Nk Ugwu with HNGi7 ID HNG-02568 and email egougwu11@yahoo.com using python for stage 2 task')
