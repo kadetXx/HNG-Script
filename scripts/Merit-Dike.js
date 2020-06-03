78035582ee4531623c17bd17c88cb93eb2f220ae
Meritdike <65596735+Meritdike@users.noreply.github.com>
2020-06-03 14:42:36 -0700

    Update Merit-Dike.js

--- scripts/Merit-Dike.js
+++ scripts/Merit-Dike.js
@@ -1,1 +1,6 @@
-console.log('Hello World, this is Merit Dike with the HNGi7 ID HNG-00392 using Javascript for stage 2 task');
+const myName = 'Merit Dike';
+const HNGi7 ID = 'HNG-00392';
+const email = 'dike.merit@yahoo.com';
+const language = 'Javascript';
+
+console.log('Hello World, this is ${myName} with the HNGi7 ID ${HNGi7 ID} and email ${email} using ${language} for stage 2 task');
