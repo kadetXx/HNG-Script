dd4e9c043dce4a8bd7b377405b044e8fdacc0556
damlois <lois0168@gmail.com>
2020-06-03 13:02:07 +0100

    added email address

--- scripts/lois-adegbohungbe.js
+++ scripts/lois-adegbohungbe.js
@@ -1,5 +1,6 @@
 var fullName = 'Lois Adegbohungbe'
 var id = 'HNG-04138'
+var email = 'loisadegbohungbe@gmail.com'
 var language = 'Javascript'
-var message = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`
+var message = `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
 console.log(message)
