713c8629ed0c21738fa182aa594c100448ab35db
Afeez-Ola <afeezbolajiola@gmail.com>
2020-06-03 10:59:27 +0100

    Update Bolaji-Ola.js

--- scripts/Bolaji-Ola.js
+++ scripts/Bolaji-Ola.js
@@ -3,6 +3,6 @@ const language = "JavaScript";
 const ID = "HNG-1703";
 
 function intro(){
-  return `Hello World, this is ${fullName} with HNGi7 ID: ${ID} using ${language} for stage 2 task`
+  return `Hello World this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`
 }
 console.log(intro())
