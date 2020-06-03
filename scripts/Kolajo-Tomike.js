d4aa107ee8e2dbbe8804eba20589c5b1da373aa6
tomike19 <66334679+tomike19@users.noreply.github.com>
2020-06-03 22:28:32 +0100

    Create Kolajo-Tomike.js

---
+++ scripts/Kolajo-Tomike.js
@@ -0,0 +1,10 @@
+const fullName = "Kolajo Tomike";
+const ID = "HNG-05480";
+Const Email = "kolajoelizabeth@gmail.com"
+const language = "Javascript";
+
+function intro() {
+    return `Hello world, this is ${fullName } with  HNGi7 ID ${ID} and email ${Email} using ${language } for Stage 2 task`
+}
+
+console.log(intro())
