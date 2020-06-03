0ed66091aa3a2fc49d8a2aa8685be218003b4162
Azeez Rahmon <arahmon001@wmt.ema.ad.pwcinternal.com>
2020-06-03 23:42:45 +0100

    Python Code For Hello World

--- scripts/Rahmon-Azeez.py
+++ scripts/Rahmon-Azeez.py
@@ -1,14 +1,1 @@
-# My Name is Rahmon Azeez
-# Stage 2
-
-Name = "Rahmon Azeez"
-HngId = "HNG-06010"
-ProgramLanguage = "Python"
-EmailAddress = "rahmonazeez7@gmail.com"
-
-print(f" Name: {Name},  HNGID: {HngId},  Programming Language: {ProgramLanguage},  Email Address: {EmailAddress}")
-
-print("")
-print(f"Hello World, This is {Name} with HNGi7 ID {HngId} and Email {EmailAddress} using {ProgramLanguage} for stage 2 task")
-
-#Thank you for reviewing this code.
+print("Hello World, this is Rahmon Azeez with HNGi7 ID HNG-06010 and email rahmonazeez7@gmail.com using Python for stage 2 task")
