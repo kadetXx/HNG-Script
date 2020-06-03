efd75755b1981d10b222fd8bd87f48bb3d19f8f3
theayofash <66254366+theayofash@users.noreply.github.com>
2020-06-03 13:53:00 +0100

    Update Fasakin-Ayomide.js

--- Fasakin-Ayomide.js
+++ Fasakin-Ayomide.js
@@ -1,1 +1,1 @@
-console.log("Hello world, this is Fasakin Ayomide with HNGi7 ID HNG-01996 using javaScript for stage 2 task")
+console.log("Hello world, this is Fasakin Ayomide with HNGi7 ID HNG-01996 and email ay.shmurda@gmail.com using javaScript for stage 2 task")
