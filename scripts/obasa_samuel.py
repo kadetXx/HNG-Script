786b4c8f5741cc92a1254f0d20689324ef797b3c
Obasa Samuel Temitope <obasasamuel96@gmail.com>
2020-06-03 15:35:12 +0100

    Obasa Samuel Task 2 commit updated with email

---
+++ scripts/obasa_samuel.py
@@ -0,0 +1,5 @@
+name = "Obasa Samuel Temitope"
+ID = "HNG-04751"
+language = "Python"
+email = "obasasamuel96@gmail.com"
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task, you can reach me via email with %s " %(name, ID, language, email))
