cc69b134460dddab77ce6500815aba9006f7eb16
Jacreator <jambone.james82@gmail.com>
2020-06-03 19:22:56 +0100

    email fixed

--- scripts/juliet-adeboye.py
+++ scripts/juliet-adeboye.py
@@ -1,4 +1,4 @@
-def bio():
-    	print('Hello World, this is Juliet Adeboye with HNGi7 ID HNG-00159 julietadeboye01@gmail.com using python for stage 2 task')
+def myScript():
+    	print('Hello World, this is Juliet Adeboye with HNGi7 ID HNG-00159 and email julietadeboye01@gmail.com using Python for stage 2 task')	
 
-bio()
+myScript()
