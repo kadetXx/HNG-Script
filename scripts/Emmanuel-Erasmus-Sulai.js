86f3305f1593d96962aa1bb2e3d18690308f7037
Emmanuel Erasmus <2016wealthtips@gmail.com>
2020-06-03 15:50:19 +0100

    Added my email address

--- scripts/Emmanuel-Erasmus-Sulai.js
+++ scripts/Emmanuel-Erasmus-Sulai.js
@@ -3,4 +3,4 @@ var HNGi7_ID = "HNG-01104";
 var email = "'2016wealthtips@gmail.com'";
 var LanguageUsed = "Javascript";
 var myMessage = "Hello world, this is " + fullname + " with HNGi7 ID " + HNGi7_ID + " and email " + email + " using " + LanguageUsed + " for stage 2 task.";
-console.log(myMessage)
+console.log(myMessage);
