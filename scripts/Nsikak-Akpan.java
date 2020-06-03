81f6af4c7a603bfdcebc4bd019286c4dfff69a44
Tonyalex <tonyalex@gmail.com>
2020-06-03 22:36:57 +0100

    Nsikak-Akpan.java with email (third commit)

--- scripts/Nsikak-Akpan.java
+++ scripts/Nsikak-Akpan.java
@@ -1,4 +1,4 @@
-public class Nsikak_Akpan {
+public class NsikakAkpan {
 
         public static void main(String[] args) {
         System.out.println("Hello World, this is Nsikak_Akpan with HNGi7 ID HNG-01250 and email tonyalex6258@gmail.com using java for my stage 2 task");
