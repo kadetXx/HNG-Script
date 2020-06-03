f4fd201089e982bf0a080ceffdc79ddcd209d2b8
eshjay <olushola.abiodun@ovi123>
2020-06-03 12:23:40 +0100

    added email

--- scripts/Abiodun-Olushola.js
+++ scripts/Abiodun-Olushola.js
@@ -2,8 +2,9 @@ const firstName = 'Abiodun';
 const lastName = 'Olushola';
 const userName = 'Eshjay';
 const fullName = `${firstName} ${lastName}`;
+const email = 'olushola.abiodun@gmail.com';
 const id = 'HNG-03204';
 const language = 'JavaScript';
-const message = `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
+const message = `Hello World, this is ${fullName} with ${id} and email ${email} using ${language} to solve stage 2 task.`;
 
 console.log(message);
