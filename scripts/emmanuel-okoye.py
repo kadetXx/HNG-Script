e6c877604f0329a849e4c49f1b3df59f20b8797f
Chuckz Okoye <tricelex@gmail.com>
2020-06-03 22:26:11 +0100

    updated emmanuel-okoye.py

--- scripts/emmanuel-okoye.py
+++ scripts/emmanuel-okoye.py
@@ -1,6 +1,12 @@
 """ Task for HNGi7 """
+
 my_name = 'Emmanuel Okoye'
 my_id = 'HNG-02995'
 my_language = 'python'
+my_email = 'tricelex@gmail.com'
 
-print(f'Hello world, this is {my_name} with HNGi7 ID {my_id} using {my_language} for stage 2 task')
+def print_message(name, id, lang, email):
+    print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + lang + " for stage 2 task")
+
+
+print_message(my_name, my_id, my_language, my_email)
