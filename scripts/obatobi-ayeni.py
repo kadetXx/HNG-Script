a86c33d6903672cd3119ded1cc44d8c4cb0cf8c8
weedydash <57416736+weedydash@users.noreply.github.com>
2020-06-03 08:18:04 +0100

    Update obatobi-ayeni.py

--- scripts/obatobi-ayeni.py
+++ scripts/obatobi-ayeni.py
@@ -1,1 +1,1 @@
-print "Hello World, this is Obatobi Ayeni with HNGi7 ID HNG-04196 using Python for stage 2 task"
+print("Hello world, this is Obatobi Ayeni with HNGi7 ID HNG-04196 using python for stage 2 task")
