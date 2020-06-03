ef8035d819a3c12fed081ede1ae2fb75442e035e
MutegekiHenry <henrymutegeki117@gmail.com>
2020-06-03 14:04:03 +0300

    feat: add henry script for team task

---
+++ scripts/henry-mutegeki.js
@@ -0,0 +1,8 @@
+const FullName = "Henry Mutegeki";
+const HngId = "HNG-01577";
+const Language = "Javascript";
+const Email = "henrymutegeki117@gmail.com"
+
+message = `Hello World, this is ${FullName} with HNGi7 ID ${HngId} and email ${Email} using ${Language} for stage 2 task`;
+
+console.log(message);
