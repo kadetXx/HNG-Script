2ffe3a31ac13edf1306238af31c08f81c2c131e8
cugbo <64258165+cugbo@users.noreply.github.com>
2020-06-03 17:08:11 +0100

    Update chima-ugbo.py

--- scripts/chima-ugbo.py
+++ scripts/chima-ugbo.py
@@ -2,4 +2,4 @@ name = "Chima Ugbo"
 ID = "HNG-01119"
 language = "Python"
 email = "chimaugbo@gmail.com"
-print("Hello World, this is " + name + " with HNGi7 ID " + ID + " and " + email + " using " + language + " for stage 2 task")
+print("Hello World, this is " + name + " with HNGi7 ID " + ID + " and email " + email + " using " + language + " for stage 2 task")
