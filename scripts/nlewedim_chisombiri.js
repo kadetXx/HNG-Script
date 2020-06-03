de2967bfdf5dddb30ced12e0f8f24f2f020f73d7
chisombiri <chisombiri@gmail.com>
2020-06-03 23:19:48 +0100

    Chisombiri hng stage 1 task

---
+++ scripts/nlewedim_chisombiri.js
@@ -0,0 +1,17 @@
+//calling my variables using es6
+//Nlewedim Chisombiri first task
+
+let output = () => {
+   const hng = {
+      firstName: "Chisombiri",
+      lastName: "Nlewedim",
+      hngID: "HNG-01454",
+      language: "Javascript",
+      email: "chisombiri@gmail.com"
+   }
+   return `Hello World, this is ${hng.firstName} ${hng.lastName} with HNGi7 ID ${hng.hngID} using ${hng.language} for stage 2 task. ${hng.email}`
+}
+
+console.log(output());
+
+//return 
