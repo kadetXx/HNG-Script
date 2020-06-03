242719c184cbac36fd86c7732c5e2ffbce54c2ed
akashprasher <prince.akash.prasher@gmail.com>
2020-06-03 14:46:23 +0530

    adding details

---
+++ scripts/Akash-Prasher.js
@@ -0,0 +1,8 @@
+let myDetails = () => {
+    let myFirstName = "Akash";
+    let myLastName = "Prasher";
+    let myHNGID = "HNG-06260";
+    console.log(`My name ${myFirstName} ${myLastName} having ID ${myHNGID}`);
+};
+
+myDetails();
