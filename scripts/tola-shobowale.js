3a78a258a6276909b2c6dbce0ec52f26796d9472
MAC <mac@MACs-MacBook-Pro.local>
2020-06-04 00:04:57 +0100

    commit

--- scripts/tola-shobowale.js
+++ scripts/tola-shobowale.js
@@ -7,4 +7,4 @@ function myDetails() {
   return `Hello World, this is ${fullname} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
 }
 
-console.log(myDetails());
+myDetails();
