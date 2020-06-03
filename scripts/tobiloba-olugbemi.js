5649ce6f5ff4d123521051aee145670d18350be5
Tibesti <tobilobaolugbemi@gmail.com>
2020-06-03 02:04:54 -0600

    Update tobiloba-olugbemi.js

--- scripts/tobiloba-olugbemi.js
+++ scripts/tobiloba-olugbemi.js
@@ -1,5 +1,5 @@
 const name = 'Tobiloba Olugbemi';
-const id = 03473;
+const id = '03473';
 const lang = 'JavaScript'
 
 console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
