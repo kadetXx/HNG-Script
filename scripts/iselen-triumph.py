2c9372a260c902cc9049d5b59f6b4fedb8550c28
geekywolf <t4riumph@gmail.com>
2020-06-03 14:31:15 +0200

    Update iselen-triumph.py

--- scripts/iselen-triumph.py
+++ scripts/iselen-triumph.py
@@ -1,8 +1,10 @@
 """This file outputs my name, registration number, email and language used"""
 
 Name= "Iselen Triumph"
-ID= "HNG-04439"
+boardid = "HNG-04439"
 Email= "t4riumph@gmail.com"
 language= "python"
 
-print(f"Hello World, this is {Name} with HNGi7 ID {ID} and email {Email} using {language} for stage 2 task")
+print("Hello World, this is " + Name + " with HNGi7 ID " + boardid + " and email " + Email + " using " + language + " for stage 2 task" ) #concatenated string as PHP doesnt recognise f literal
+
+
