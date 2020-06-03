3e1cfc23ea50fbfa0c6037b00a340bc059d252c6
Jules-Renaud <julesrenaud10@gmail.com>
2020-06-03 13:31:19 +0100

    remove comments

--- scripts/jules-tjahe.py
+++ scripts/jules-tjahe.py
@@ -4,4 +4,3 @@ def sayHello(myName,myId,myLanguage, myEmail):
     return
 
 sayHello("Tjahe Essomba Jules Renaud","HNG-01452","python","julesrenaud10@gmail.com")
-#Hello World, this is Elisha Simeon Ukpong Udoh with HNGi7 ID HNG-01827 and email simeon.udoh45@gmail.com using python for stage 2 task
