34dd9b0345a5ede9993b4b7b774f1fa204325a38
edmund1645 <edmund.timfon@gmail.com>
2020-06-03 12:16:17 +0100

    modify Timfon Ekott to add email

--- scripts/Timfon-Ekott.js
+++ scripts/Timfon-Ekott.js
@@ -2,8 +2,9 @@ const fullName = 'Timfon Ekott';
 const id = 'HNG-04227';
 const userName = 'Blodhound';
 const language = 'JavaScript';
+const email = 'edmund.timfon@gmail.com';
 
 const message = (() =>
-  `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)();
+  `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`)();
 
 console.log(message);
