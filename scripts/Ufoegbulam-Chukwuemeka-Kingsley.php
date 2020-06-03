722d5e97c0d8377cefe113e53af878636778fc00
Kingsley31 <kingsleyemeka31@gmail.com>
2020-06-03 13:22:21 +0100

    Added email to my script

--- scripts/Ufoegbulam-Chukwuemeka-Kingsley.php
+++ scripts/Ufoegbulam-Chukwuemeka-Kingsley.php
@@ -3,7 +3,8 @@
   $fullname="Ufoegbulam Chukwuemeka Kingsley";
   $hng_board_id="HNG-05670";
   $language_used="PHP";
+  $email="kingsleyemeka31@gmail.com";
   
-  echo "Hello World, this is ".$fullname." with HNGi7 ID ".$hng_board_id." using ".$language_used." for stage 2 task.";
+  echo "Hello World, this is ".$fullname." with HNGi7 ID ".$hng_board_id."and email ".$email." using ".$language_used." for stage 2 task.";
 
 ?>
