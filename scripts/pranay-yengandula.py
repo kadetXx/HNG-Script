631f47f80d2cc46832d09983aae95aa5baa2f21f
Pranay Yenagandula <Pranay41@github.com>
2020-06-03 15:24:12 +0530

    changes

--- scripts/pranay-yengandula.py
+++ scripts/pranay-yengandula.py
@@ -1,4 +1,7 @@
-name="Pranay Yenagandula"
-ID="HNG-01542"
-language="Python"
-print("Hello World, this is",name,"with HNGi7 ID ",ID,"using",language,"for stage 2 task",flush=True)
+#Pranay-Yenagandula 
+
+class display_string:
+    def __init__(self):
+        print("Hello World, this is Pranay Yenagandula with HNGi7 ID HNG-01542 using Python for stage 2 task")
+
+d=display_string()
