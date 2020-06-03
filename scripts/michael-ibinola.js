b44544a0168442d1ac43bd99c64b5262cb497a6a
Ibinola Michael Oluwanifemi <38398043+Ibinola@users.noreply.github.com>
2020-06-03 18:22:43 +0100

    Update michael-ibinola.js

--- scripts/michael-ibinola.js
+++ scripts/michael-ibinola.js
@@ -1,3 +1,3 @@
 console.log(
-  "Hello World, this is Michael Ibinola with HNGi7 ID HNG-02066 with email ibinolamichael1@gmail.com using Javascript for stage 2 task"
+  "Hello World, this is Michael Ibinola with HNGi7 ID HNG-02066 and email ibinolamichael1@gmail.com using Javascript for stage 2 task"
 );
