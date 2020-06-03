a0a5d84a48d0f655e5b0b8bf8b1b54da7546e07a
mike-ske <66280654+mike-ske@users.noreply.github.com>
2020-06-03 21:58:29 +0100

    Update Alumona-micah.php

--- scripts/Alumona-micah.php
+++ scripts/Alumona-micah.php
@@ -1,10 +1,7 @@
 <?PHP 
 
-$_full_name = "Alumona Micah";
-$_id = "HNC-04840";
-$_lang = "php";
 
-echo "Hello world, this is ".$_full_name." with 
-HNGi7 ID ".$_id." using ".$_lang." for stage 2 task.";
+echo "Hello world, this is Alumona Micah with 
+HNGi7 ID HNG-04840 and email micahalumona@gmail.com using PHP for stage 2 task.";
 
 ?>
