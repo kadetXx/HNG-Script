cad8a275c02eb51ac86b188aeca6e8ffd3c7d3fd
Dayvvo <45407706+Dayvvo@users.noreply.github.com>
2020-06-03 06:06:12 +0300

    Add files via upload

---
+++ scripts/Opeyemi-Adeyemi.js
@@ -0,0 +1,7 @@
+
+
+
+(function (name="Adeyemi Opeyemi David",id="1982794",language="Javascript") {
+    document.write('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using language ' + language + ' for stage 2 task ' )
+
+ })()
