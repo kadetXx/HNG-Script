c527e328ff86eb4b5f45ff34cb20b9e9b079e6b4
Bizzdimeji <66257627+Bizzdimeji@users.noreply.github.com>
2020-06-03 23:28:05 +0100

    oladimeji-abiola.py
    Script created using Python

--- oladimeji-abiola.java
+++
@@ -1,6 +0,0 @@
-class OladimejiAbiola {
-    public static void main(String[] args) {
-        System.out.print("Hello World, ");
-        System.out.println("this is Oladimeji Abiola with HNGi7 ID HNG-04204 and email bizzdimeji@gmail.com using Java for stage 2 task");
-    }
-}
