49df928293ed499e07fe430d4c75a17084ce9618
edmund1645 <edmund.timfon@gmail.com>
2020-06-03 11:40:20 +0100

    add Timfon Ekott script

---
+++ scripts/Timfon-Ekott.js
@@ -0,0 +1,9 @@
+const fullName = 'Timfon Ekott';
+const id = 'HNG-04227';
+const userName = 'Blodhound';
+const language = 'JavaScript';
+
+const message = (() =>
+  `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)();
+
+console.log(message);
