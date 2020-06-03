15a4930da9a14d37e3d94fbd8f11de961cedafcc
Michael Ajanaku <remiljw@gmail.com>
2020-06-03 17:28:50 +0100

    removed hyphen

--- scripts/Michael-Ajanaku.py
+++ scripts/Michael-Ajanaku.py
@@ -4,6 +4,6 @@ def stage1_task():
     id ="02854"
     email = "remiljw@gmail.com"
     language = "Python"
-    print("Hello world, this is " + name + " with"+ " HNGi7 ID-" + id + " and email " + email + " using " + language + " for stage task 2.")
+    print("Hello world, this is " + name + " with"+ " HNGi7 ID" + id + " and email " + email + " using " + language + " for stage task 2.")
 
 stage1_task()
