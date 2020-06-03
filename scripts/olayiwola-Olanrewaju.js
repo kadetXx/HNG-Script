006cb69975f3ecd014892b025f3486798d764df4
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-06-03 10:04:57 +0100

    fixed script

--- scripts/olayiwola-Olanrewaju.js
+++ scripts/olayiwola-Olanrewaju.js
@@ -1,10 +1,12 @@
 const lanreScript=(value)=>{
-    console.log(`Hello World, this is ${value.name} with HNGi7 ID ${value.id} using JavaScript for stage 2 task`)
+    console.log(`Hello World, this is ${value.name} with HNGi7 ID ${value.id} using ${value.language} for stage 2 task`)
 }
 
 const value={
     name:"Olayiwola Olanrewaju",
-    id:"HNG-02315"
+    id:"HNG-02315",
+    email:"larry_coal@outlook.com",
+    language:"Javascript"
 };
 
 lanreScript(value)
