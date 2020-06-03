9a9587136378ec376c9440aacaaef36907134514
geekywolf <t4riumph@gmail.com>
2020-06-03 13:12:42 +0200

    Create iselen-triumph.py
    this is my submitted task

---
+++ scripts/iselen-triumph.py
@@ -0,0 +1,8 @@
+"""This file outputs my name, registration number, email and language used"""
+
+Name= "Iselen Triumph"
+ID= "HNG-04439"
+Email= "t4riumph@gmail.com"
+language= "python"
+
+print(f"Hello World, this is {Name} with HNGi7 ID {ID} using {language} for stage 2 task")
