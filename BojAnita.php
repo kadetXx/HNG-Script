7ca79ac9166c537b066663358e4d2f8fa8f732aa
Bojanita <66220339+Bojanita@users.noreply.github.com>
2020-06-03 11:00:17 -0700

    Add files via upload

---
+++ BojAnita.php
@@ -0,0 +1,12 @@
+<?php
+$Surname='Jessa';
+$firstname='Anita';
+$othername='Okeoghene';
+$HNGi7='HNG-02058';
+$email='brightjaniluv@gmail.com';
+$lang='PHP';
+
+echo "Hello world, this is $Surname $firstname $othername with $HNGi7 and $email
+using $lang for stage two task.";
+
+?>
