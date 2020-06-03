ffe7c559026896bd3e3ec7b638cd11b6651380a9
Tobi Osimosu <osimosutobi@gmail.com>
2020-06-03 21:41:02 +0100

    changes made

--- scripts/opeyemi-peter.js
+++ scripts/opeyemi-peter.js
@@ -1,9 +1,10 @@
 const fullName = "Opeyemi Peter";
 const ID = "HNG-01612";
+const Email = "opeyemiadebowale1759@gmail.com";
 const language = "Javascript";
 
 function intro() {
-    return `Hello world, this is ${fullName }, with  HNGi7 ID: ${ID} using ${language } for Stage 2 task`
+  return `Hello World, this is ${fullName} with HNGi7 ID ${ID} and email ${Email} using ${language} for Stage 2 task`;
 }
 
-console.log(intro())
+intro();
