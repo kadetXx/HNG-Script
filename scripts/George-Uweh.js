34dc35b36f716c2f830a3a6ad003bf6f2d6b25f9
GAMHnile <georgeamhuweh@gmail.com>
2020-06-03 10:29:23 +0100

    Added George-Uweh.js to scripts folder

---
+++ scripts/George-Uweh.js
@@ -0,0 +1,6 @@
+//File to complete HNG task 1 group work
+//By GAMHnile
+const GEORGE_DATA = ['George Uweh', 'HNG-01125', 'JavaScript'];
+
+
+console.log (`Hello World, this is ${GEORGE_DATA[0]} with HNGi7 ID ${GEORGE_DATA[1]} using ${GEORGE_DATA[2]} for stage 2 task`);
