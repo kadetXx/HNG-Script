2a2a9e0fb4b5bc5f0fb93df426c0f624960d210d
Bizzdimeji <66257627+Bizzdimeji@users.noreply.github.com>
2020-06-03 14:27:35 +0100

    oladimeji-abiola.java
    Script created with java

---
+++ oladimeji-abiola.java
@@ -0,0 +1,6 @@
+class OladimejiAbiola {
+    public static void main(String[] args) {
+        System.out.print("Hello World, ");
+        System.out.println("this is Oladimeji Abiola with HNGi7 ID HNG-04204 using Java for stage 2 task");
+    }
+}
