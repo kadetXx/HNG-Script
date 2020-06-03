e0f26e1fe01360135de2c1d89a0a2a219067cded
MarleyByte <gdashua29@gmail.com>
2020-06-03 15:05:38 +0100

    added a script to display my HNGi7 details as requirement for promotion to stage 2

---
+++ scripts/Dashua_Gamaliel.js
@@ -0,0 +1,3 @@
+let myDetails = { full_name: 'Dashua Gamaliel', hng_id: 'HNG-02186', scripting_lang:'JavaScript'};
+
+console.log("Hello World, this is "+ myDetails.full_name + " with HNGi7 ID "+ myDetails.hng_id + " using " + myDetails.scripting_lang + " for stage 2 task");
