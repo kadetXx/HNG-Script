70ceac6e384c572dd60887678526e9640cb7cbfe
Jules-Renaud <julesrenaud10@gmail.com>
2020-06-03 12:01:17 +0100

    simplify my code with a single method

--- scripts/jules-tjahe.py
+++ scripts/jules-tjahe.py
@@ -1,10 +1,2 @@
-name = "Tjahe Essomba Jules Renaud"
-id = "HNG-01452"
-language = "python"
-message = f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
-
-def helloworld():
-    print(message)
-    return
-
-helloworld()
+print("Hello World, this is {fname} with HNGi7 ID {id} using {language} for stage 2 task"
+    .format(fname = "Tjahe Essomba Jules Renaud", id = "HNG-01452", language = "python"))
