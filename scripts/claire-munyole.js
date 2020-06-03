c914dbb1a57cab25e9c1d94a9e9791d704b0113c
munyolec <33727407+munyolec@users.noreply.github.com>
2020-06-03 14:58:19 +0300

    Update claire-munyole.js

--- scripts/claire-munyole.js
+++ scripts/claire-munyole.js
@@ -1,11 +1,11 @@
 
 
-function task_two_helloworld(name, identification, scripting_language, email) {
+function task_two_helloworld(name, identification, email, scripting_language) {
   console.log(
-    `Hello World, this is ${name} with HNGi7 ID ${identification} using ${scripting_language} for stage 2 task`
+    `Hello World, this is ${name} with HNGi7 ID ${identification} and ${email} using ${scripting_language} for stage 2 task`
   );
 }
-task_two_helloworld("Claire Munyole", "HNG-00045", "Javascript","munyolec@gmail.com");
+task_two_helloworld("Claire Munyole", "HNG-00045", "munyolec@gmail.com", "Javascript");
 
 
 
