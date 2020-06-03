7327a6f6dd2431f8ba3dcc74ef2ae0ea594ce010
Steven Kolawole <45284829+SteveKola@users.noreply.github.com>
2020-06-03 14:08:30 +0100

    Update Steven_Kolawole.py

--- scripts/Steven_Kolawole.py
+++ scripts/Steven_Kolawole.py
@@ -1,5 +1,4 @@
 def task_2():
-  return "Hello World, this is Steven Kolawole with HNGi7 ID HNG-01749 and \
-email kolawolesteven99@gmail.com using Python for stage 2 task."
+  return "Hello World, this is Steven Kolawole with HNGi7 ID HNG-01749 and email kolawolesteven99@gmail.com using python for stage 2 task."
   
 print(task_2())
