dd7f173bddb3a5a52052264364763b51c26796ac
tchytech <65729360+tchytech@users.noreply.github.com>
2020-06-03 17:15:16 +0300

    onah script

---
+++ onah-tochukwu.js
@@ -0,0 +1,6 @@
+var name= 'onah tochukwu'
+username= tchytech
+email=pearlpey1;
+HNG = HNG-02371
+//LOG
+console. log(" Hello world, this is ${name} with the HNGi ID-02371 using javascript for stage 2")
