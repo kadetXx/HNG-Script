1c075e1139fb09631eb4fdd6f745aa6e51c1a6eb
OtchereDev <59093261+OtchereDev@users.noreply.github.com>
2020-06-03 12:22:11 +0000

    added email

--- scripts/oliverotchere.js
+++ scripts/oliverotchere.js
@@ -2,7 +2,8 @@
 const name = 'Oliver Otchere';
 const id = 'HNG-01090';
 const language = 'javaScript';
+const email='oliverotchere4@gmail.com';
 
-const info = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+const info = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
 
 console.log(info);
