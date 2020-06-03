f4c5559877bdb234e47731a532ed7b0a27eeec7c
proxy6 <progresseze@gmail.com>
2020-06-03 04:31:24 +0100

    Modified Progress-eze.js file

--- scripts/Progress-Eze.js
+++ scripts/Progress-Eze.js
@@ -1,3 +1,2 @@
-var str = ("Hello World, this is [Progress Eze] with HNGi7 ID [00291] using [javascript] for stage 2 task");
+let str = ("Hello World, this is Progress Eze with HNGi7 ID HNG-00291 using javascript for stage 2 task");
 console.log(str);
-
