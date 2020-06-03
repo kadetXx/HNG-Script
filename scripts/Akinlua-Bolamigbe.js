195becdb6436b6adbe55c82e479909c7fd9d8faf
Akinlua Bolamigbe Jomiloju <bolamigbeakinlua@gmail.com>
2020-06-03 13:00:44 +0100

    added my email to the output string

--- scripts/Akinlua-Bolamigbe.js
+++ scripts/Akinlua-Bolamigbe.js
@@ -3,6 +3,7 @@ const myCredentials = {
   firstName: 'Akinlua',
   lastName: 'Bolamigbe Jomiloju',
   HNGi7_ID: 'HNG-04764',
+  email: 'bolamigbeakinlua@gmail.com',
   language: 'Javascript',
 };
 //
@@ -12,7 +13,7 @@ const createOutput = (arg) =>
 ((credentials) => {
   const pertitions = {
     pertition_1: `Hello World, this is ${credentials.firstName} ${credentials.lastName}`,
-    pertition_2: ` with HNGi7 ID ${credentials.HNGi7_ID} using `,
+    pertition_2: ` with HNGi7 ID ${credentials.HNGi7_ID} and email ${credentials.email} using `,
     pertition_3: `${credentials.language} for stage 2 task`,
   };
   const result = createOutput(pertitions);
