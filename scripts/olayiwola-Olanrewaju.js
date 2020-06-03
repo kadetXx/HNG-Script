192ff34869fe2925b3bb1e27f39e8a6d9f809578
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-06-03 09:47:34 +0100

    fixed script

--- scripts/olayiwola-Olanrewaju.js
+++ scripts/olayiwola-Olanrewaju.js
@@ -1,1 +1,10 @@
-console.log("Hello World, this is Olayiwola Olanrewaju with HNGi7 ID HNG-02315 using JavaScript for stage 2 task")
+const lanreScript=(value)=>{
+    console.log(`Hello World, this is ${value.name} with HNGi7 ID ${value.id} using JavaScript for stage 2 task`)
+}
+
+const value={
+    name:"Olayiwola Olanrewaju",
+    id:"HNG-02315"
+};
+
+lanreScript(value)
