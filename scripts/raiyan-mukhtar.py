01282525c8a2416c8b5842c4545bc2df8f572cf4
raiyan-aegon <62125235+raiyan-aegon@users.noreply.github.com>
2020-06-03 01:28:12 +0000

    Update raiyan-mukhtar.py

--- scripts/raiyan-mukhtar.py
+++ scripts/raiyan-mukhtar.py
@@ -1,5 +1,5 @@
 name = "Raiyan Mukhtar"
-id = "01816"
+id = "HNG-01816"
 language ="python"
 
 print("Hello world, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.".format(name,id,language))
