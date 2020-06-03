05ef25efa562829312e11144b04891d425af2470
Dahnie <57042077+Dahnie@users.noreply.github.com>
2020-06-03 11:16:35 +0100

    Create Daniel_Adeneye.js

---
+++ scripts/Daniel_Adeneye.js
@@ -0,0 +1,9 @@
+const STATUS = {
+    "firstName": "Daniel",
+    "lastName": "Adeneye",
+    "language": "Javascript",
+    "id": "HNG-02304"
+};
+
+const RESULT = () => `Hello World, this is ${STATUS.firstName} ${STATUS.lastName} with HNGi7 ID ${STATUS.id} using ${STATUS.language} for stage 2 task`;
+console.log(RESULT());
