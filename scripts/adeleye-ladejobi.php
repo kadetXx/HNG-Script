39fd97bfdd40062bf59cdc5c9f5fc7f9af1774d0
lexemmy <emmy007.el@gmail.com>
2020-06-03 10:12:34 +0100

    php script

---
+++ scripts/adeleye-ladejobi.php
@@ -0,0 +1,10 @@
+<?php
+
+$first_name = "Adeleye";
+$last_name = "Ladejobi";
+$hng_id = "HNG-02958";
+
+echo "Hello World, this is ".$first_name." ".$last_name." with HNGi7 ID ".$hng_id." using php for stage 2 task";
+
+?>
+
