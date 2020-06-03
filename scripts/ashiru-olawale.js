188df0f807155d47247ccc606aba3d36317741c6
wale-bant <wale.ashiru04@gmail.com>
2020-06-03 13:27:41 +0100

    add email

--- scripts/ashiru-olawale.js
+++ scripts/ashiru-olawale.js
@@ -1,9 +1,10 @@
-const fullName = 'Ashiru Olawale';
-const id = 'HNG-01958';
-const language = 'JavaScript';
-const email = 'walebant1@gmail.com';
+const person = {
+  fullName: 'Ashiru Olawale',
+  id: 'HNG-01958',
+  language: 'JavaScript',
+  email: 'walebant1@gmail.com',
+};
 
-const greet = () =>
-  `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
+const speech = `Hello World, this is ${person.fullName} with HNGi7 ID ${person.id} and email ${person.email} using ${person.language} for stage 2 task`;
 
-console.log(greet());
+console.log(speech);
