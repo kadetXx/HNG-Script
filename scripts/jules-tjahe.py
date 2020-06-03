415fa5353e59f549bca11b684657867d95cb2a94
Jules-Renaud <julesrenaud10@gmail.com>
2020-06-03 12:41:56 +0100

    making my code unique Jules-tjahe

--- scripts/jules-tjahe.py
+++ scripts/jules-tjahe.py
@@ -1,2 +1,6 @@
-print("Hello World, this is {fname} with HNGi7 ID {id} using {language} for stage 2 task"
-    .format(fname = "Tjahe Essomba Jules Renaud", id = "HNG-01452", language = "python"))
+def sayHello(myName,myId,myLanguage):
+    print("Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
+    .format(name = myName, id = myId, language = myLanguage))
+    return
+
+sayHello("Tjahe Essomba Jules Renaud","HNG-01452","python")
