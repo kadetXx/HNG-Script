09102be0f78eb6b9b77d6f99626754ab143de6da
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-06-03 18:33:32 +0100

    Update and rename Zakari-Umar.py to Zakari-Umar.js

---
+++ scripts/Zakari-Umar.js
@@ -0,0 +1,1 @@
+console.log("hello world, this is Umar Muhammad Zakari with HNGi7 ID HNG-01897 and email umarfarouqft@gmail.com using python for stage 2 task")
--- scripts/Zakari-Umar.py
+++
@@ -1,1 +0,0 @@
-print("hello world, this is Umar Muhammad Zakari with HNGi7 ID HNG-01897 and email umarfarouqft@gmail.com using python for stage 2 task")
