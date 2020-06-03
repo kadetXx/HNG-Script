a411e12365fd41b416ea6f285bb6cfb381259964
Barnabas Asha <barnabee58@gmail.com>
2020-06-03 15:34:41 +0100

    update

--- scripts/Karen-Efereyan.js
+++ scripts/Karen-Efereyan.js
@@ -1,6 +1,6 @@
 function MoveToStage2(name, id, email, language) {
   console.log(
-    `Hello World, this is ${name} with HNG ID ${id} using ${language} and email ${email} for stage 2 task`
+    'Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' and email ' + email + ' using ' + language + ' for stage 2 task'
   );
 }
 
