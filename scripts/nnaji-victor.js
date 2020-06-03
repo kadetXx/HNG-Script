45e29f8db54dd2a469cf72ea565ca552d86197ee
Nnaji Victor <nnajivictor0@gmail.com>
2020-06-03 12:33:36 +0100

    Create nnaji-victor.js
    Including my script

---
+++ scripts/nnaji-victor.js
@@ -0,0 +1,7 @@
+//data for nnaji victor
+const data = { 
+  name: 'Victor Nnaji', 
+  ID: 'HNG-04553', 
+  language:'javaScript'
+  };
+console.log (`Hello World, this is ${data.name} with HNGi7 ID ${data.ID} using ${data.language} for stage 2 task`);
