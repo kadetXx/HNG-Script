69a0aaed0b41bb061e695dc9d6d6a35008a46aa3
Kadet <techkadet@gmail.com>
2020-06-03 12:07:44 +0100

    updated script according to new string format by kadet

--- scripts/collins-enebeli.js
+++ scripts/collins-enebeli.js
@@ -6,7 +6,7 @@ function hngTask() {
     email: 'collynizy@gmail.com'
   };
 
-  const output = `Hello World, this is ${intern.fullName} with HNGi7 ID ${intern.id} using ${intern.language} for stage 2 task`;
+  const output = `Hello World, this is ${intern.fullName} with HNGi7 ID ${intern.id} and email ${intern.email} using ${intern.language} for stage 2 task`;
 
   return output;
 }
