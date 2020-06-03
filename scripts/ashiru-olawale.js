c6380f87b56f0a94933b473de2b09d4c9f1e4b94
wale-bant <wale.ashiru04@gmail.com>
2020-06-03 11:51:18 +0100

    ashiru-olawale stage 2 task

---
+++ scripts/ashiru-olawale.js
@@ -0,0 +1,9 @@
+const fullName = 'Ashiru Olawale';
+const id = 'HNG-01958';
+const language = 'JavaScript';
+const email = 'walebant1@gmail.com';
+
+const greet = () =>
+  `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
+
+console.log(greet());
