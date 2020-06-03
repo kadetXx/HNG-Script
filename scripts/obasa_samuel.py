8f9363f96cbaf5420eb0b14ac50fbd49d756a1cb
Obasa Samuel Temitope <obasasamuel96@gmail.com>
2020-06-03 15:41:20 +0100

    Updated Obasa Samuel Script with email

--- scripts/obasa_samuel.py
+++ scripts/obasa_samuel.py
@@ -2,4 +2,4 @@ name = "Obasa Samuel Temitope"
 ID = "HNG-04751"
 language = "Python"
 email = "obasasamuel96@gmail.com"
-print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task, you can reach me via email with %s " %(name, ID, language, email))
+print("Hello World, this is %s with HNGi7 ID %s and email %s using %s for stage 2 task" %(name, ID, email, language))
