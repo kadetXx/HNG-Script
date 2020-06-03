5b525385af3afdb369bc86a8dbe62261bafc708c
Ekor Ibor <47329386+obaseotumoyi@users.noreply.github.com>
2020-06-03 23:51:21 +0100

    Added Email

--- scripts/ekor-ibor.js
+++ scripts/ekor-ibor.js
@@ -1,5 +1,6 @@
-task();
+passTest();
 
-function task() {
-    console.log("Hello World, this is Ekor Ibor with HNGi7 ID HNG-03190 using JavaScript for stage 2 task");
+function passTest() {
+    console.log("Hello World, this is Ekor Ibor with HNGi7 ID HNG-03190 and email iborekor@gmail.com using JavaScript for stage 2 task");
 }
+
