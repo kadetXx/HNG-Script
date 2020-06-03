82a455e5023367f621e93f68d15fa4b6265478b9
Karen Efereyan <thedevkaren@gmail.com>
2020-06-03 16:45:04 +0100

    Update script

--- scripts/Karen-Efereyan.js
+++ scripts/Karen-Efereyan.js
@@ -1,10 +1,8 @@
 function MoveToStage2(name, id, language, email) {
-  (this.name = name), (this.id = id), (this.language = language);
   console.log(
-    `Hello World, This is ${name} with HNG ID ${id} using ${language} and email ${email} for stage2 task`
+    `Hello World, this is ${name} with HNG ID ${id} using ${language} and email ${email} for stage2 task `
   );
 }
-
 MoveToStage2(
   "Efereyan Karen Simisola",
   "HNG-01050",
