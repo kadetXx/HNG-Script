fcab60f69e85b0300d4b53433ac8036a9d2e79dd
netae <ehiheboloaugustar@gmail.com>
2020-06-03 12:45:35 +0100

    AugustaEhihebolo details commit

---
+++ scripts/Augusta-Ehihebolo.js
@@ -0,0 +1,5 @@
+function myDetails(fullName, id, language, email){
+   return (`Hello World, this is ${fullName} with HNGi7 ID-${id} and email ${email} using ${language} for stage 2 task 2`)
+}
+
+console.log (myDetails("Augusta Ehihebolo", "02000", "Node.js", "ehiheboloaugustar@gmail.com"))
