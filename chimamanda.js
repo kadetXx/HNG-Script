3fe130cfad88f5242d0d92fcc0a40e0e316f3c8d
fidele000 <fihacker000@gmail.com>
2020-06-03 23:46:31 +0300

    update

--- chimamanda.js
+++
@@ -1,2 +0,0 @@
-//team task one : a simple output.
-console.log('Hello World, this is Onunwa Glory Chimamanda with HNGi7 ID HNG-03761 and email mcparadikay546@gmail.com using javascript for stage 2 task.')
