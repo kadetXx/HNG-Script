d9363060cefe7ee71519747bab9862839584a427
Heccubernny <ipom4eva@gmail.com>
2020-06-03 15:22:55 +0100

    Heccubernny-HNG-03910 script

---
+++ scripts/heccubernny-HNG-03910.py
@@ -0,0 +1,11 @@
+class mydetails:
+	def __init__(self, name,ID,language,email):
+		self.name = name
+		self.ID = ID
+		self.language = language
+		self.email = email
+
+r = mydetails("Ikubanni Paul", "HNG-03910", "Python","ipom4eva@gmail.com")
+print("Hello World, this is",r.name,"with HNGi7 ID",r.ID,"using",r.language,"for stage 2 task")
+
+		
