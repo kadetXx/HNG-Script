edd1882e7bbf692e85f23b5005c9f0181862ca66
simidawalang <58118557+simidawalang@users.noreply.github.com>
2020-06-04 00:04:50 +0100

    Update simi-dawalang.py

--- scripts/simi-dawalang.py
+++ scripts/simi-dawalang.py
@@ -1,10 +1,5 @@
 full_name = "Simi Morayo Da-Walang"
 hng_ID = "HNG-04578" 
-
-"""The ID is a string because a zero is
-the first digit. As an integer, it would
-have been omitted in the output."""
-
 email = "simidawalang@gmail.com"
 language = "Python"
 
