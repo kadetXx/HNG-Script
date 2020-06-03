aed066fecbed8d630ae0006df32348dc0df60ef2
pythagoras-dev <58697223+pythagoras-dev@users.noreply.github.com>
2020-06-03 22:13:27 +0100

    Add files via upload

---
+++ scripts/Mustapha-Mubarak.py
@@ -0,0 +1,8 @@
+my_info = {"name":"Mubarak Mustapha",
+"id":"HNG-03644",
+"lanquage":"python",
+"email":"pythagoras.dev15@gmail.com"}
+
+
+print("Hello World, this is "+my_info["name"]+" with HNGi7 ID "+my_info["id"]+" and email "+my_info["email"]+" using "+my_info["lanquage"]+" for stage 2 task")
+#print("Hello World, this is Mustapha Mubarak with HNGi7 ID HNG-03644  and email pythagoras.dev15@gmail.com using python for stage 2 task ")
