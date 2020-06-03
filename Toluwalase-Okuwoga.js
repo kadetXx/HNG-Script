7364b02e6cc666bc386881f556782f26f606778b
Toluwalase <55381822+ToluwalaseOkuwoga@users.noreply.github.com>
2020-06-03 06:02:14 +0100

    Update Toluwalase-Okuwoga.js

--- Toluwalase-Okuwoga.js
+++ Toluwalase-Okuwoga.js
@@ -1,4 +1,4 @@
 const fullName = 'Toluwalase Okuwoga';
 const hngId = 'HNG-01109';
-const language = 'HTML, CSS, JavaScript';
+const language = 'JavaScript';
 console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`);
