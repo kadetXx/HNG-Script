5b0e5ea60ec8147f6eaa19d5fcac2ea8d37e7881
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2020-06-03 18:58:32 +0100

    added my script

---
+++ scripts/Emmanuel-Ikwuoma.js
@@ -0,0 +1,13 @@
+const details = {
+    name:   "Ikwuoma Emmanuel",
+    hngID  : "HNG-04940",
+    lang:   "JavaScript",
+
+}
+
+const {name, hngID, lang} = details
+
+const blog = `Hello World, this is ${name} with HNGi7 ID ${hngID} using ${lang} for stage 2 task`;
+console.log(blog);
+
+
