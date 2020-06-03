6c7301df4011dcae33a1d0e5a30b91f5bf0594a8
tchytech <65729360+tchytech@users.noreply.github.com>
2020-06-03 20:30:56 +0100

    Delete onah-tochukwu.js
    delete this

--- onah-tochukwu.js
+++
@@ -1,6 +0,0 @@
-var name= 'onah tochukwu'
-username= tchytech
-email=pearlpey1;
-HNG = HNG-02371
-//LOG
-console. log(" Hello world, this is ${name} with the HNGi ID-02371 using javascript for stage 2")
