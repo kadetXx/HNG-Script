e5627573550c688343b83cc295b6a80fc2eb9707
PreshNd <66221967+PreshNd@users.noreply.github.com>
2020-06-03 19:54:19 +0100

    Update Precious-Ndigwe.js

--- scripts/Precious-Ndigwe.js
+++ scripts/Precious-Ndigwe.js
@@ -3,5 +3,5 @@ const ID = "HNG-02193";
 const lang = "JavaScript";
 const email = "pndigwe23@gmail.com";
 
-const task = `Hello world, this is ${fullName} with HNGi7 ID ${ID} using ${lang} and email ${email} for stage 2 task `;
+const task = `Hello World, this is ${fullName} with HNGi7 ID ${ID} and email ${email} using ${lang} for stage 2 task `;
 console.log(task);
