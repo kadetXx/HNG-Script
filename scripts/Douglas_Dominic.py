37be6f334c0707f4390a1ed7f274ca0b9749195f
xi6th <42514061+xi6th@users.noreply.github.com>
2020-06-03 22:27:18 +0100

    Update Douglas_Dominic.py

--- scripts/Douglas_Dominic.py
+++ scripts/Douglas_Dominic.py
@@ -1,5 +1,5 @@
 Name = "Douglas Dominic"
 ID = "HNG-01669"
 Language = "Python"
-Email = "ejise45@gmail.com"
-print("Hello World, this is",Name, "with HNGi7 ID", ID,"using",Language,"for stage 2 task", "you can reach me via email with ",Email)
+Email = ""
+print("Hello World, this is",Name, "with HNGi7 ID", ID,"using",Language,"for stage 2 task", "you can reach me via email at ejise45@gmail.com ")
