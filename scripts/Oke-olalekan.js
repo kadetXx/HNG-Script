b17ea94b125ac1f04bfff4035987b99bea5f042c
TheAstutian <speakingatoms@gmail.com>
2020-06-03 08:27:22 +0100

    add file to script

---
+++ scripts/Oke-olalekan.js
@@ -0,0 +1,9 @@
+
+const myData = {
+	fullname: "Oke Emmanuel",
+	ID: "HNG-01322",
+	language: "Javascript" 
+}
+
+
+console.log (`Hello World, this is ${myData.fullname} with HNGi7 ID ${myData.ID} using ${myData.language} for stage 2 task`) 
