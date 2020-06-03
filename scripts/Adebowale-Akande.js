2c631c9e26a87feb6d3476b6ee6357a84413ef8d
Edebo <akandeadebowale0@gmail.com>
2020-06-03 14:29:05 +0100

    created Adebowale-Akande.js file

---
+++ scripts/Adebowale-Akande.js
@@ -0,0 +1,9 @@
+let person = {
+  name: "Adebowale Akande",
+  id: "HNG-01754",
+  language: "Javascript",
+};
+
+console.log(
+  `Hello World, this is ${person.name} with HNGi7 ID ${person.id} using ${person.language} for stage 2 task`
+);
