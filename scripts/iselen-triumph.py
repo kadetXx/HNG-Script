e2c5fd3a4106dc41090c3a66f10d9fb84e874673
geekywolf <t4riumph@gmail.com>
2020-06-03 14:06:27 +0200

    Update iselen-triumph.py
    i edited it to add the email criteria

--- scripts/iselen-triumph.py
+++ scripts/iselen-triumph.py
@@ -5,4 +5,4 @@ ID= "HNG-04439"
 Email= "t4riumph@gmail.com"
 language= "python"
 
-print(f"Hello World, this is {Name} with HNGi7 ID {ID} using {language} for stage 2 task")
+print(f"Hello World, this is {Name} with HNGi7 ID {ID} and email {Email} using {language} for stage 2 task")
