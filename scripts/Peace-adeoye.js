ab64204dc1014826c8f54f36279f2e4d7b28415a
Peace-adebola <60345650+Peace-adebola@users.noreply.github.com>
2020-06-03 12:14:09 +0100

    Update Peace-adeoye.js

--- scripts/Peace-adeoye.js
+++ scripts/Peace-adeoye.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Peace Adebola Adeoye with HNGi7 ID HNG-06335 using JavaScript for stage 2 task");
+console.log("Hello World, this is Peace Adeoye with HNGi7 ID HNG-06335 using JavaScript for stage 2 task");
