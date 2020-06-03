95f462280bd361aa77b788c41796be2a6d957838
ORJI CECILIA O <ceciliaorji.co@gmail.com>
2020-06-03 11:06:35 +0100

    Update Orji-Cecilia.js

--- scripts/Orji-Cecilia.js
+++ scripts/Orji-Cecilia.js
@@ -1,6 +1,6 @@
 
 //task 2 to stage 2 promotion using javascript
 
-const myResult = "Hello World, this is Orji Cecilia with HNGi7 ID HNG-02009 using Javascript for stage 2 task";
+const myResult = "Hello World, this is Orji Cecilia with HNGi7 ID 02009 using Javascript for stage 2 task.";
 
 console.log(myResult);
