adcf6e8bb75bbcc97452d6b169a96c43f42f2a60
speak2debby <54291723+speak2debby@users.noreply.github.com>
2020-06-03 13:20:25 +0100

    Update Deborah-ajayi.js

--- Deborah-ajayi.js
+++ Deborah-ajayi.js
@@ -2,4 +2,4 @@ var name= 'Deborah Ajayi'
 username= 'speak2debby'
 id= 'HNG-03850'
 email='speak2debby@gmail.com'
-console.log(`Hello World, this is ${name}, Email-Address: ${email} with slack username: ${username} and HND-ID: ${id} for Stage 2 task`)
+console.log(`Hello World, this is ${name}, with HND-ID ${id}, and email address ${email} using Java-script language  for Stage 2 task`)
