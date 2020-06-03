c3b713594553f8715634941c5730c08748469928
Olamide <saintlammy@gmail.com>
2020-06-03 16:55:19 +0100

    My HNGi7 task one for team sentry

---
+++ scripts/olamide-agboola.js
@@ -0,0 +1,8 @@
+const name = "Olamide Agboola";
+const id = "HNG-00261";
+const email = "saintlammy@gmail.com"
+const language = "Javascript";
+
+const display = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+
+console.log(display);
