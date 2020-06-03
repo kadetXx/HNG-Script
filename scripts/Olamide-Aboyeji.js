352b84ac80d9544b53523a91ad4ac4018cb4a3b1
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2020-06-03 02:19:15 +0100

    Update Olamide-Aboyeji.js

--- scripts/Olamide-Aboyeji.js
+++ scripts/Olamide-Aboyeji.js
@@ -1,1 +1,9 @@
-console.log("Hello World, This is Olamide Aboyeji with HNGi7 ID HNG-05560 using Javascript for stage 2 task.");
+const myData = {
+  name : 'Olamide Aboyeji',
+  HNG_ID : 'HNG-05560',
+  language : 'Javascript',
+  stage : 2
+};
+
+const { name, HNG_ID, language, stage } = myData;
+console.log(`Hello World, This is ${name} with HNGi7 ID ${HNG_ID} using ${language} for stage ${stage} task`);
