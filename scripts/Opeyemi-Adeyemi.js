7a37fe190d03aa8f26c7c72b7a4512b8b172bfd1
Dayvvo <45407706+Dayvvo@users.noreply.github.com>
2020-06-03 19:25:02 +0100

    Update Opeyemi-Adeyemi.js

--- scripts/Opeyemi-Adeyemi.js
+++ scripts/Opeyemi-Adeyemi.js
@@ -4,9 +4,9 @@
 
 (function (name="Adeyemi Opeyemi David",id="HNG-03135",language="Javascript", email="dyvvoo@gmail.com") {
     console.log('Hello World, this is ' +name+ ' with HNGi7 ID ' +id+ ' and email ' +email+ ' using language ' +language+ ' for stage 2 task ')
-=======
-(function (name="Adeyemi Opeyemi David",id="HNG-03135",language="Javascript") {
-    console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using language ' + language + ' for stage 2 task' )
+
+
+
 
 
  })()
