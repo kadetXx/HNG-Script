7eb17926239f7f4f886ab677434128e3700fda43
raraku <airenov500@gmail.com>
2020-06-03 11:53:31 +0100

    Israel Airenovboise, removed email

--- scripts/Israel-Airenovboise.py
+++ scripts/Israel-Airenovboise.py
@@ -1,6 +1,10 @@
+# HNG stage 2 task
+# Airenovboise Israel
+# HNG-05846
+
 print(
-    "Hello World, this is {} with HNGi7 ID {} and email {} using python for stage 2 task".format(
-        "Airenovboise Israel", "HNG-05846", "airenov500@gmail.com"
+    "Hello World, this is {} with HNGi7 ID {} using python for stage 2 task".format(
+        "Airenovboise Israel", "HNG-05846"
     ),
     flush=True,
 )
