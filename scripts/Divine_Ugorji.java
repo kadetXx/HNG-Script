501bd33c83e6f1edc44426fb7c4a3b15c3ee9f2d
Divine Ugorji <divineugorji37@gmail.com>
2020-06-03 21:03:14 +0100

    Corrected update on  my HNG task2

---
+++ scripts/Divine_Ugorji.java
@@ -0,0 +1,6 @@
+public Divine_Ugorji{
+public static void main(String[] args){
+        System.out.print("Hello World, this is Divine Ugorji with HNGi7 ID: HNG-01043 and email divineugorji37@gmail.com " +
+        "using java for stage 2 task");
+        }
+}
