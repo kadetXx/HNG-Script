ba010346a459639e9cf1fe0fd394747447608185
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 03:27:06 +0100

    Update caleb-effiong.js

--- scripts/caleb-effiong.js
+++ scripts/caleb-effiong.js
@@ -6,6 +6,6 @@ developer = {
 
 }
 
-const output = `Hello world, this is ${developer.name} with HNGi7 ID ${developer.id} email ${developer.email} using ${developer.language} for stage 2 task`;
+const output = `Hello world, this is ${developer.name} with HNGi7 ID ${developer.id} and email ${developer.email} using ${developer.language} for stage 2 task`;
 
 console.log(output);
