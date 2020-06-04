4ecb2afa58c5874ab5dc7d4ec0f0c84e27d0bb1b
ejeh godwin <ejehgodwin60@gmail.com>
2020-06-04 21:02:18 +0100

    ejeh-godwin

--- scripts/ejeh-godwin.py
+++ scripts/ejeh-godwin.py
@@ -1,3 +1,3 @@
 
-print('hello world, this is Ejeh Godwin with HNGi7 ID HNG-00319 and email ejehgodwin60@gmail.com using python for stage 2 task')
+print('Hello World, this is Ejeh Godwin with HNGi7 ID HNG-00319 and email ejehgodwin60@gmail.com using python for stage 2 task')
 
