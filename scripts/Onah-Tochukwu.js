19797cc670555352a871a7881b64c46321dfaafd
tchytech <65729360+tchytech@users.noreply.github.com>
2020-06-04 01:10:28 +0100

    Update Onah-Tochukwu.js
    Bug fixing double quote

--- scripts/Onah-Tochukwu.js
+++ scripts/Onah-Tochukwu.js
@@ -4,7 +4,7 @@ let language = "javascript";
 let email = "pearlpey1@gmail.com";
 
 function printMessage(){
-    let message = `Hello World, this is ${fullName} with HNGi7 ID ${hngid} and email ${email} using ${language} for stage 2 task.`
+    let message = "Hello World, this is ${fullName} with HNGi7 ID ${hngid} and email ${email} using ${language} for stage 2 task.";
     
     return message;
 }
