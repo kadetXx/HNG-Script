5132dc0d15fbddd58d79c81bcd43960c7eff637e
folarin oyenuga <folami25@gmail.com>
2020-06-04 15:32:41 +0100

    commit by Folarin Oyenuga

---
+++ scripts/folarin-oyenuga.js
@@ -0,0 +1,10 @@
+const myInfo = [
+  {
+    name: 'Folarin Oyenuga',
+    'HNGi7 ID': 'HNG-00063',
+    email: 'folami25@gmail.com',
+    language: 'JavaScript'
+  }
+];
+
+document.write(`Hello AudioWorkletNode, my name is ${myInfo[0].name} with HNGi7 ID ${myInfo[0]['HNGi7 ID']} and email ${myInfo[0].email} using ${myInfo[0].language} for my stage 2's task.`);
