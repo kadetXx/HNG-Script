17ea2351127be09f1bbb09006e77667573eb5a70
Abdoullah Alee U <abdoulfurya@gmail.com>
2020-06-04 10:01:05 +0100

    Updated my failed script
    Removed the `:` from the string

--- scripts/abdullahi.aliyu.js
+++ scripts/abdullahi.aliyu.js
@@ -5,5 +5,5 @@
 	HNGID: 	01827,
 	email: "abdoulfurya@gmail.com"
 }
-console.log("Hello World, this is  " + me.firstName +  " "+ me.lastName + " with HNGi7 ID HNG-"+ me.HNGID + " and email: " + me.email + " using javascript for stage 2 task.")
+console.log("Hello World, this is  " + me.firstName +  " "+ me.lastName + " with HNGi7 ID HNG-"+ me.HNGID + " and email " + me.email + " using javascript for stage 2 task.")
 }
