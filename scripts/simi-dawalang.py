c61d03b3bb930cf1f017f82e9c59a4b1e6276a4c
simidawalang <58118557+simidawalang@users.noreply.github.com>
2020-06-04 10:03:02 +0100

    Update simi-dawalang.py

--- scripts/simi-dawalang.py
+++ scripts/simi-dawalang.py
@@ -3,4 +3,4 @@ HNGid  = "HNG-04578"
 email = "simidawalang@gmail.com"
 plang = "Python"
 
-print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(fullname,HNGid, email,plang))
+print('Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task'.format(fullname,HNGid, email,plang))
