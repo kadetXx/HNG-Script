e07f38a1733a579b0eb1ab3ad37a58269285decf
babstunder <63543336+babstunder@users.noreply.github.com>
2020-06-04 12:05:21 +0100

    Create Lawal-Toheeb.js

---
+++ scripts/Lawal-Toheeb.js
@@ -0,0 +1,3 @@
+function remark(){  
+console.log("Hello World, this is Lawal Toheeb Babatunde with HNGi7 ID HNG-01600 and email lawaltoheeb231@gmail.com using javascript for stage 2 task")}; 
+remark();
