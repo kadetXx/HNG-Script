db31974c44d786ea899516ae9a088c120584b1dc
Joshua Ogbonna <devjaykes@gmail.com>
2020-06-04 14:03:42 +0100

    Modified joshua-ogbonna.js

--- scripts/joshua-ogbonna.js
+++ scripts/joshua-ogbonna.js
@@ -1,5 +1,5 @@
 const myName = 'Joshua Ogbonna'
 const hngId = 'HNG-03663'
-const language = 'JavaScript'
+const language = 'javascript'
 
 console.log(`Hello World, this is ${myName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`)
