74cad9d7e32a11fd64531063084aeab7ae3cf694
Ekor Ibor <47329386+obaseotumoyi@users.noreply.github.com>
2020-06-04 13:29:11 +0100

    Update Ekor-Ibor.js
    Removed the space strings

--- scripts/Ekor-Ibor.js
+++ scripts/Ekor-Ibor.js
@@ -4,7 +4,7 @@ let myEmail = 'iborekor@gmail.com';
 
 
 function passTest() {
-    console.log("Hello World, this is" + " " + myName + " " + "with HNGi7 ID" + " " + myID + " " + "and email" + " " + myEmail + " " + "using JavaScript for stage 2 task");
+    console.log("Hello World, this is " + myName + " with HNGi7 ID " + myID + " and email " + myEmail + " using JavaScript for stage 2 task");
 }
 
 passTest();
