545925ede564c331d32943577db80c5e8119c7a0
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 03:36:56 +0100

    Update Oscar-Ekeyekwu.js

--- scripts/Oscar-Ekeyekwu.js
+++ scripts/Oscar-Ekeyekwu.js
@@ -5,6 +5,6 @@ const Me = {
   lang: 'JavaScript',
 };
 ((me) => {
-  const output = `Hello World, this is ${me.name}  with HNGi7 ID ${me.id} and Email ${me.email} using ${me.lang} for Stage 2 task`;
+  const output = `Hello World, this is ${me.name} with HNGi7 ID ${me.id} and email ${me.email} using ${me.lang} for Stage 2 task`;
   console.log(output);
 })(Me);
