3643225a5ebafa56fcafa1ebc61702c409ffe2a7
root <root@localhost.localdomain>
2020-06-04 09:21:59 -0400

    added details

---
+++ scripts/ahunanya-uche.js
@@ -0,0 +1,6 @@
+let FullName = "Uche Ahunanya Blair";
+let ID = "HNG-04756";
+let Email = "thatboywayne95@gmail.com";
+let Language = "JavaScript";
+
+console.log(`Hello World, this is ${FullName} with HNGi7 ID ${ID} and email ${Email} using ${Language} for stage 2 task`)
