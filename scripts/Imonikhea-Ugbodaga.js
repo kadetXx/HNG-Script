9fd9ee6e784ecb75e985c93b298132de0c764228
nikhea <imonikheaugbodaga@gmail.com>
2020-06-04 17:48:54 +0100

    imonikheaUgbodaga joined

---
+++ scripts/Imonikhea-Ugbodaga.js
@@ -0,0 +1,9 @@
+// Task 2 here i come
+const fullName = 'Imonikhea Ugbodaga';
+const id = 'HNG-06146';
+const email = 'imonikheaugbodaga@gmail.com';
+const language = 'javascript';
+
+console.log(
+	`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+);
