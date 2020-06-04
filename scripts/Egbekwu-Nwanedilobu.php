19c1ea80ead066853d2d72e5ee8e2177f9333155
Egbekwu Nwanedilobu Uche <65601832+N-DiLo@users.noreply.github.com>
2020-06-04 05:41:38 +0100

    Update Egbekwu-Nwanedilobu.php

--- scripts/Egbekwu-Nwanedilobu.php
+++ scripts/Egbekwu-Nwanedilobu.php
@@ -1,14 +1,13 @@
 <?php
-    		$firstname = ['Nwanedilobu'];
-		$lastname = ['Egbekwu'];
-		$middlename = ['Uche']
+// Name & HNGi7 ID Strings to be used for task
+$fullname = "Egbekwu Nwanedilobu Uche";
+$hng_id = "HNG-02761";
 
-						if($firstname = $firstname and $lastname = $lastname
-							and $middlename = $middlename){
-							echo '
-							"Hello World", 
-							this is '$lastname'." ".'$firstname'." ".'$middlename'
-			    		 with HNGi7 ID HNG-02761 using  PHP for stage 2 task
-								';
-							}else//end if
+//Message Strings
+$msg1 = '"Hello World", this is' .$fullname;
+$msg2 = "with HNGi7 ID";
+$msg3 = "using PHP for stage 2 task";
+
+//Displayed Message
+echo $msg1." ".$msg2." ".$hng_id." ".$msg3;
 ?>
