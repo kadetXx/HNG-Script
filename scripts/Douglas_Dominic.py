56c830866e559ced87c905b682cba434f6c567bd
xi6th <42514061+xi6th@users.noreply.github.com>
2020-06-04 11:51:32 +0100

    Update and rename Douglas_Dominic.py to Douglas-Dominic.py

--- scripts/Douglas_Dominic.py
+++
@@ -1,4 +0,0 @@
-Name = "Douglas Dominic"
-ID = "HNG-01669"
-Language = "Python"
-print("Hello World, this is",Name, "with HNGi7 ID", ID,"and email ejise45@gmail.com","using",Language,"for stage 2 task")
