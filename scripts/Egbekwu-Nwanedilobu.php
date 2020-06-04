4feef8a9d14e55d6b3ad9cad7e4df5c51601b5a4
Egbekwu Nwanedilobu Uche <65601832+N-DiLo@users.noreply.github.com>
2020-06-04 02:49:48 +0100

    Create Egbekwu-Nwanedilobu.php

---
+++ scripts/Egbekwu-Nwanedilobu.php
@@ -0,0 +1,14 @@
+<?php
+    		$firstname = ['Nwanedilobu'];
+		$lastname = ['Egbekwu'];
+		$middlename = ['Uche']
+
+						if($firstname = $firstname and $lastname = $lastname
+							and $middlename = $middlename){
+							echo '
+							"Hello World", 
+							this is '$lastname'." ".'$firstname'." ".'$middlename'
+			    		 with HNGi7 ID HNG-02761 using  PHP for stage 2 task
+								';
+							}else//end if
+?>
