0f358e431ff4ba08d6a74b275e0659a1e88e386a
Machin3ry <54332715+Machin3ry@users.noreply.github.com>
2020-06-04 17:27:25 +0100

    So it can pass

--- scripts/Chukwuanieze-Samson.js
+++ scripts/Chukwuanieze-Samson.js
@@ -4,7 +4,7 @@ const email="samsonnnamdi88@gmail.com";
 const language="Javascript";
 
 
-const statement=`Hello World, this is  ${fullname} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
+const statement=`Hello World, this is ${fullname} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
 
 
 
