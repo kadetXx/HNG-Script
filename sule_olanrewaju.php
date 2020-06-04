47962bff43b18212241981537f006172a18d1660
Sule Olanrewaju <suleabimbola@gmail.com>
2020-06-04 14:13:35 +0100

    Update sule_olanrewaju.php

--- sule_olanrewaju.php
+++ sule_olanrewaju.php
@@ -1,1 +1,2 @@
-<?php echo "Hello World, I'm Sule Olanrewaju, with HnGi ID, HNG-05633 and email suleabimbola@gmail.com using php for my stage 2 task"; ?>
+<?php
+  echo "Hello World, Sule Olanrewaju by name, with HnGi ID HNG-05633 and email suleabimbola@gmail.com using php for my stage 2 task"; ?>
