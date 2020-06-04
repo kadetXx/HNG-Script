3606a4ed050f8f8e5bd9832c818aad1798ccc995
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 10:30:55 +0100

    Delete merit-dike.js
    An old duplicate

--- scripts/merit-dike.js
+++
@@ -1,6 +0,0 @@
-const myName = 'Merit Dike';
-const HNGi7_id = 'HNG-00392';
-const email = 'dike.merit@yahoo.com';
-const language = 'Javascript';
-
-console.log('Hello World, this is ${myName} with the HNGi7 ID ${HNGi7_id} and email ${email} using ${language} for stage 2 task');
