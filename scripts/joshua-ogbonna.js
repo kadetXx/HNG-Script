028f0ed4f609c790435df69be28587ba612746cc
Joshua Ogbonna <devjaykes@gmail.com>
2020-06-04 15:52:19 +0100

    Added Email

--- scripts/joshua-ogbonna.js
+++ scripts/joshua-ogbonna.js
@@ -1,5 +1,6 @@
 const myName = 'Joshua Ogbonna'
 const hngId = 'HNG-03663'
 const language = 'javascript'
+const email = 'devjaykes@gmail.com'
 
 console.log(`Hello World, this is ${myName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`)
