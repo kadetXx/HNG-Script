640a0ca0bbb9c19c5690e9c7c5c080d34eb9b372
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 12:34:23 +0100

    Delete ONYEUKWU-CHIDUBEM.js
    Outside scripts folder

--- ONYEUKWU-CHIDUBEM.js
+++
@@ -1,9 +0,0 @@
-var fullName = "Onyeukwu Chidubem";
-var identity = "HNG-03733";
-var language = "Javascript";
-var email = "onyeukwuchidubemclinton@gmail.com";
-
-function readMessage(){
-   var message =" Hello World, this is " + fullName + " with HNGi7 ID  " +  identity + " and email " + email +  " using " + language + " for stage 2 task.";                                                                                                                            return message;
-};                                                               
-console.log(readMessage());
