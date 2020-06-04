b47d5e7ba0b1c736bb60038c498f0a7b05e71386
Ifeoluwa <64071631+IfeAkins@users.noreply.github.com>
2020-06-04 10:33:38 +0100

    Updated Ifeoluwa-Akintayo.py

--- Ifeoluwa-Akintayo.py
+++ Ifeoluwa-Akintayo.py
@@ -2,4 +2,4 @@ name = "Akintayo Ifeoluwa Janet"
 ID = "HNG-00295"
 language = "Python"
 email = "akintayoife94@gmail.com"
-print("Hello World,this is", name, "with HNGi7 ID", ID, "and", email, "using", language, "for stage 2 task.")
+print("Hello World, this is", name, "with HNGi7 ID", ID, "and email", email, "using", language, "for stage 2 task.")
