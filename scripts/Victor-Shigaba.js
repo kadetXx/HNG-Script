0d9e74b4cd6f62d008e1081869313f0e52838954
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 10:33:21 +0100

    Delete Victor-Shigaba.js
    An old duplicate

--- scripts/Victor-Shigaba.js
+++
@@ -1,8 +0,0 @@
-const user = {
-  name: "Victor shigaba",
-  internshipId: "HNG-02863",
-  language: "Javasript",
-  email:"victorshigaba300@gmail.com"
-};
-const response = 'Hello World, this is ${user.name} with  HNGi7 ID ${user.internshipId} and using email ${user.email} using ${user.language} for stage 2 task';
-console.log(response);
