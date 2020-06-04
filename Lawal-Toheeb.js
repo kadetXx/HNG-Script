639bec85c8609c9d9bb46c7abca59d7fe6bd2d3a
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 11:06:47 +0100

    Delete Lawal-Toheeb.js
    Outside scripts folder

--- Lawal-Toheeb.js
+++
@@ -1,3 +0,0 @@
-function remark(){
-  console.log("Hello World, this is Lawal Toheeb Babatunde with HNGi7 ID HNG-01600 and email lawaltoheeb231@gmail.com using javascript for stage 2 task")};
-  remark();
