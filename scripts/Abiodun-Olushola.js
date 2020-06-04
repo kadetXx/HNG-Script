c7589d74f0760f41e688c08773b2c0fd20412427
Henry Mutegeki <36065782+MutegekiHenry@users.noreply.github.com>
2020-06-04 11:27:40 +0300

    Revert "added hngi7"

--- scripts/Abiodun-Olushola.js
+++ scripts/Abiodun-Olushola.js
@@ -5,5 +5,5 @@ const fullName = `${firstName} ${lastName}`;
 const id = 'HNG-03204';
 const email = 'olushola.abiodun@gmail.com';
 const language = 'JavaScript';
-const message = `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
+const message = `Hello World, this is ${fullName} with ${id} and email ${email} using ${language} for stage 2 task.`;
 console.log(message);
