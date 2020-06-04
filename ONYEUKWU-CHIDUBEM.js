64d3baedfd46d7fe81e02fbbda2066ad5752ae76
chidubem-max <60617530+chidubem-max@users.noreply.github.com>
2020-06-04 11:35:54 +0100

    Update ONYEUKWU-CHIDUBEM.js

--- ONYEUKWU-CHIDUBEM.js
+++ ONYEUKWU-CHIDUBEM.js
@@ -4,6 +4,6 @@ var language = "Javascript";
 var email = "onyeukwuchidubemclinton@gmail.com";
 
 function readMessage(){
-   var message =" Hello World, this is " + fullName + " with HNGi7 ID  " +  identity + " and email: " + email +  " using " + language + " for stage 2 task.";                                                                                                                            return message;
+   var message =" Hello World, this is " + fullName + " with HNGi7 ID  " +  identity + " and email " + email +  " using " + language + " for stage 2 task.";                                                                                                                            return message;
 };                                                               
 console.log(readMessage());
