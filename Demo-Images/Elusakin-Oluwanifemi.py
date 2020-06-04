9ce5ff52f4a5ba4e14e21486ea78ea12eaf32da7
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 19:27:35 +0100

    Delete Elusakin-Oluwanifemi.py

--- Demo-Images/Elusakin-Oluwanifemi.py
+++
@@ -1,6 +0,0 @@
-myName='Elusakin Oluwanifemi'
-HNGi7_id='HNG-05816'
-email='nelusakin@gmail.com'
-language='python'
-
-print('Hello World, this is '+myName+' with the HNGi7 ID '+HNGi7_id+' and email '+email+' using '+language+' for stage 2 task')
