eeec4354ff302c25cfa3ce68bc0f1dfe2311f2af
Ponmile-creator <64560647+Ponmile-creator@users.noreply.github.com>
2020-06-04 10:17:36 +0100

    Add files via upload

--- scripts/Ponmile.js
+++ scripts/Ponmile.js
@@ -1,10 +1,10 @@
 const fullname = "HAFIZ LAWAL";
 const hng_id = "HNG-00562";
 const language = "JavaScript";
+const email = "hafizoluwaponmile@gmail.com"
 
-
-const hngProfile=()=>{
-    return 'Hello World, this is ${HAFIZ OLUWAPONMILE LAWAL} with ID ${00562} and email ${hafizoluwaponmile@gmail.com} using ${JavaScript} for stage 2 task';
+const hngProfile=()=> {
+    return 'Hello World, this is ${name} with ID ${hng_id} and email ${email} using ${language} for stage 2 task';
 }
 
 console.log(hngProfile());
