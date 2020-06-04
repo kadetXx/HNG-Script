ba9307a44fd835f24892ea41d1a1f3c1f5cf9060
elsie23-hash <61351217+elsie23-hash@users.noreply.github.com>
2020-06-04 15:09:34 +0100

    Create Elsie_Okoli.py

---
+++ Elsie_Okoli.py
@@ -0,0 +1,11 @@
+intern_name = "Okoli Elsie"
+intern_id = "HNG-05618"
+intern_email = "okolielsie2@gmail.com"
+programming_language = "python"
+
+if intern_name == "Okoli Elsie":
+    print(
+        'Hello world! this is',  intern_name, 'with HNGi7 ID',  intern_id,  'and email',  intern_email,  'using',  programming_language, 'for stage 2 Task')
+
+else :
+    print('Visitors are not allowed in, kindly use the exit')
