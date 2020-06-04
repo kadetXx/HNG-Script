d0550c5f0ba1f107258a5484074677f06f534513
simidawalang <58118557+simidawalang@users.noreply.github.com>
2020-06-04 02:07:41 +0100

    Add files via upload

---
+++ scripts/simi-dawalang.py
@@ -0,0 +1,6 @@
+fullname = "Simi Da-Walang"
+HNDid  = "HND-04578"
+email = "simidawalang@gmail.com"
+lang = "Python"
+
+print('Hello World, this is {} with HNG ID {} and email {} using {} for stage 2 task'.format(fullname,HNDid, email,lang))
