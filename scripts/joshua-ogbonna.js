670429e2ebc3cfe9370a7b5151422b0e2d19a2b0
Joshua Ogbonna <devjaykes@gmail.com>
2020-06-04 18:27:46 +0100

    Added mail to console.log

--- scripts/joshua-ogbonna.js
+++ scripts/joshua-ogbonna.js
@@ -3,4 +3,4 @@ const hngId = 'HNG-03663'
 const language = 'javascript'
 const email = 'devjaykes@gmail.com'
 
-console.log(`Hello World, this is ${myName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`)
+console.log(`Hello World, this is ${myName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`)
