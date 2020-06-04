48eb7d03fe65b46f491dbf3da374c59037a93e90
Tobi Osimosu <osimosutobi@gmail.com>
2020-06-04 02:31:13 +0100

    Re-editted My Code :D

--- scripts/Tobi-Osimosu.js
+++ scripts/Tobi-Osimosu.js
@@ -1,17 +1,32 @@
 // Task 2
 // The script should return the text "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task".
 
-output = (lastName, firstName, middleName, HNGID, email, language) => {
+// output = (lastName, firstName, middleName, HNGID, email, language) => {
+//   console.log(
+//     `Hello World, this is ${lastName} ${firstName} ${middleName} with HNGi7 ID HNG-${HNGID} and email ${email} using ${language} for stage 2 task`
+//   );
+// };
+
+// output(
+//   "Osimosu",
+//   "Oluwatobiloba",
+//   "James",
+//   "01133",
+//   "osimosutobi@gmail.com",
+//   "JavaScript"
+// );
+
+let lastName = "Osimosu";
+let firstName = "Oluwatobiloba";
+let middleName = "James";
+let HNGID = 01133;
+let email = "osimosutobi@gmail.com";
+let language = "JavaScript";
+
+if (lastName && firstName && middleName && HNGID && email && language) {
   console.log(
     `Hello World, this is ${lastName} ${firstName} ${middleName} with HNGi7 ID HNG-${HNGID} and email ${email} using ${language} for stage 2 task`
   );
-};
-
-output(
-  "Osimosu",
-  "Oluwatobiloba",
-  "James",
-  "01133",
-  "osimosutobi@gmail.com",
-  "JavaScript"
-);
+} else {
+  console.log("Error!");
+}
