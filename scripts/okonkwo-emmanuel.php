2cd805bf3ae5567194c165b59a8f9cb744a0da8c
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 03:19:14 +0100

    semi colon added

--- scripts/okonkwo-emmanuel.php
+++ scripts/okonkwo-emmanuel.php
@@ -2,7 +2,7 @@
 
 $name = 'Okonkwo Emmanuel';
 $hngId = 'HNG-00082';
-$email = 'emmanchigo10@gmail.com'
+$email = 'emmanchigo10@gmail.com';
 $language = 'php';
 
 echo("Hello World, this is $name with HNGi7 ID $hngId and email $email using $language for stage 2 task");
