5e68ec69cc356bf63183cddf8bd1481a68a57386
iMorgann <60246004+iMorgann@users.noreply.github.com>
2020-06-04 23:23:14 +0100

    Added Mail

--- scripts/Emmanuel-Nwachukwu.py
+++ scripts/Emmanuel-Nwachukwu.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Emmanuel Nwachukwu with INTERNSHIP ID: HNG-00964 using Python for stage 2 task")
+print("Hello World, this is Emmanuel Nwachukwu with INTERNSHIP ID: HNG-00964 and email: imanuelnwachukwu17@gmail.com using Python for stage 2 task.")
