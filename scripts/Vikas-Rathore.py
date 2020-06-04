b93f063e7536c6542719ea0b8419d2d16d7e5cd8
Vikas Rathore <55776424+VikasRathore162@users.noreply.github.com>
2020-06-04 11:31:21 +0530

    Update Vikas-Rathore.py

--- scripts/Vikas-Rathore.py
+++ scripts/Vikas-Rathore.py
@@ -1,7 +1,7 @@
 def HNG_task():
     name = " Vikas Rathore "
-    ID1 = " HNG-00296 "
+    ID = " HNG-00296 "
     email = " vikasrathour162@gmail.com "
     language=" Python "
-    print('Hello World, this is' +name+'with HNGi7 ID'+ID1+ 'and email' +email+ 'using'+language+ 'for stage 2 task')
+    print('Hello World, this is' +name+'with HNGi7 ID'+ID+ 'and email' +email+ 'using'+language+ 'for stage 2 task')
 HNG_task()
