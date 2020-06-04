1537b744fe342708ef150d9c503bb8d0e522722b
Austin-Deccentric <caugust19.ac@gmail.com>
2020-06-04 04:59:28 +0200

    chore: refactored code.

--- scripts/Augustine-Chukwu.py
+++ scripts/Augustine-Chukwu.py
@@ -1,1 +1,16 @@
-print("Hello World, this is Augustine Chukwu With HNGi7 ID HNG-01030 and email caugust19.ac@gmail.com using python for stage 2 task")
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 04:49:40 2020
+
+@author: Augustine Chukwu
+"""
+
+params = {
+    'name': 'Augustine Chukwu',
+    'ID': 'HNG-01030',
+    'language': 'Python',
+    'email': 'caugust19.ac@gmail.com'
+}
+
+if __name__ == "__main__":
+    print("Hello World, this is " + params['name'] + " With HNGi7 ID " + params['ID'] + " and email " + params['email'] +" using " + params['language']+" for stage 2 task")
