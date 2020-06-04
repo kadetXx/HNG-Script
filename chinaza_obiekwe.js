3b9f3f15a09eb4e3212bdcfc00c0155429f405b7
chinazaaa <50022245+chinazaaa@users.noreply.github.com>
2020-06-04 20:04:57 +0100

    Create chinaza_obiekwe.js

---
+++ chinaza_obiekwe.js
@@ -0,0 +1,9 @@
+const taskTwo = {
+    name: "Naza",
+    id: "HNG - 06705",
+    email: "obiekweagnesmary@gmail.com",
+    submit: () => 
+`Hello World, this is ${this.name} with hng-id ${this.id} and email ${this.email} using Javascript for stage 2 task`
+}
+
+console.log(taskTwo.submit);
