6c952e6c5434b9faf4a3653546be39267cc59962
Philip Daniel Oghenetega <tegadaniels8@gmail.com>
2020-06-04 10:54:38 +0100

    Updated corrections

--- scripts/philip-daniel-oghenetega.js
+++ scripts/philip-daniel-oghenetega.js
@@ -4,6 +4,6 @@ const person = {
   language: 'JavaScript',
   email: 'oghenetegaphilip@gmail.com'
 }
-const values = `Hello World, this is ${person.name} with HNG ID: ${person.hngID}, and email: ${person.email} using ${person.language} for stage 2 task.`;
+const values = `Hello World, this is ${person.name} with HNG ID ${person.hngID} and email ${person.email} using ${person.language} for stage 2 task.`;
 
 console.log(values);
