1853ce6428a3141f00578c369f4ef013886a1293
Tobi Osimosu <osimosutobi@gmail.com>
2020-06-04 02:42:47 +0100

    Re-editted My Code :D

--- scripts/Tobi-Osimosu.js
+++ scripts/Tobi-Osimosu.js
@@ -1,6 +1,7 @@
-// Task 2
-// The script should return the text "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task".
+// TOPIC: Task 2 Challenge
+// INSTRUCTION: The script should return the text "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task".
 
+//                    OPTION 1 (Using ES6 arrow function)
 // output = (lastName, firstName, middleName, HNGID, email, language) => {
 //   console.log(
 //     `Hello World, this is ${lastName} ${firstName} ${middleName} with HNGi7 ID HNG-${HNGID} and email ${email} using ${language} for stage 2 task`
@@ -16,6 +17,7 @@
 //   "JavaScript"
 // );
 
+//                    OPTION 2 (Using If-else statement and ES6 Template Literals)
 let lastName = "Osimosu";
 let firstName = "Oluwatobiloba";
 let middleName = "James";
