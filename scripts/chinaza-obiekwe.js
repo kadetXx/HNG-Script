dc3272858eb53e14862eeee8c8299ffb5e9839fd
chinazaaa <50022245+chinazaaa@users.noreply.github.com>
2020-06-04 21:22:39 +0100

    Update and rename chinaza.obiekwe.js to chinaza-obiekwe.js

---
+++ scripts/chinaza-obiekwe.js
@@ -0,0 +1,9 @@
+const taskTwo = {
+    name: "Naza",
+    id: "HNG - 06705",
+    email: "obiekweagnesmary@gmail.com",
+    submit: () => 
+`Hello World, this is ${taskTwo.name} with hng-id ${taskTwo.id} and email ${taskTwo.email} using Javascript for stage 2 task`
+}
+
+console.log(taskTwo.submit());
