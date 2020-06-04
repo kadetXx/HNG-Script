f7a50b9125fe1451c1fbc3a2d5eb6a28e6198fba
folarin oyenuga <folami25@gmail.com>
2020-06-04 16:11:11 +0100

    Correction by Folarin Oyenuga

--- scripts/folarin-oyenuga.js
+++ scripts/folarin-oyenuga.js
@@ -1,10 +1,9 @@
-const myInfo = [
+const myInfo = 
   {
-    name: 'Folarin Oyenuga',
-    'HNGi7 ID': 'HNG-00063',
+    fullName: 'Folarin Oyenuga',
+    id: 'HNG-00063',
     email: 'folami25@gmail.com',
     language: 'JavaScript'
-  }
-];
+  };
 
-document.write(`Hello AudioWorkletNode, my name is ${myInfo[0].name} with HNGi7 ID ${myInfo[0]['HNGi7 ID']} and email ${myInfo[0].email} using ${myInfo[0].language} for my stage 2's task.`);
+console.log(`Hello World, this is ${myInfo.fullName} with HNGi7 ID ${myInfo.id} and email ${myInfo.email} using ${myInfo.language} for stage 2 task.`);
