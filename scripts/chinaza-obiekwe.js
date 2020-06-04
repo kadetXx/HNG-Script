99cedd0a7d1acc970bca1b237f7c7573eef93885
chinazaaa <50022245+chinazaaa@users.noreply.github.com>
2020-06-04 21:25:57 +0100

    Update chinaza-obiekwe.js

--- scripts/chinaza-obiekwe.js
+++ scripts/chinaza-obiekwe.js
@@ -1,6 +1,15 @@
 const taskTwo = {
-    name: "Naza",
+    name: "Chinaza Obiekwe",
     id: "HNG - 06705",
+    email: "obiekweagnesmary@gmail.com",
+    submit: () => 
+`Hello World, this is ${taskTwo.name} with hng-id ${taskTwo.id} and email ${taskTwo.email} using Javascript for stage 2 task`
+}
+
+console.log(taskTwo.submit());
+const taskTwo = {
+    name: "Naza",
+    id: "HNG-06705",
     email: "obiekweagnesmary@gmail.com",
     submit: () => 
 `Hello World, this is ${taskTwo.name} with hng-id ${taskTwo.id} and email ${taskTwo.email} using Javascript for stage 2 task`
