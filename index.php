d48267a59170735b0956130108a802963f2b39a3
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-04 18:58:18 +0100

    added counter

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ $json = $_SERVER["QUERY_STRING"] ?? '';
 
 $files = scandir("scripts/");
 
-//echo json_encode($files);
-//
-//return;
+
 unset($files[0]);
 unset($files[1]);
 unset($files[2]);
@@ -89,6 +87,10 @@ if (isset($json) && strtolower($json) == 'json') {
 
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script
+                src="https://code.jquery.com/jquery-3.5.1.min.js"
+                integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+                crossorigin="anonymous"></script>
     </head>
 
     <body>
@@ -111,8 +113,23 @@ if (isset($json) && strtolower($json) == 'json') {
         </nav>
     </div>
     <div class="container">
-        <div class="row" style="padding: 2em 0" class="text-center">
 
+        <div class="row" style="padding: 6em 0" class="text-center">
+            <div class="col-md-4 text-center">
+                <button type="button" class="btn">
+                    Submitted <span class="badge badge-primary"><?php echo count($files) ?></span>
+                </button>
+            </div>
+            <div class="col-md-4 text-center">
+                <button type="button" class="btn">
+                    Passes <span class="badge badge-success" id="success">0</span>
+                </button>
+            </div>
+            <div class="col-md-4 text-center">
+                <button type="button" class="btn">
+                    Fails <span class="badge badge-danger" id="failure">0</span>
+                </button>
+            </div>
         </div>
         <table class="table table-hover center table-striped">
             <thead class="thead-dark">
@@ -195,7 +212,13 @@ if (isset($json) && strtolower($json) == 'json') {
                                 </td>
                                 <td>$status ✅</td>
                                 </tr>
+
                              EOL;
+                    ?>
+                    <script>
+                        $('#success').html(Number($('#success').html()) + 1);
+                    </script>
+                    <?php
                 }
                 else {
                     echo <<<EOL
@@ -209,6 +232,12 @@ if (isset($json) && strtolower($json) == 'json') {
                                 <td>$status ❌</td>
                                 </tr>
                             EOL;
+
+                    ?>
+                    <script>
+                        $('#failure').html(Number($('#failure').html()) + 1);
+                    </script>
+                    <?php
                 }
                 $row++;
 
