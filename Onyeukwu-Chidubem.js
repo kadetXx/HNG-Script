c6c3a2088742bc33147284435c08fe12ca30c39e
chidubem-max <60617530+chidubem-max@users.noreply.github.com>
2020-06-04 11:21:58 +0100

    Delete Onyeukwu-Chidubem.js

--- Onyeukwu-Chidubem.js
+++
@@ -1,8 +0,0 @@
-var fullName = "Onyeukwu Chidubem";
-var identity = "HNG-03733";
-var language = "Javascript";
-
-function readMessage(){
-   var message =" Hello World, this is " + fullName + " with HNGi7 ID  " +  identity + " using " + language + " for stage 2 task.";                                                                                                                            return message;
-};                                                               
-console.log(readMessage());
