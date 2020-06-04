ebe7448229f339bc0498ed4e0569c74f1297ba06
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-04 18:13:41 +0100

    Update jalil-zakaria.js

--- scripts/jalil-zakaria.js
+++ scripts/jalil-zakaria.js
@@ -1,1 +1,1 @@
-console.log("Hello World,this is abdul jalil zakaria with HNGi7 ID HNG-03280 using javascript for stage 2 task. abduljalilzakaria1@gmail.com")
+console.log("Hello World, this is abdul jalil zakaria with HNGi7 ID HNG-03280 and email abduljalilzakaria1@gmail.com using javascript for stage 2 task")
