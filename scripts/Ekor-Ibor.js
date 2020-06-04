96f4e3097786ac12221693c42cfa4c2273903c73
Ekor Ibor <47329386+obaseotumoyi@users.noreply.github.com>
2020-06-04 12:22:09 +0100

    Updated the script
    Added outer variables to the script

--- scripts/Ekor-Ibor.js
+++ scripts/Ekor-Ibor.js
@@ -1,10 +1,10 @@
 let myName = 'Ekor Ibor';
 let myID = 'HNG-03190';
-let email = 'iborekor@gmail.com';
+let myEmail = 'iborekor@gmail.com';
 
 
 function passTest() {
-    console.log("Hello World, this is" + " " + myName + " " + "with HNGi7 ID" + " " + myID + " " + "and email" + " " + email + " " + "using JavaScript for stage 2 task");
+    console.log("Hello World, this is" + " " + myName + " " + "with HNGi7 ID" + " " + myID + " " + "and email" + " " + myEmail + " " + "using JavaScript for stage 2 task");
 }
 
 passTest();
