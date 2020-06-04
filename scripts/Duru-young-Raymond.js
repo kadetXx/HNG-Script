7482494c5c0899b5706a5eff1505b30476fc791d
smartray <duruyoungcr@gmail.com>
2020-06-04 14:16:12 +0100

    chore: Create script

---
+++ scripts/Duru-young-Raymond.js
@@ -0,0 +1,12 @@
+function output(fullName, id, email, language) {
+  console.log(
+    `Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+  );
+}
+
+output(
+  "Duru-young Raymond",
+  "HNG-05544",
+  "duruyoungcr@gmail.com",
+  "javaScript"
+);
