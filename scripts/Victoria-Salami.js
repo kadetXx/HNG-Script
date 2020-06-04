2e9ff53703f9c787fff99116bd0f8c1cf4d79424
ikiv-rach <victoriasalmi18@gmail.com>
2020-06-04 01:02:48 +0100

    added a space

--- scripts/Victoria-Salami.js
+++ scripts/Victoria-Salami.js
@@ -2,4 +2,4 @@ let fullname="Victoria Salami";
 let ID = "HNG-03877";
 let language = "Javascript";
 let email = "victoriasalami18@gmail.com";
-console.log(`Hello World,this is ${fullname} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task.`);
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task.`);
