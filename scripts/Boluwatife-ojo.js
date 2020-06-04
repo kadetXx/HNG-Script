fcd719e58440fe73257b5e568b4e013ff7239834
Tabeethaa <66218416+Tabeethaa@users.noreply.github.com>
2020-06-04 13:31:19 +0100

    Update Boluwatife-ojo.js
    Updated my repository

--- scripts/Boluwatife-ojo.js
+++ scripts/Boluwatife-ojo.js
@@ -1,3 +1,3 @@
 console.log(
- "Hello World, this is Boluwatife Ojo with HNGi7 ID HNG-01355 and email ojoboluwatif miemail.com using Javascript for stage 2 task"
+ "Hello World, this is Boluwatife Ojo with HNGi7 ID HNG-01355 and email ojoboluwatife017@gmail.com using Javascript for stage 2 task"
  );
