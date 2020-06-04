881c62e01ceba59e65c05dbba4b06d5aa2a1c1fd
naijauser <unix1gl@gmail.com>
2020-06-04 08:46:41 +0100

    added email

--- scripts/abdullah-momoh.php
+++ scripts/abdullah-momoh.php
@@ -4,9 +4,10 @@
     private $lastname = 'Momoh';
     private $id = 'HNG-00030';
     private $language = 'PHP';
+    private $email = 'momohabdullah20@gmail.com';
 
     public function output(){
-      return "Hello World, this is {$this->firstname} {$this->lastname} with HNGi7 ID {$this->id} using {$this->language} for stage 2 task";
+      return "Hello World, this is {$this->firstname} {$this->lastname} with HNGi7 ID {$this->id} and email {$this->email} using {$this->language} for stage 2 task";
     }
   }
 
