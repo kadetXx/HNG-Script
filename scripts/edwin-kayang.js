b9ff4900517ea2f834b7bb2f5bcadc44c20e446c
Kamikaze49 <44038301+Kamikaze49@users.noreply.github.com>
2020-06-04 16:05:56 +0000

    Update edwin-kayang.js

--- scripts/edwin-kayang.js
+++ scripts/edwin-kayang.js
@@ -1,5 +1,6 @@
 var name = "Edwin Kayang";
 var ID = "HNG-00019";
 var language = "Javascript";
+var emailAddress = "pelpuo2000@gmail.com";
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} and email ${emailAddress} using ${language} for stage 2 task`);
