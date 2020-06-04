f37ef7a8ea2a646b0f786f087f2657661b4934c3
vaatiesther <essyking@gmail.com>
2020-06-04 17:51:33 +0300

    create esthervaati.py file

---
+++ scripts/esthervaati.py
@@ -0,0 +1,5 @@
+# This script prints my details for the HNG internship
+
+
+print("Hello World, this is Esther Vaati with HNGi7 ID HNG-06183 and email vaatiesther@gmail.com using Python for stage 2 task")
+
