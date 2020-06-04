9ff29880ea768db093e22773f362282169a2574e
Adesoji7 <66220562+Adesoji7@users.noreply.github.com>
2020-06-04 10:37:49 -0700

    Email update
    email and language updated

--- scripts/adesoji-adejumo.js
+++ scripts/adesoji-adejumo.js
@@ -1,5 +1,5 @@
 function adesoji(){
-    return "Hello World, this is Adesoji Adejumo with HNGi7 ID HNG-01599 using Jacascript for stage 2 task"
+    return "Hello World, this is Adesoji Adejumo with HNGi7 ID HNG-01599 and email adesojiadejumo@yahoo.com using JacaScript for stage 2 task"
 }
 console.log(adesoji())
  
