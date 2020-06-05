c29b1896e4260ccf64ac1773d2da699fef0b451b
Tonyalex <tonyalex@gmail.com>
2020-06-05 18:33:58 +0100

    sixth commit with email

--- scripts/Nsikak-Akpan.java
+++ scripts/Nsikak-Akpan.java
@@ -2,7 +2,7 @@ def HNG():
     fullname="NsikakAkpan"
     ID="HNG0-1250"
     language="Python"
-    print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task" %(fullname, ID, language))
+    print("Hello World, this is %s with HNGi7 ID %s and email tonyalex6258@gmail.com using %s for stage 2 task" %(fullname, ID, language))
 HNG()
 
 
