d6ccbbe3cf107a0827dce369a0259741051cc485
babyabdul <aral-hassan001@st.ug.edu.gh>
2020-06-05 14:46:14 +0100

    done

--- scripts/abdulrafik.py
+++ scripts/abdulrafik.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Abdul Rafik Al-hassan  with HNGi7 ID HNG-05318 using Python for stage 2 task.aral-hassan001@st.ug.edu.gh")
+print("Hello World, this is Abdul Rafik Al-hassan  with HNGi7 ID HNG-05318 and email aral-hassan001@st.ug.edu.gh using Python for stage 2 task")
