15e438353d05e9585603d6ce1f761950fe194281
babyabdul <aral-hassan001@st.ug.edu.gh>
2020-06-05 18:02:00 +0100

    changed file name and email

--- scripts/rafik.py
+++
@@ -1,2 +0,0 @@
-introduction = "Hello World!, this is Abdul Rafik Al-hassan with HNGi7 ID HNG-05318 and email aral-hassan001@st.ug.edu.gh using Python for Stage 2 task"
-print(introduction)
---
+++ scripts/stage2task.py
@@ -0,0 +1,2 @@
+introduction = "Hello World!, this is Abdul Rafik Al-hassan with HNGi7 ID HNG-05318 and email abdulrafikalhassan@yahoo.com using Python for Stage 2 task"
+print(introduction)
