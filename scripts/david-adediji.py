eee83a0cbf74bf16385fc8a6e8f61b266a4977e2
davidadediji <davidadediji@gmail.com>
2020-06-05 23:03:03 +0100

    new commit

---
+++ scripts/david-adediji.py
@@ -0,0 +1,6 @@
+name="David Adediji"
+id="HNG-05328"
+email="davidadediji@gmail.com"
+language="Python"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
