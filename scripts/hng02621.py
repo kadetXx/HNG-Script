c8f9cf4e7ee0b26848c2c568fced722d938fa3c4
McChizzy <66224357+McChizzy@users.noreply.github.com>
2020-06-06 00:12:48 +0100

    Changes
    id and language edited

--- scripts/hng02621.py
+++ scripts/hng02621.py
@@ -1,6 +1,6 @@
 Name = 'Chikezie Ikeche'
-Language = 'Python'
-id = "HNG_02621"
+Language = 'python'
+id = "HNG-02621"
 email = "ikechex@gmail.com"
 
 print("Hello World, this is", Name, "with HNGi7 ID", id, "and email", email, "using", Language, "for stage 2 test.",)
