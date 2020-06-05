533a4403aed7c33ddb4231d995fbe00468085e0a
babyabdul <aral-hassan001@st.ug.edu.gh>
2020-06-05 17:15:26 +0100

    stage 2 task

--- scripts/abdulrafik.py
+++
@@ -1,9 +0,0 @@
-name = "Abdul Rafik Al-hassan"
-id = "HNG-05318"
-language = "python"
-email = "aral-hassan001@st.ug.edu.gh"
-
-print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
-
-
-
