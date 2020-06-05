25caccd638b19346718221b98869225d40c0fd05
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-05 07:57:16 +0100

    Update Adeyemi-Matthew.py

--- scripts/Adeyemi-Matthew.py
+++ scripts/Adeyemi-Matthew.py
@@ -6,6 +6,6 @@ A script that returns my fullname, HNGi7 ID using python Language.
 """
 
 def helloworld():
-    print("Hello World this is Adeyemi Matthew Iyanuoluwa With HNGi7 ID HNG-03087 and Email using python for stage 2 task")
+    print("Hello World, this is Adeyemi Matthew Iyanuoluwa with HNGi7 ID HNG-03087 and email using python for stage 2 task")
 helloworld()
 
