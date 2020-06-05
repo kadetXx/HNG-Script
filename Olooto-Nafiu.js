85c18538fb412c277967826d38044c6a97306e64
AbuRoyhaanu <66345768+AbuRoyhaanu@users.noreply.github.com>
2020-06-05 06:53:44 +0100

    Create Olooto-Nafiu.js

---
+++ Olooto-Nafiu.js
@@ -0,0 +1,5 @@
+var name = 'Olooto Nafiu';
+var id = '01384';
+var language = 'Javascript';
+
+console.log(' Hello world, this is ' + name + ' with HNG-ID ' + id + ' using ' + language + ' as Stage 2 task.');
