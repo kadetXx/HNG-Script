a569ba3cd7c2d1a3688c8480a3b7d01f17855064
Tonyalex <tonyalex@gmail.com>
2020-06-05 20:29:51 +0100

    corrected commit

--- scripts/Nsikak-Akpan.py
+++ scripts/Nsikak-Akpan.py
@@ -1,6 +1,6 @@
 def HNG():
     fullname="NsikakAkpan"
-    ID="HNG0-1250"
+    ID="HNG-1250"
     language="Python"
     print("Hello World, this is %s with HNGi7 ID %s and email tonyalex6258@gmail.com using %s for stage 2 task" %(fullname, ID, language))
 HNG()
