b15267ff68f591cc08c81d62c851a052d6930c65
matt-marly <66228538+matt-marly@users.noreply.github.com>
2020-06-05 07:51:46 +0100

    New Request

--- scripts/Adeyemi-Matthew.py
+++ scripts/Adeyemi-Matthew.py
@@ -5,10 +5,7 @@ A script that returns my fullname, HNGi7 ID using python Language.
 
 """
 
-while True:
-    Full_Name= "Adeyemi Matthew Iyanuoluwa"
-    HNGi7_ID= "HNG-03087"
-    Language= "Python"
-    Email= "mattadeyemi15r@gmail.com"
-    print("Hello World,this is",Full_Name,"with HNGi7 ID",HNGi7_ID,'and email',Email,'using',Language,'for stage 2 task')
-    break
+def helloworld():
+    print("Hello World this is Adeyemi Matthew Iyanuoluwa With HNGi7 ID HNG-03087 and Email using python for stage 2 task")
+helloworld()
+
