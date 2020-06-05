b2ba06a06a9a57bf3a118913fd9da7217439f454
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-05 07:42:20 +0100

    Delete josiahidahosa-1.py
    Outside scripts folder

--- josiahidahosa-1.py
+++
@@ -1,8 +0,0 @@
-surn='Idahosa'
-fname='Josiah'
-hng_id= 'HNG-04694'
-email='idahosajosiah5@gmail.com'
-lang='python'
-mytasks="Hello world, this is {0} {1} with {2} and {3} using {4} for stage 2 task."
-
-print(mytasks. format(surn, fname, hng_id, email, lang))
