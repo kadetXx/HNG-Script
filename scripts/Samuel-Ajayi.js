188f0e11a90e4ebae87885ffbc7daefac43b1016
eljay16163 <66219175+eljay16163@users.noreply.github.com>
2020-06-05 21:56:55 +0100

    Update Samuel-Ajayi.js
    Added email

--- scripts/Samuel-Ajayi.js
+++ scripts/Samuel-Ajayi.js
@@ -1,8 +1,4 @@
-var myFullName= "Samuel Ajayi Toluwa";
-var myHngId= "HNG-00302";
-var	myEmail= "troysammie7@gmail.com";
-var progLanguage= "javascript";
-var outPut= "hello world this is " + myFullName + " with " + myHngId + " and " +  myEmail + " using " + progLanguage + " for stage 2 task ";
-
-
-console.log(outPut) 
+function	hng(name, id,language, email) {
+	console.log(`hello world, this is ${name} with HNGi7 ID ${id} using ${language} and ${email} for stage 2 task`)
+}
+hng ("Ajayi Samuel Toluwa", "HNG-00302", "javascript", "troysammie7@gmail.com")
