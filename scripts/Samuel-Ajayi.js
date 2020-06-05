4ba6a7d04f389d4a10b15e4c870d1342dec9c121
eljay16163 <66219175+eljay16163@users.noreply.github.com>
2020-06-05 22:07:38 +0100

    Update Samuel-Ajayi.js
    bug fixed

--- scripts/Samuel-Ajayi.js
+++ scripts/Samuel-Ajayi.js
@@ -1,4 +1,4 @@
 function	hng(name, id,language, email) {
-	console.log(`hello world, this is ${name} with HNGi7 ID ${id} using ${language} and ${email} for stage 2 task`)
+	console.log(`hello world, this is ${name} with HNGi7 ID ${id} and ${email} using ${language} for stage 2 task`)
 }
 hng ("Ajayi Samuel Toluwa", "HNG-00302", "javascript", "troysammie7@gmail.com")
