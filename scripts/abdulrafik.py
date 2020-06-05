deaf50dbdda9b1eb4336c3778742535fbca75e78
babyabdul <aral-hassan001@st.ug.edu.gh>
2020-06-05 16:47:14 +0100

    now in the right format

--- scripts/abdulrafik.py
+++ scripts/abdulrafik.py
@@ -1,1 +1,9 @@
-print("Hello World, this is Abdul Rafik Al-hassan  with HNGi7 ID HNG-05318 and email aral-hassan001@st.ug.edu.gh using Python for stage 2 task")
+name = "Abdul Rafik Al-hassan"
+id = "HNG-05318"
+language = "python"
+email = "aral-hassan001@st.ug.edu.gh"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
+
+
+
