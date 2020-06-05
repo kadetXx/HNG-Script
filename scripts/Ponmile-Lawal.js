0f2c4a50cbb758075fc37bd8438aaf5f5f678922
Ponmile-creator <64560647+Ponmile-creator@users.noreply.github.com>
2020-06-05 23:45:40 +0100

    Update Ponmile-Lawal.js

--- scripts/Ponmile-Lawal.js
+++ scripts/Ponmile-Lawal.js
@@ -6,4 +6,4 @@ var email = "hafizoluwaponmile@gmail.com"
 
     
 
-console.log ('Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task. ${email}');
+console.log (`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task. ${email}`);
