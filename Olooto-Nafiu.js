20f9ac867efb4d5efac8aed0506cac1c2f28064a
Tobi Osimosu <Osimosutobi@gmail.com>
2020-06-05 07:40:13 +0100

    Update Olooto-Nafiu.js

--- Olooto-Nafiu.js
+++ Olooto-Nafiu.js
@@ -2,4 +2,4 @@ var name = 'Olooto Nafiu';
 var id = '01384';
 var language = 'Javascript';
 
-console.log(' Hello world, this is ' + name + ' with HNG-ID ' + id + ' using ' + language + ' as Stage 2 task.');
+console.log(' Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + language + ' as Stage 2 task.');
