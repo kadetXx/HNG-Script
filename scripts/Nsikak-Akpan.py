5fd0a180bc500e442f791cbdfcb53e4a282486d7
Tonyalex <tonyalex@gmail.com>
2020-06-05 20:39:57 +0100

    a new .py commit

--- scripts/Nsikak-Akpan.py
+++ scripts/Nsikak-Akpan.py
@@ -1,6 +1,6 @@
 def HNG():
     fullname="NsikakAkpan"
-    ID="HNG-1250"
+    ID="HNG-01250"
     language="Python"
     print("Hello World, this is %s with HNGi7 ID %s and email tonyalex6258@gmail.com using %s for stage 2 task" %(fullname, ID, language))
 HNG()
