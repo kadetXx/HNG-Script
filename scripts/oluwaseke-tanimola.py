9c4528bfd1882e0c8a1c5e20eb080e77275f41bc
oluwaseke <66311945+oluwaseke@users.noreply.github.com>
2020-06-05 23:48:07 +0100

    Update oluwaseke-tanimola.py

--- scripts/oluwaseke-tanimola.py
+++ scripts/oluwaseke-tanimola.py
@@ -2,4 +2,4 @@ name = "Oluwaseke Tanimola"
 hngID = "HNG-01339"
 email = "ooluwaseke@gmail.com"
 language = "Python"
-print("Hello World, this is", name, "with HNG ID", hngID, "and email", email, "using", language, "for Stage 2 task")
+print("Hello World, this is", name, "with HNGi7 ID", hngID, "and email", email, "using", language, "for Stage 2 task")
