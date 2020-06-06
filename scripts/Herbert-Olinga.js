1a214450cc1e459f0d31a3a93ef5f15bf1e40221
olinga-dev <tikewilly@gmail.com>
2020-06-06 13:57:37 +0300

    Addeing my Javascript

---
+++ scripts/Herbert-Olinga.js
@@ -0,0 +1,6 @@
+var fullName = "Herbert Olinga " 
+var hngId = "HNG-03838"
+var scriptLang = "Javascript"
+var email = "tikewilly@gmail.com"
+
+console.log("Hello World, this is"+" "+fullName+" "+"with HNGi7 ID"+" "+hngId+" "+"using"+" "+scriptLang+" "+"for stage 2 task and email"+" "+email)
