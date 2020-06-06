49ee0f1f79a43d6c8e64d51a9c0b41dcb8783e44
miztadavid <60026711+miztadavid@users.noreply.github.com>
2020-06-06 20:42:22 +0100

    Update Ofeimu-david.js

--- scripts/Ofeimu-david.js
+++ scripts/Ofeimu-david.js
@@ -4,12 +4,13 @@ class newIntern{
         this.id=id;
         this.email=email;
         this.language=language;
+        
     }
     myProfile(){
-       console.log( `Hello World, this is ${this.name} with HNGi7 ID: ${this.id} and email: ${this.email} using ${this.language} for stage 2 Task.`)
+       console.log( `Hello World, this is ${this.name} with HNGi7 ID HNG-${this.id} and email ${this.email} using ${this.language} for stage 2 Task.`)
     }
 }
-const Me= new newIntern("David",06683, "robinsondavid640@gmail.com","Javascript");
+const Me= new newIntern("Ofeimu David",06683, "robinsondavid640@gmail.com","Javascript");
 Me.myProfile();
 
 
