575ccb81c66c197f2b1bbdf9a0b4f500b7639ffb
aliemeka7 <57525628+aliemeka7@users.noreply.github.com>
2020-06-06 14:16:37 +0100

    Uploaded my file

--- scripts/Chukwuemeka-Ali.js
+++ scripts/Chukwuemeka-Ali.js
@@ -3,4 +3,5 @@ var id = "HNG-01160";
 var email = "aliemeka7@gmail.com";
 var language = "Javascript";
 
-console.log(`Hello world, this is ${fullName} with HNGi7id ${id} and email ${email} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} 
+and email ${email} using ${language} for stage 2 task`);
