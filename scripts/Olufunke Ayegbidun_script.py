2d3dbe5193ee094e7b2cb8f2c02064818f273a20
Olufunke <66262170+oayegbidun@users.noreply.github.com>
2020-06-06 11:00:35 +0100

    Delete Olufunke Ayegbidun_script.py

--- scripts/Olufunke Ayegbidun_script.py
+++
@@ -1,5 +0,0 @@
-email = "oayegbidun@gmail.com"
-full_name = "Olufunke Ayegbidun"
-hng_id = "HNG-04430"
-language = "python"
-print('Hello World, this is ' + full_name + ' with HNGi7 ID ' +  hng_id + ' and email ' + email + ' using ' + language + ' for stage 2 task.')
