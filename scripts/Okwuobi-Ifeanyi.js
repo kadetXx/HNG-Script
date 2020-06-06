67082dabcc6a464455221fdd7a89617dc3c07a8b
Ifeanyi Fredrick Okwuobi <fredrickokwuobi@gmail.com>
2020-06-06 16:36:13 +0100

    corrected error in my Okwuobi-Ifeanyi.js it passes now

--- scripts/Okwuobi-Ifeanyi.js
+++ scripts/Okwuobi-Ifeanyi.js
@@ -3,5 +3,5 @@
 	const ID = "HNG-03608";
 	const Email = "fredrickokwuobi@gmail.com";
 	const Language = "JavaScript";
-	let outputText = `Hello World, this is ${FullName} with HNGi7 ID: ${ID} and email ${Email} and i am using ${Language} for stage 2 task`;
+	let outputText = `Hello World, this is ${FullName} with HNGi7 ID ${ID} and email ${Email} and i am using ${Language} for stage 2 task`;
 	console.log(outputText);
