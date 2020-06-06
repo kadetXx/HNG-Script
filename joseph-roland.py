8fe1f24490574bc9b64ad812969e519cdd9d4d4d
roland joseph <wordofwisdom_2@yahoo.com>
2020-06-06 14:30:21 +0100

    new change to World

--- joseph-roland.py
+++
@@ -1,1 +0,0 @@
-print("Hello world, this is Joseph Roland with HNGi7 ID HNG-06046 and email wordofwisdom_2@yahoo.com using python for stage 2 task")
---
+++ scripts/joseph-roland.py
@@ -0,0 +1,1 @@
+print("Hello World, this is Joseph Roland with HNGi7 ID HNG-06046 and email wordofwisdom_2@yahoo.com using python for stage 2 task")
