85d406582bfb2224752c116530f3fd4eb0dca6d3
Sule Olanrewaju <suleabimbola@gmail.com>
2020-06-06 19:24:26 +0100

    latest olanrewaju-sule.php

--- olanrewaju-sule.php
+++ olanrewaju-sule.php
@@ -3,4 +3,4 @@ $username="sule olanrewaju";
 $hngId="HNG-05633";
 $email_address="suleabimbola@gmail.com";
 $lang="php";
-echo "Hello World, this is ".$username." with HNGi7 ID ".$hngId." and email ".$email_address." using ".$lang." for stage 2 task ";?>
+echo "Hello World, this is ".$username." with HNGi7 ID ".$hngId." and email ".$email_address." using ".$lang." for stage 2 task";?>
