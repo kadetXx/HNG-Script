19f6e385a2effac8b3e807a8fb52df4e21008e1f
MAC <mac@MACs-MacBook-Pro.local>
2020-06-06 18:05:19 +0100

    commit

--- scripts/tola-shobowale.js
+++ scripts/tola-shobowale.js
@@ -1,10 +1,10 @@
 const user = {
   fullname: "Tola Shobowale",
   id: "HNG-00272",
-  language: "javaScript",
+  language: "JavaScript",
   email: "shobowaletola@gmail.com",
 };
 
 console.log(
-  `Hello World, this is ${user.fullname} with HNGi7 ID ${user.id} and email ${user.email}using ${user.language}for stage 2 task`
+  `Hello World, this is ${user.fullname} with HNGi7 ID ${user.id} and email${user.email}using ${user.language}for stage 2 task`
 );
