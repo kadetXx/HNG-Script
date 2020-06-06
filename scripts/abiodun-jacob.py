76fcef4d7b400dc802617ebe10d94df814358379
jacobyanmife <jacobyanmife@gmail.com>
2020-06-06 10:29:17 +0100

    added abiodun-jacob.py

---
+++ scripts/abiodun-jacob.py
@@ -0,0 +1,5 @@
+full_name = "Abiodun Jacob"
+hng_id = "HNG-02431"
+language = "Python"
+email = "jacobyanmife@gmail.com"
+print("Hello World, this is " + full_name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task email " + email +".")
