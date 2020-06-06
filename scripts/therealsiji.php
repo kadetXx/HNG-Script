0535305179099b124f2ddb6919e433efd8116bc0
siji007 <pomolia007@gmail.com>
2020-06-06 10:49:50 -0700

    updated therealsiji.php

--- scripts/therealsiji.php
+++ scripts/therealsiji.php
@@ -3,5 +3,5 @@ $name = "Oluwasijibomi Olaifa";
 $hng_id = "HNG-05396";
 $email = "pomolia007@gmail.com";
 $language = "PHP";
-echo "Hello World this is ".$name." with HNG ID ".$hng_id." "."and email ".$email." "."using ".$language." "."for stage2 task";
+echo "Hello world, this is ".$name." with HNGI7 ID: ".$hng_id." "."and email ".$email." "."using ".$language." "."for stage 2 task";
 ?>
