7d4cd59029daf83b8a50640ff9ca567f349e74ff
Saba Saeed <62014225+sabasaeed408@users.noreply.github.com>
2020-06-06 17:24:45 +0100

    Added new file

---
+++ scripts/Saba-Saeed.java
@@ -0,0 +1,14 @@
+
+public class SabaSaeed{
+
+    public static void main(String[]args){
+
+        String internName = "Saba Saeed";
+        String internIdNumber = "HNG-06812";
+        String mailAddress = "sabasaeed408@gmail.com";
+
+
+        System.out.println("Hello World, this is " + internName + " with HNGi7 ID " + internIdNumber + " and email " + mailAddress + " using Java for stage 2 task");
+
+    }
+}
