db2df1d26eb0ed4da4dc31925dca79a9a6522cfa
jacobyanmife <jacobyanmife@gmail.com>
2020-06-06 11:13:12 +0100

    abiodun-jacob.py

--- scripts/abiodun-jacob.py
+++ scripts/abiodun-jacob.py
@@ -2,4 +2,4 @@ full_name = "Abiodun Jacob"
 hng_id = "HNG-02431"
 language = "Python"
 email = "jacobyanmife@gmail.com"
-print("Hello World, this is " + full_name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task email " + email +".")
+print("Hello World, this is " + full_name + " with HNGi7 ID " + hng_id + " and email " + email + " using python for stage 2 task")
