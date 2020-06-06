3b23a68ed55949d276abbb21f892e5a4e875c87c
weedydash <57416736+weedydash@users.noreply.github.com>
2020-06-06 22:31:51 +0100

    Update obatobi-ayeni.js

--- scripts/obatobi-ayeni.js
+++ scripts/obatobi-ayeni.js
@@ -12,7 +12,7 @@ let myEmail="ayeniobatobi@gmail.com";
 let myLanguage="JavaScript";
 
 function output(){
-    console.log('Hello World, this is ' + myName + ' with HNGi7 ID ' + myId + ' and email ' + myEmail + ' using ' + myLanguage + ' for stage 2 task.');
+    console.log('Hello World, this is ' + myName + ' with HNGi7 ID ' + myId + ' and email ' + myEmail + ' using ' + myLanguage + ' for stage 2 task');
 }
 
 let info=new info(myName, myId, myEmail, myLanguage);
