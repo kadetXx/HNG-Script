052471401efa157c880ec79dc18fb23222cfd5b2
mcvoltik <49316230+mcvoltik@users.noreply.github.com>
2020-06-06 14:31:17 +0100

    Delete Michael-Ashi.py

--- scripts/Michael-Ashi.py
+++
@@ -1,6 +0,0 @@
-name = "Michael Ashi"
-myId = "HNG-01089"
-language = "python"
-email = "ashimichaelashi@gmail.com"
-
-print("Hello World, this is " + name + " with HNGi7 ID " + myId + " using " + language + " for stage 2 task " + email + "")
