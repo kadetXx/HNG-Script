9de786d76f8eaad69638d2320edfdc68ec78be3e
Peace Ododo <37791651+Onoyiza@users.noreply.github.com>
2020-06-06 18:43:41 +0100

    Add files via upload

---
+++ scripts/Ododo_Peace_O.py
@@ -0,0 +1,8 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Fullname = 'Ododo Peace'
+Hng_id = 'HNG-00106'
+Email = 'ododopeace@gmail.com'
+Language = 'Python'
+result_Peace_O = 'Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task'
+print(result_Peace_O.format(Fullname, Hng_id, Email, Language))
