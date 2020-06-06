2492bb403315d4a12af820bea8720bb6597be740
Tola-byte <66223408+Tola-byte@users.noreply.github.com>
2020-06-06 12:48:10 +0100

    Update Adetola-Adeyeye.js

--- scripts/Adetola-Adeyeye.js
+++ scripts/Adetola-Adeyeye.js
@@ -2,7 +2,7 @@
 let fullName = "Adeyeye Adetola";
 let hngid = "HNG-00876";
 let language = "javascript";
-let email = "adetolaisaih17@gmail.com";
+let email = "adetolaisaiah17@gmail.com";
 function printMessage() {
   let message = `Hello World, this is ${fullName} with HNGi7 ID ${hngid} and email ${email} using ${language} for stage 2 task`;
   return message;
