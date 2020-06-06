56a4d056b5e5c18fb47216aac21d74956c860ad3
Audu Godwin DavizXcoded <33580206+DavizXcoded@users.noreply.github.com>
2020-06-06 18:00:57 +0100

    Add files via upload

---
+++ DavizXcoded-HNG-04018.js
@@ -0,0 +1,6 @@
+var name="Audu Godwin DavizXcoded";
+var myid="HNG-04018";
+var email="swagalima@gmail.com";
+var language="Javascript";
+
+console.log("Hello World, this is" + " " + name + " " + "with HNGi7 ID" + " " + myid + " " + "and email" + " " + email + " " + "using" + " " + language+ " " + "for stage 2 task");
