62263fda66d9675932abe86a2edc89d5c9b9ad1a
McChizzy <66224357+McChizzy@users.noreply.github.com>
2020-06-06 11:23:19 +0100

    NEW UPDATE
    Made changes using .format to parse variables

--- scripts/Ikeche-Chikezie.py
+++ scripts/Ikeche-Chikezie.py
@@ -1,6 +1,7 @@
-Name = 'Chikezie Ikeche'
-Language = 'python'
+Name = "Chikezie Ikeche"
+Language = "python"
 id = "HNG-02621"
 email = "ikechex@gmail.com"
 
-print("Hello World, this is", Name, "with HNGi7 ID", id, "and email", email, "using", Language, "for stage 2 test.",)
+print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format("Chikezie Ikeche", "HNG-02621", "ikechex@gmail.com", "python"))
+
