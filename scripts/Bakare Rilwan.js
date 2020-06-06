1a215ebfe9f726c4904601436ad413b91d2909ef
Bakis-world <65019521+Bakis-world@users.noreply.github.com>
2020-06-06 19:13:43 +0100

    Bakare Rilwan.js

---
+++ scripts/Bakare Rilwan.js
@@ -0,0 +1,5 @@
+let Full_Name ="Bakare Rilwan";
+let HNG_ID="HNG-06612";
+let language="javascript";
+let email="bakis4life@gmail.com";
+console.log("Hello World, this is ",`${Full_Name}`,"with HNGi7 ID",`${HNG_ID}`,"using",`${language}`,"for stage 2 task.",`${email}`)
