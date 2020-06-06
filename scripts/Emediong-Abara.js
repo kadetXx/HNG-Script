6d3e449e29d157c83b2b138cf871de6d737f7380
Emediong Abara <31096230+Emediongabara@users.noreply.github.com>
2020-06-06 21:16:20 +0100

    Create Emediong-Abara.js
    Updated email

---
+++ scripts/Emediong-Abara.js
@@ -0,0 +1,5 @@
+function message(name, id, language, email){
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
+}
+
+message("Emediong Abara", "HNG-00594", "JavaScript", "emediongabara@gmail.com" )
