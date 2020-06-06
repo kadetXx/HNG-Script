335bdb743d8923cebd7b7678bd184cd479dd477c
Bakis-world <65019521+Bakis-world@users.noreply.github.com>
2020-06-06 21:21:54 +0100

    Delete Bakis-Rilwan.js

--- scripts/Bakis-Rilwan.js
+++
@@ -1,5 +0,0 @@
-let Full_Name ="Bakare Rilwan";
-let HNG_ID="HNG-06612";
-let language="javascript";
-let email="bakis4life@gmail.com";
-console.log("Hello World, this is ",`${Full_Name}`,"with HNGi7 ID",`${HNG_ID}`,"using",`${language}`,"for stage 2 task.",`${email}`)
