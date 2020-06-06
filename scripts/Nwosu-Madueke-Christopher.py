e0d3dc739be42b3ec8f36c7375dd27252c0b64fa
Lord-Chris <58702861+Lord-Chris@users.noreply.github.com>
2020-06-06 15:14:20 +0100

    Add files via upload

--- scripts/Nwosu-Madueke-Christopher.py
+++ scripts/Nwosu-Madueke-Christopher.py
@@ -2,5 +2,5 @@ fullname = 'Nwosu-Madueke Christopher'
 ID = 'HNG-06465'
 language = 'python'
 email = 'maduekechris65@gmail.com'
-text = 'Hello World, this is '+fullname+' with HNGi7 ID '+ID+' and email '+email+' using '+language+ ' for stage 2 task.'
+text = 'Hello World, this is '+fullname+' with HNGi7 ID '+ID+' and email '+email+' using '+language+ ' for stage 2 task'
 print (text)
