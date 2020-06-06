15a09b1eaa76cec49345b122ab8b63ac4db2ad79
Bolaji Ayeni <Ayenibolaji3@gmail.com>
2020-06-06 10:16:40 +0100

    Update bolaji-ayeni.js

--- scripts/bolaji-ayeni.js
+++ scripts/bolaji-ayeni.js
@@ -1,2 +1,8 @@
-const HNG = { name: 'Bolaji Ayeni', id: 'HNG-01495', email:'ayenibolaji3@gmail.com', lang:'JavaScript'};
-console.log (`Hello World, my name is ${HNG.name} with Internship ID: ${HNG.id} and email ${HNG.email} using ${HNG.lang} for the stage 2 task`);
+
+let fullname = 'Bolaji Ayeni';
+let HNGid = 'HNG-01495';
+let email = 'ayenibolaji3@gmail.com';
+let proglanguage = 'JavaScript';
+
+console.log(`Hello world, this is ${fullname} with HNGi7 ID ${HNGid} and email ${email} using ${proglanguage} for stage 2 task`)
+
