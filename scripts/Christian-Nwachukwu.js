5fbf0ce699cf359e13c22d1318a41060dea5b69b
Christian <christian.nwachukwu2020@gmail.com>
2020-06-06 01:53:49 +0100

    colon and white space rmoved

--- scripts/Christian-Nwachukwu.js
+++ scripts/Christian-Nwachukwu.js
@@ -1,12 +1,8 @@
 //The function takes fname,id,email and lang as parameters.
 
-function myDetails(fname, id, email, lang) {
+function myDetails(fname,id,email,lang) {
+    console.log(`Hello World, this is ${fname} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`);
+    }
 
-    console.log(`Hello World, this is ${fname} 
-                \nwith HNGi7 ID ${id} and email: ${email} 
-                \nusing ${lang} for stage 2 task.`);
-    
-}
-
-//Calling function with the following arguments.
+//Calling functiaon with the following arguments.
 myDetails("Christian Nwachukwu", "HNG-00941", "christian.nwachukwu2020@gmail.com","Javascript");
