84123e687874cbb6aa6a63e4c0bf0cf85164f6e4
ayodejinicholas <66234864+ayodejinicholas@users.noreply.github.com>
2020-06-07 04:46:54 -0700

    update and rename ayodejinicholas.py to ayodeji-nicholas.py

---
+++ scripts/ayodeji-nicholas.py
@@ -0,0 +1,7 @@
+name = "Ayodeji Nicholas"
+ID = "HNG-00743"
+language = "python"
+
+
+
+print("Hello world, this is " + name + " with email " + email + " with HNGi7 ID " + ID + " and email ayodejinicholas7@gmail.com using " + language + " for stage 2 task")
