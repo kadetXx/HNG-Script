48fa5f5649bfd1d4eb1a00a898382a96d3a70b4c
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-06-07 10:09:45 +0100

    victoriaagbamoro

---
+++ scripts/Agbamoro-Victoria.js
@@ -0,0 +1,11 @@
+let person = "Agbamoro Victoria";
+let hngId = "HNG-04766";
+let language = "javascript";
+let email = "agbamorovictoria@gmail.com";
+
+
+function printData() {
+  let data = `Hello World, this is ${person} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`;
+  return data;
+}
+console.log(printData());
